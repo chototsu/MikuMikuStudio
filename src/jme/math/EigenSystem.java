d00b1dfb91a8974a9de351523bdefa6dbcd79353
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2003-08-21 20:57:18 +0000

    Documented and cleaned up the code. Ready for testing.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@55 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jme/math/EigenSystem.java
+++ src/jme/math/EigenSystem.java
@@ -31,6 +31,10 @@
  */
 package jme.math;
 
+import java.util.logging.Level;
+
+import jme.utility.LoggingSystem;
+
 /**
  * <code>EigenSystem</code> defines a system of eigen values and associated
  * eigen vectors that solves the case AX = (alpha)X where (A - alpha(I)) = 0.
@@ -39,10 +43,10 @@ package jme.math;
  * QL algorithm than shifts the matrix to a diagonal one. 
  * 
  * <br><br>
- * <b>NOTE:</b> See 3D Game Engine Design. David H. Eberly.
+ * <b>NOTE:</b> See 3D Game Engine Design. David H. Eberly. Also, JAMA.
  * 
  * @author Mark Powell
- * @version $Id: EigenSystem.java,v 1.1 2003-08-21 19:18:34 mojomonkey Exp $
+ * @version $Id: EigenSystem.java,v 1.2 2003-08-21 20:57:18 mojomonkey Exp $
  */
 public class EigenSystem {
     private float[][] matrix;
@@ -51,6 +55,7 @@ public class EigenSystem {
     private float[] realEigenValues;
     private float[] imaginaryEigenValues;
     private int size;
+
     /**
      * Constructor instantiates a new <code>EigenSystem</code> object from
      * a supplied matrix. Where the matrix is A in the equation: 
@@ -65,6 +70,8 @@ public class EigenSystem {
         this.matrix = matrix;
         //insure the matrix is square.
         if (matrix[0].length != matrix.length) {
+            LoggingSystem.getLoggingSystem().getLogger().log(Level.WARNING, 
+                "Matrix is not square.");
             return;
         }
         size = matrix.length;
@@ -73,113 +80,120 @@ public class EigenSystem {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (this.matrix[i][j] != this.matrix[j][i]) {
+                    LoggingSystem.getLoggingSystem().getLogger().log(Level.WARNING, 
+                        "Matrix is not symmetric.");
                     return;
                 }
             }
         }
-        
-        
 
         //initialize data and calculate eigen system
         realEigenValues = new float[size];
         imaginaryEigenValues = new float[size];
         eigenVectors = new float[size][size];
         hessenberg = new float[size][size];
-        
+
         //initialize vectors as initial matrix
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-               eigenVectors[i][j] = matrix[i][j];
+                eigenVectors[i][j] = matrix[i][j];
             }
-         }
+        }
     }
 
+    /**
+     * <code>decreasingSort</code> sorts eigenvalues and their associated
+     * eigenvectors in order of largest to smallest. Such that
+     * e[0] >= ... >= e[size - 1].
+     */
     public void decreasingSort() {
-        // sort eigenvalues in decreasing order, e[0] >= ... >= e[iSize-1]
-        for (int i0 = 0, i1; i0 <= size - 2; i0++) {
+        for (int currentValue = 0; currentValue <= size - 2; currentValue++) {
             // locate maximum eigenvalue
-            i1 = i0;
-            float max = realEigenValues[i1];
-            int i2;
-            for (i2 = i0 + 1; i2 < size; i2++) {
-                if (realEigenValues[i2] > max) {
-                    i1 = i2;
-                    max = realEigenValues[i1];
+            int placeHolder = currentValue;
+            float max = realEigenValues[placeHolder];
+            for (int swapValue = currentValue + 1;
+                swapValue < size;
+                swapValue++) {
+                if (realEigenValues[swapValue] > max) {
+                    placeHolder = swapValue;
+                    max = realEigenValues[placeHolder];
                 }
             }
 
-            System.out.println("Current Max: " + max);
-
-            if (i1 != i0) {
-                System.out.println("Swapping: " );
+            if (placeHolder != currentValue) {
                 // swap eigenvalues
-                realEigenValues[i1] = realEigenValues[i0];
-                realEigenValues[i0] = max;
+                realEigenValues[placeHolder] = realEigenValues[currentValue];
+                realEigenValues[currentValue] = max;
 
                 // swap eigenvectors
-                for (i2 = 0; i2 < size; i2++) {
-                    float tmp = eigenVectors[i2][i0];
-                    eigenVectors[i2][i0] = eigenVectors[i2][i1];
-                    eigenVectors[i2][i1] = tmp;
+                for (int swapValue = 0; swapValue < size; swapValue++) {
+                    float tmp = eigenVectors[swapValue][currentValue];
+                    eigenVectors[swapValue][currentValue] =
+                        eigenVectors[swapValue][placeHolder];
+                    eigenVectors[swapValue][placeHolder] = tmp;
                 }
             }
         }
     }
 
+    /**
+     * <code>increasingSort</code> sorts eigenvalues and their associated
+     * eigenvectors in order of smallest to largest. Such that
+     * e[0] <= ... <= e[size - 1].
+     *
+     */
     public void increasingSort() {
         // sort eigenvalues in increasing order, e[0] <= ... <= e[size-1]
-        for (int i0 = 0, i1; i0 <= size - 2; i0++) {
+        for (int currentValue = 0; currentValue <= size - 2; currentValue++) {
             // locate minimum eigenvalue
-            i1 = i0;
-            float min = realEigenValues[i1];
-            System.out.println("new min " + min);
-            int i2;
-            for (i2 = i0 + 1; i2 < size; i2++) {
-                if (realEigenValues[i2] < min) {
-                    i1 = i2;
-                    min = realEigenValues[i1];
-                    System.out.println("new min " + min);
+            int placeHolder = currentValue;
+            float min = realEigenValues[placeHolder];
+            for (int swapValue = currentValue + 1;
+                swapValue < size;
+                swapValue++) {
+                if (realEigenValues[swapValue] < min) {
+                    placeHolder = swapValue;
+                    min = realEigenValues[placeHolder];
                 }
             }
 
-            if (i1 != i0) {
+            if (placeHolder != currentValue) {
                 // swap eigenvalues
-                System.out.println("swapping");
-                realEigenValues[i1] = realEigenValues[i0];
-                realEigenValues[i0] = min;
+                realEigenValues[placeHolder] = realEigenValues[currentValue];
+                realEigenValues[currentValue] = min;
 
                 // swap eigenvectors
-                for (i2 = 0; i2 < size; i2++) {
-                    float fTmp = eigenVectors[i2][i0];
-                    eigenVectors[i2][i0] = eigenVectors[i1][i2];
-                    eigenVectors[i2][i1] = fTmp;
+                for (int swapValue = 0; swapValue < size; swapValue++) {
+                    float tmp = eigenVectors[swapValue][currentValue];
+                    eigenVectors[swapValue][currentValue] =
+                        eigenVectors[placeHolder][swapValue];
+                    eigenVectors[swapValue][placeHolder] = tmp;
                 }
             }
         }
     }
 
+    /**
+     * <code>tridiagonalReduction</code> reduces the matrix to a tridiagonal
+     * matrix using the Householder transformations. 
+     *
+     */
     public void tridiagonalReduction() {
-
-        //  This is derived from the Algol procedures tred2 by
-        //  Bowdler, Martin, Reinsch, and Wilkinson, Handbook for
-        //  Auto. Comp., Vol.ii-Linear Algebra, and the corresponding
-        //  Fortran subroutine in EISPACK.
-
+        //initialize the eigenvalues to the last row of eigenVectors.
         for (int j = 0; j < size; j++) {
             realEigenValues[j] = eigenVectors[size - 1][j];
         }
 
-        // Householder reduction to tridiagonal form.
-
+        //Householder transformation
         for (int i = size - 1; i > 0; i--) {
 
             // Scale to avoid under/overflow.
-
             float scale = 0;
             float h = 0;
             for (int k = 0; k < i; k++) {
                 scale = scale + Math.abs(realEigenValues[k]);
             }
+            
             if (scale == 0) {
                 imaginaryEigenValues[i] = realEigenValues[i - 1];
                 for (int j = 0; j < i; j++) {
@@ -190,7 +204,6 @@ public class EigenSystem {
             } else {
 
                 // Generate Householder vector.
-
                 for (int k = 0; k < i; k++) {
                     realEigenValues[k] /= scale;
                     h += realEigenValues[k] * realEigenValues[k];
@@ -208,7 +221,6 @@ public class EigenSystem {
                 }
 
                 // Apply similarity transformation to remaining columns.
-
                 for (int j = 0; j < i; j++) {
                     f = realEigenValues[j];
                     eigenVectors[j][i] = f;
@@ -275,15 +287,13 @@ public class EigenSystem {
         imaginaryEigenValues[0] = 0;
     }
 
-    // Symmetric tridiagonal QL algorithm.
-
+    /**
+     * <code>tridiagonalQL</code> reduces the tridiagonal matrix to a diagonal
+     * one using the QL algorithm.
+     *
+     */
     public void tridiagonalQL() {
-
-        //  This is derived from the Algol procedures tql2, by
-        //  Bowdler, Martin, Reinsch, and Wilkinson, Handbook for
-        //  Auto. Comp., Vol.ii-Linear Algebra, and the corresponding
-        //  Fortran subroutine in EISPACK.
-
+        
         for (int i = 1; i < size; i++) {
             imaginaryEigenValues[i - 1] = imaginaryEigenValues[i];
         }
@@ -403,112 +413,69 @@ public class EigenSystem {
         }
     }
 
-    // Nonsymmetric reduction to Hessenberg form.
-
-    private void orthes() {
-        float[] ort = new float[size];
-
-        //  This is derived from the Algol procedures orthes and ortran,
-        //  by Martin and Wilkinson, Handbook for Auto. Comp.,
-        //  Vol.ii-Linear Algebra, and the corresponding
-        //  Fortran subroutines in EISPACK.
-
-        int low = 0;
-        int high = size - 1;
-
-        for (int m = low + 1; m <= high - 1; m++) {
-
-            // Scale column.
-
-            float scale = 0;
-            for (int i = m; i <= high; i++) {
-                scale = scale + Math.abs(hessenberg[i][m - 1]);
-            }
-            if (scale != 0) {
-
-                // Compute Householder transformation.
-
-                float h = 0;
-                for (int i = high; i >= m; i--) {
-                    ort[i] = hessenberg[i][m - 1] / scale;
-                    h += ort[i] * ort[i];
-                }
-                float g = (float) Math.sqrt(h);
-                if (ort[m] > 0) {
-                    g = -g;
-                }
-                h = h - ort[m] * g;
-                ort[m] = ort[m] - g;
-
-                // Apply Householder similarity transformation
-                // H = (I-u*u'/h)*H*(I-u*u')/h)
-
-                for (int j = m; j < size; j++) {
-                    float f = 0;
-                    for (int i = high; i >= m; i--) {
-                        f += ort[i] * hessenberg[i][j];
-                    }
-                    f = f / h;
-                    for (int i = m; i <= high; i++) {
-                        hessenberg[i][j] -= f * ort[i];
-                    }
-                }
-
-                for (int i = 0; i <= high; i++) {
-                    float f = 0;
-                    for (int j = high; j >= m; j--) {
-                        f += ort[j] * hessenberg[i][j];
-                    }
-                    f = f / h;
-                    for (int j = m; j <= high; j++) {
-                        hessenberg[i][j] -= f * ort[j];
-                    }
-                }
-                ort[m] = scale * ort[m];
-                hessenberg[m][m - 1] = scale * g;
-            }
-        }
-
-        // Accumulate transformations (Algol's ortran).
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                eigenVectors[i][j] = (i == j ? 1 : 0);
-            }
-        }
-
-        for (int m = high - 1; m >= low + 1; m--) {
-            if (hessenberg[m][m - 1] != 0) {
-                for (int i = m + 1; i <= high; i++) {
-                    ort[i] = hessenberg[i][m - 1];
-                }
-                for (int j = m; j <= high; j++) {
-                    float g = 0;
-                    for (int i = m; i <= high; i++) {
-                        g += ort[i] * eigenVectors[i][j];
-                    }
-                    // float division avoids possible underflow
-                    g = (g / ort[m]) / hessenberg[m][m - 1];
-                    for (int i = m; i <= high; i++) {
-                        eigenVectors[i][j] += g * ort[i];
-                    }
-                }
-            }
-        }
+    /**
+     * <code>getEigenVector</code> returns a single element of an eigenvector.
+     * The element is defined by the row and column requested.
+     * @param row the row of the eigenvectors
+     * @param col the column of the eigenvectors
+     * @return the eigenvector value.
+     */
+    public float getEigenvector(int row, int col) {
+        return eigenVectors[row][col];
+    }
+    
+    /**
+     * <code>getEigenvectors</code> returns the complete matrix of eigenvectors.
+     * 
+     * @return the complete matrix of eigenvectors.
+     */
+    public float[][] getEigenvectors() {
+        return eigenVectors;
     }
 
-    public float getEigenvector(int iRow, int iCol) {
-        return eigenVectors[iRow][iCol];
-    }
-
+    /**
+     * <code>getRealEigenvalue</code> returns the real components of the
+     * eigenvalues.
+     * @return the real components of the eigenvalues.
+     */
     public float[] getRealEigenvalue() {
         return realEigenValues;
     }
 
+    /**
+     * <code>getRealEigenvalue</code> returns a single eigenvalue defined by
+     * the parameter index.
+     * @param i the eigenvalue desired.
+     * @return the eigenvalue referenced by the parameter index.
+     */
     public float getRealEigenvalue(int i) {
         return realEigenValues[i];
     }
 
+    /**
+     * <code>getImaginaryEigenvalue</code> returns the imaginary components of 
+     * the eigenvalues.
+     * @return the imaginary components of the eigenvalues.
+     */
+    public float[] getImaginaryEigenValue() {
+        return imaginaryEigenValues;
+    }
+
+    /**
+     * <code>getImaginaryEigenvalue</code> returns a single eigenvalue defined by
+     * the parameter index.
+     * @param i the eigenvalue desired.
+     * @return the eigenvalue referenced by the parameter index.
+     */
+    public float getImaginaryEigenValue(int i) {
+        return imaginaryEigenValues[i];
+    }
+
+    /**
+     * Main method used for testing. Creates a simple matrix and calculates the
+     * eigenvalues and eigenvectors.
+     * @param args command line arguments.
+     */
     public static void main(String[] args) {
         float[][] matrix = new float[3][3];
         matrix[0][0] = 2;
