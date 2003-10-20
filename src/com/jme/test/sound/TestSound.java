12fb101123b6df1ff21362702086bda1e742303a
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2003-10-20 15:59:34 +0000

    Changed reference to sound file. File now in data directory, test reflects that.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@102 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/test/sound/TestSound.java
+++ src/com/jme/test/sound/TestSound.java
@@ -15,7 +15,7 @@ public class TestSound {
 	public static void main(String[] args) throws Exception {
 		SoundSystem system=SoundSystem.getSoundSystem("LWJGL");
 		system.createSoundSystem(1, 1);
-		system.getRenderer().loadWaveAs("test", "Battle.wav", 0);
+		system.getRenderer().loadWaveAs("test", "data/sound/Battle.wav", 0);
 		system.getRenderer().play("test");
 		Thread.sleep(5000);
 	}
