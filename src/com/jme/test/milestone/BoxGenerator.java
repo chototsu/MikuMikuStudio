541ba4263976573624f726851a7de058ededc814
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2003-11-13 21:02:56 +0000

    Test for 0.2
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@153 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/com/jme/test/milestone/BoxGenerator.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. 
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution. 
+ * 
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
+ * names of its contributors may be used to endorse or promote products derived 
+ * from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package com.jme.test.milestone;
+
+import com.jme.math.Vector3f;
+import com.jme.scene.Box;
+import com.jme.scene.Spatial;
+
+/**
+ * <code>BoxGenerator</code>
+ * @author Mark Powell
+ * @version 
+ */
+public class BoxGenerator implements Runnable {
+
+    private Vector3f min, max;
+    private TestMilestone2 game;
+    
+    public BoxGenerator(TestMilestone2 game) {
+        this.game = game;
+        min = new Vector3f(0,0,0);
+        max = new Vector3f(1,1,1);
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Runnable#run()
+     */
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep(3000);
+                Spatial s = new Box(min, max);
+                float x = (float)Math.random() * 10;
+                float y = (float)Math.random() * 10;
+                float z = (float)Math.random() * 10;
+                System.out.println("creating box at: " + x + " " + y + " " + z);
+                
+                s.setLocalTranslation(new Vector3f(x,y,z));
+                game.addSpatial(s);
+                
+            } catch (Exception e) {}
+        }
+
+    }
+
+}
