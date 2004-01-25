118a426b4e272ce9da11ef7bf0d18f4afb74d47f
Anakan <Anakan@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-01-25 18:48:17 +0000

    new sound system
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@279 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/com/jme/sound/scene/second/SphericalSound.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. 
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution. 
+ * 
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
+ * names of its contributors may be used to endorse or promote products derived 
+ * from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Created on 25 janv. 2004
+ *
+ */
+package com.jme.sound.scene.second;
+
+import com.jme.sound.second.ISoundRenderer;
+import com.jme.sound.second.ISource;
+import com.jme.sound.second.SoundAPIController;
+
+
+/**
+ * @author Arman Ozcelik
+ *
+ */
+public class SphericalSound extends SoundSpatial {
+
+	
+	private ISource source;
+	private int cullMode;
+	
+	public SphericalSound(String file){
+		source=SoundAPIController.getSoundSystem().loadSource(file);
+		cullMode=SoundSpatial.CULL_DISTANCE;			
+	}
+	
+	
+	/* (non-Javadoc)
+	 * @see com.jme.sound.scene.SoundSpatial#draw(com.jme.sound.scene.SoundRenderer)
+	 */
+	public void draw(ISoundRenderer r) {
+		r.draw(this);
+	}
+	
+	
+
+	/**
+	 * @return
+	 */
+	public ISource getSource() {
+		return source;
+	}
+
+	/**
+	 * @param source
+	 */
+	public void setSource(ISource source) {
+		this.source= source;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getCullMode() {
+		return cullMode;
+	}
+
+	/**
+	 * @param i
+	 */
+	public void setCullMode(int i) {
+		cullMode= i;
+	}
+
+}
---
+++ src/com/jme/sound/second/ISource.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. 
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution. 
+ * 
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
+ * names of its contributors may be used to endorse or promote products derived 
+ * from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Created on 22 janv. 2004
+ *
+ */
+package com.jme.sound.second;
+
+import com.jme.math.Vector3f;
+
+
+/**
+ * @author Arman Ozcelik
+ *
+ */
+public interface ISource {
+	
+	public void play();
+	
+	public void pause();
+	
+	public void stop();
+	
+	public void rewind();
+	
+	public void delete();
+	
+	public void setPitch(float pitch);
+	
+	public float getPitch();
+	
+	public void setGain(float gain);
+	
+	public float getGain();
+	
+	public void setMaxDistance(float maxDistance);
+	
+	public float getMaxDistance();
+	
+	public void setRolloffFactor(float rolloffFactor);
+	
+	public float getRolloffFactor();
+	
+	public void setReferenceDistance(float referenceDistance);
+	
+	public float getReferenceDistance();
+	
+	public void setMinGain(float minGain);
+	
+	public float getMinGain();
+	
+	public void setMaxGain(float maxGain);
+	
+	public float getMaxGain();
+	
+	public void setConeOuterGain(float coneOuterGain);
+	
+	public float getConeOuterGain();
+	
+	public void setPosition(Vector3f position);
+	
+	public void setPosition(float x, float y, float z);
+	
+	public Vector3f getPosition();
+	
+	public void setVelocity(Vector3f velocity);
+
+	public void setVelocity(float x, float y, float z);
+	
+	public Vector3f getVelocity();
+	
+	public void setDirection(Vector3f direction);
+	
+	public void setDirection(float x, float y, float z);
+	
+	public Vector3f getDirection();
+	
+	public void setSourceRelative(boolean isRelative);
+	
+	public boolean isSourceRelative();
+	
+	public void setLooping(boolean isLooping);
+	
+	public boolean getLooping();
+	
+	public int getBuffersQueued();
+	
+	public int getBuffersProcessed();
+	
+	public void setBuffer(IBuffer buffer);
+	
+	public IBuffer getBuffer();
+	
+	public void queueBuffers(IBuffer[] buffers);
+	
+	public void unqueueBuffers(IBuffer[] buffers);
+	
+	public boolean isPlaying();
+	
+}
