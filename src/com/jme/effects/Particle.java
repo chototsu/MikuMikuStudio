7e226483b802f20e5e4aa522e1b5b74946da9b75
darkprophet <darkprophet@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-02-03 22:44:13 +0000

    new particle system implementation, extending TriMesh
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@327 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/effects/Particle.java
+++ src/com/jme/effects/Particle.java
@@ -1,71 +1,77 @@
 /*
- * Created on Jan 20, 2004
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding All rights reserved.
  * 
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this 
- * list of conditions and the following disclaimer. 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
  * 
- * Redistributions in binary form must reproduce the above copyright notice, 
- * this list of conditions and the following disclaimer in the documentation 
- * and/or other materials provided with the distribution. 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * 
- * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
- * names of its contributors may be used to endorse or promote products derived 
- * from this software without specific prior written permission. 
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
+ *  
  */
 package com.jme.effects;
 
+import com.jme.math.Vector2f;
 import com.jme.math.Vector3f;
 import com.jme.renderer.ColorRGBA;
+import com.jme.scene.TriMesh;
 
 /**
+ * <code>Particle</code>
+ * 
  * @author Ahmed
+ * @version $Id: Particle.java,v 1.2 2004-02-03 22:44:13 darkprophet Exp $
  */
-public class Particle {
+public class Particle extends TriMesh {
 
-	// how much to fade by johny?
-	public float fade;
-
-	// johny son, im dying, i only have a maximum of 1.0f!!
-	public float life;
-
-	// johny dude...calm down, you can squash me with your current
-	// size
-	public Vector3f size;
-
-	// johny, please dude, dont sit on the bird shit!!
-	public Vector3f position;
-
-	// speed on the highway!!
-	public Vector3f velocity;
-
-	// ewwww, blue bird shit!
+	public float fade, life, size;
+	public Vector3f position, velocity;
 	public ColorRGBA color;
+	
+	private ColorRGBA[] cornerColors;
 
 	public Particle() {
-		// Its all coming up millhouse!!
-		// yes, 0s are coming...ruuuunnnn!!
-		position = new Vector3f(0, 0, 0);
-		fade = 0.0f;
-		life = 0.0f;
-		velocity = new Vector3f(0, 0, 0);
-		size = new Vector3f(0, 0, 0);
-		color = new ColorRGBA(0, 0, 0, 0);
+		super();
+	}
+
+	public Particle(
+		Vector3f[] vertices,
+		Vector3f[] normal,
+		ColorRGBA[] color,
+		Vector2f[] texture,
+		int[] indices) {
+		super(vertices, normal, color, texture, indices);
+		
+		cornerColors = new ColorRGBA[color.length];
+		size = fade = life = 0.0f;
+		position = velocity = new Vector3f(0, 0, 0);
+		this.color = new ColorRGBA(0, 0, 0, 0);
+	}
+
+	public void updateColor() {
+		for (int i = 0; i < cornerColors.length; i++) {
+			cornerColors[i] = this.color;
+		}
+		super.setColors(cornerColors);
 	}
 
 }
--- src/com/jme/effects/ParticleSystem.java
+++ src/com/jme/effects/ParticleSystem.java
@@ -1,128 +1,110 @@
 /*
- * Created on Jan 20, 2004
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding All rights reserved.
  * 
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this 
- * list of conditions and the following disclaimer. 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
  * 
- * Redistributions in binary form must reproduce the above copyright notice, 
- * this list of conditions and the following disclaimer in the documentation 
- * and/or other materials provided with the distribution. 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * 
- * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
- * names of its contributors may be used to endorse or promote products derived 
- * from this software without specific prior written permission. 
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
+ *  
  */
 package com.jme.effects;
 
+import com.jme.math.Vector2f;
 import com.jme.math.Vector3f;
 import com.jme.renderer.ColorRGBA;
-import com.jme.renderer.Renderer;
-import com.jme.scene.BoundingVolume;
-import com.jme.scene.Geometry;
+import com.jme.scene.Node;
 
 /**
+ * <code>ParticleSystem</code>
+ * 
  * @author Ahmed
+ * @version $Id: ParticleSystem.java,v 1.4 2004-02-03 22:44:13 darkprophet Exp $
  */
-public class ParticleSystem extends Geometry {
+public class ParticleSystem extends Node {
 
-	// the television set
+	private int numOfParticles;
+	private float startSize, endSize, fade, speed, friction;
+	private ColorRGBA startColor, endColor;
+	private Vector3f gravity, position;
+
 	private Particle[] particles;
+	private ParticleController pc;
 
-	// the remote control 
-	private ParticleController partController;
-
-	// johny? keep it steady now!
-	private float speed;
-
-	// johny? your fired! you are a disgrace!
-	// captain friction can do better!
-	private float friction;
-
-	// friction, set me the fade
-	private float fade;
-
-	// dont like your name friction, change it to johny!
-	// oh and while your at it, change all the particle colours to
-	// the start colour
-	private ColorRGBA startColor;
-	private ColorRGBA endColor;
-
-	// wow johny, its huge!
-	private Vector3f startSize;
-	private Vector3f endSize;
-
-	// where's sir isaac when you need him?
-	private Vector3f gravity;
-
-	// where are we going to place that infamouse particlesystem
-	private Vector3f position;
-
-	// do we need randomneessesss?
-	private boolean random;
-	private Vector3f randomness;
-
-	// culling stuff...yes, cull em alll...mwwaaaahahaaaaa
-	private Vector3f positionsOfParticles[];
-	
-	public ParticleSystem(int numOfParticles) {
-		speed = 0;
-		friction = 0;
-		startColor = new ColorRGBA(0, 0, 0, 0);
-		endColor = new ColorRGBA(0, 0, 0, 0);
-		startSize = new Vector3f(0, 0, 0);
-		endSize = new Vector3f(0, 0, 0);
-		gravity = new Vector3f(0, 0, 0);
-		position = new Vector3f(0, 0, 0);
-		random = false;
-		randomness = new Vector3f();
-
+	public ParticleSystem(int num) {
+		super();
+		numOfParticles = num;
 		particles = new Particle[numOfParticles];
-		positionsOfParticles = new Vector3f[numOfParticles];
+
+		startSize = endSize = 1.0f;
+		fade = speed = friction = 0.0f;
+		startColor = endColor = new ColorRGBA(0, 0, 0, 0);
+		gravity = position = new Vector3f(0, 0, 0);
+
+		Vector3f[] vertices = new Vector3f[4];
+		vertices[0] = new Vector3f(0, 0, 0);
+		vertices[1] = new Vector3f(0, 1, 0);
+		vertices[2] = new Vector3f(1, 1, 0);
+		vertices[3] = new Vector3f(1, 0, 0);
+
+		ColorRGBA[] colors = new ColorRGBA[4];
+		colors[0] = new ColorRGBA(0, 0, 0, 0);
+		colors[1] = new ColorRGBA(0, 0, 0, 0);
+		colors[2] = new ColorRGBA(0, 0, 0, 0);
+		colors[3] = new ColorRGBA(0, 0, 0, 0);
+
+		Vector2f[] tex = new Vector2f[4];
+		tex[0] = new Vector2f(0, 0);
+		tex[1] = new Vector2f(0, 1);
+		tex[2] = new Vector2f(1, 1);
+		tex[3] = new Vector2f(1, 0);
+
+		int[] indices = { 0, 1, 3, 2, 3, 1 };
 
 		for (int i = 0; i < numOfParticles; i++) {
-			particles[i]= new Particle();
-			positionsOfParticles[i] = new Vector3f();
+			particles[i] = new Particle(vertices, null, colors, tex, indices);
+			particles[i].setName("Particle " + (i + 1));
+			attachChild(particles[i]);
 		}
 	}
-
-	public void draw(Renderer r) {
-		super.draw(r);
-		r.draw(this);
-	}
-
-	public void update(float time) {
-		partController.update(time);
-	}
-
-	public void addController(ParticleController pc) {
-		partController = pc;
-	}
-
+	
+	//----
 	// Getter Methods
+	//----
+	public float getStartSize() {
+		return startSize;
+	}
+	public float getEndSize() {
+		return endSize;
+	}
+	public float getFade() {
+		return fade;
+	}
 	public float getSpeed() {
 		return speed;
 	}
 	public float getFriction() {
 		return friction;
-	}
-	public float getFade() {
-		return fade;
 	}
 	public ColorRGBA getStartColor() {
 		return startColor;
@@ -130,107 +112,51 @@ public class ParticleSystem extends Geometry {
 	public ColorRGBA getEndColor() {
 		return endColor;
 	}
-	public Vector3f getStartSize() {
-		return startSize;
-	}
-	public Vector3f getEndSize() {
-		return endSize;
-	}
 	public Vector3f getGravity() {
 		return gravity;
 	}
-	public Vector3f getPosition() {
+	public Vector3f getStartPosition() {
 		return position;
 	}
-	public Particle[] getParticles() {
-		return particles;
+	
+	//----
+	// Setter Methods
+	//----
+	public void setStartSize(float s) {
+		startSize = s;
 	}
-	public boolean getRandom() {
-		return random;
+	public void setEndSize(float s) {
+		endSize = s;
 	}
-	public Vector3f getPlane() {
-		return randomness;
+	public void setFade(float f) {
+		fade = f;
 	}
-	public Vector3f[] getParticlePosition() {
-		return positionsOfParticles;
-	}
-	public BoundingVolume getModelBound() {
-		return bound;
-	}
-
-	// setter methods
 	public void setSpeed(float s) {
 		speed = s;
 	}
 	public void setFriction(float f) {
 		friction = f;
 	}
-	public void setFade(float f) {
-		fade = f;
-	}
 	public void setStartColor(ColorRGBA c) {
-		startColor.r = c.r;
-		startColor.g = c.g;
-		startColor.b = c.b;
-		startColor.a = c.a;
+		startColor = c;
 	}
 	public void setEndColor(ColorRGBA c) {
-		endColor.r = c.r;
-		endColor.g = c.g;
-		endColor.b = c.b;
-		endColor.a = c.a;
-	}
-	public void setStartSize(Vector3f s) {
-		startSize.x = s.x;
-		startSize.y = s.y;
-		startSize.z = s.z;
-	}
-	public void setEndSize(Vector3f s) {
-		endSize.x = s.x;
-		endSize.y = s.y;
-		endSize.z = s.z;
+		endColor = c;
 	}
 	public void setGravity(Vector3f g) {
-		gravity.x = g.x;
-		gravity.y = g.y;
-		gravity.z = g.z;
+		gravity = g;
 	}
-	public void setPosition(Vector3f p) {
-		position.x = p.x;
-		position.y = p.y;
-		position.z = p.z;
+	public void setStartPosition(Vector3f p) {
+		position = p;
 	}
-	public void setPlane(boolean r, Vector3f i) {
-		random = r;
-		randomness.x = i.x + 0.0000001f;
-		randomness.y = i.y + 0.0000001f;
-		randomness.z = i.z + 0.0000001f;
+	
+	//----
+	// misc methods
+	//----
+	public void update(float t) {
+		pc.update(t);
 	}
-	public void setParticlePosition(Vector3f p, int i) {
-		positionsOfParticles[i] = p;
-	}
-
-	public void setModelBound(BoundingVolume b) {
-		bound = b;
-	}
-    
-    /**
-     * <code>updateWorldBound</code> updates the bounding volume that contains
-     * this geometry. The location of the geometry is based on the location
-     * of all this node's parents. 
-     * @see com.jme.scene.Spatial#updateWorldBound()
-     */
-    public void updateWorldBound() {
-        if(bound != null) {
-            updateModelBound();
-            worldBound =
-                bound.transform(worldRotation, worldTranslation, worldScale);
-        }  
-    } 
-
-	public void updateModelBound() {
-        if(bound != null) {
-		  bound.computeFromPoints(positionsOfParticles);
-        }
+	public void addController(ParticleController p) {
+		pc = p;
 	}
 }
