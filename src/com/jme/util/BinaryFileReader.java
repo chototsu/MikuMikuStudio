c82a1de995597c16f6443425bc82c290d31d21bc
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-02-06 20:20:57 +0000

    documentation completed.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@337 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/util/BinaryFileReader.java
+++ src/com/jme/util/BinaryFileReader.java
@@ -39,15 +39,26 @@ import java.io.IOException;
 import com.jme.system.JmeException;
 
 /**
- * <code>BinaryFileReader</code>
+ * <code>BinaryFileReader</code> provides a convienience class that wraps the
+ * DataInputStream object for easier reading. Rather than having to deal with
+ * a binary file one byte at a time, this class will handle reading: byte,
+ * short, int, float and string. The file is read at the start, the file
+ * stored as an array of bytes. An index into the array will keep track of
+ * the reading point. The index can be manually adjusted via the 
+ * <code>setOffset</code> method.
  * @author Mark Powell
- * @version $Id: BinaryFileReader.java,v 1.1 2004-02-05 22:41:37 mojomonkey Exp $
+ * @version $Id: BinaryFileReader.java,v 1.2 2004-02-06 20:20:57 mojomonkey Exp $
  */
 public class BinaryFileReader {
 	private int fileSize;
 	private byte[] fileContents;
-	private int m_FilePointer = 0;
+	private int fileIndex = 0;
 	
+	/**
+	 * Constructor instantiates a new <code>BinaryFileReader</code> object, 
+	 * loading the provided file and reading the data into a byte array.
+	 * @param f the file to read.
+	 */
 	public BinaryFileReader(File f) {
 		try {
 			FileInputStream is = new FileInputStream(f);
@@ -66,51 +77,93 @@ public class BinaryFileReader {
 		}
 	}
 	
+	/**
+	 * 
+	 * <code>readByte</code> reads a single byte from the array and 
+	 * returns this. The file index is then increased by one.
+	 * @return the byte at the current index.
+	 */
 	public int readByte() {
-		int b1 = (fileContents[m_FilePointer] & 0xFF);
-		m_FilePointer += 1;
+		int b1 = (fileContents[fileIndex] & 0xFF);
+		fileIndex += 1;
 		return (b1);
 	}
 
+	/**
+	 * 
+	 * <code>readShort</code> reads two bytes from the array, generating
+	 * a short. The file index is then increased by two. The short is then
+	 * inserted into an integer for convienience.
+	 * @return the short at the current index.
+	 */
 	public int readShort() {
-		int s1 = (fileContents[m_FilePointer] & 0xFF);
-		int s2 = (fileContents[m_FilePointer + 1] & 0xFF) << 8;
-		m_FilePointer += 2;
+		int s1 = (fileContents[fileIndex] & 0xFF);
+		int s2 = (fileContents[fileIndex + 1] & 0xFF) << 8;
+		fileIndex += 2;
 		return (s1 | s2);
 	}
 
+	/**
+	 * 
+	 * <code>readInt</code> reads four bytes from the array, generating
+	 * an int. The file index is then increased by four.
+	 * @return the int at the currrent index.
+	 */
 	public int readInt() {
-		int i1 = (fileContents[m_FilePointer] & 0xFF);
-		int i2 = (fileContents[m_FilePointer + 1] & 0xFF) << 8;
-		int i3 = (fileContents[m_FilePointer + 2] & 0xFF) << 16;
-		int i4 = (fileContents[m_FilePointer + 3] & 0xFF) << 24;
-		m_FilePointer += 4;
+		int i1 = (fileContents[fileIndex] & 0xFF);
+		int i2 = (fileContents[fileIndex + 1] & 0xFF) << 8;
+		int i3 = (fileContents[fileIndex + 2] & 0xFF) << 16;
+		int i4 = (fileContents[fileIndex + 3] & 0xFF) << 24;
+		fileIndex += 4;
 		return (i1 | i2 | i3 | i4);
 	}
 
+	/**
+	 * 
+	 * <code>readFloat</code> reads four bytes from the array, generating
+	 * a float. The file index is then increased by four.
+	 * @return the float at the current index.
+	 */
 	public float readFloat() {
 		return Float.intBitsToFloat(readInt());
 	}
 
+	/**
+	 * 
+	 * <code>readString</code> reads a specified number of bytes to 
+	 * form a string. The length of the string (number of characters)
+	 * is required to notify when reading should stop. The index is
+	 * increased the number of characters read.
+	 * @param size the length of the string to read.
+	 * @return the string read.
+	 */
 	public String readString(int size) {
 		//Look for zero terminated string from byte array
-		for (int i = m_FilePointer; i < m_FilePointer + size; i++) {
+		for (int i = fileIndex; i < fileIndex + size; i++) {
 			if (fileContents[i] == (byte) 0) {
 				String s = new String(
 						fileContents,
-						m_FilePointer,
-						i - m_FilePointer);
-				m_FilePointer += size;
+						fileIndex,
+						i - fileIndex);
+				fileIndex += size;
 				return s;
 			}
 		}
 		
-		String s = new String(fileContents, m_FilePointer, size);
-		m_FilePointer += size;
+		String s = new String(fileContents, fileIndex, size);
+		fileIndex += size;
 		return s;
 	}
 	
+	/**
+	 * 
+	 * <code>setOffset</code> sets the index of the file data.
+	 * @param offset the new index of the file pointer.
+	 */
 	public void setOffset(int offset) {
-		m_FilePointer = offset;
+		if(offset < 0 ||offset > fileContents.length) {
+			throw new JmeException("Illegal offset value. " + offset );
+		}
+		fileIndex = offset;
 	}
 }
