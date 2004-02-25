72efe9d38f92a2ec22d19d8501d1516d6ebeb8d1
renanse <renanse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-02-25 22:43:54 +0000

    efficiency boost added
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@459 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/scene/Node.java
+++ src/com/jme/scene/Node.java
@@ -2,41 +2,41 @@
  * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- * Redistributions of source code must retain the above copyright notice, this 
- * list of conditions and the following disclaimer. 
- * 
- * Redistributions in binary form must reproduce the above copyright notice, 
- * this list of conditions and the following disclaimer in the documentation 
- * and/or other materials provided with the distribution. 
- * 
- * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
- * names of its contributors may be used to endorse or promote products derived 
- * from this software without specific prior written permission. 
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
  */
- 
+
  /*
   * EDIT:  02/05/2004 - Added detachAllChildren method. GOP
-  * EDIT:  02/05/2004 - Added check for null on first child before setting 
+  * EDIT:  02/05/2004 - Added check for null on first child before setting
   *                     firstBound to true in updateWorldBound method.  GOP
   * EDIT: 02/14/2004 - Made children protected rather than private. MAP
   */
-  
+
 package com.jme.scene;
 
 import java.io.Serializable;
@@ -48,12 +48,12 @@ import com.jme.util.LoggingSystem;
 
 /**
  * <code>Node</code> defines an internal node of a scene graph. The internal
- * node maintains a collection of children and handles merging said children 
- * into a single bound to allow for very fast culling of multiple nodes. 
+ * node maintains a collection of children and handles merging said children
+ * into a single bound to allow for very fast culling of multiple nodes.
  * Node allows for any number of children to be attached.
  * @author Mark Powell
  * @author Gregg Patton
- * @version $Id: Node.java,v 1.10 2004-02-20 20:17:49 mojomonkey Exp $
+ * @version $Id: Node.java,v 1.11 2004-02-25 22:43:54 renanse Exp $
  */
 public class Node extends Spatial implements Serializable {
     //List to hold the children.
@@ -70,19 +70,19 @@ public class Node extends Spatial implements Serializable {
         children = new ArrayList();
         LoggingSystem.getLogger().log(Level.INFO, "Node created.");
     }
-    
+
     /**
-     * 
-     * <code>getQuantity</code> returns the number of children this node 
+     *
+     * <code>getQuantity</code> returns the number of children this node
      * maintains.
      * @return the number of children this node maintains.
      */
     public int getQuantity() {
         return children.size();
     }
-    
+
     /**
-     * 
+     *
      * <code>attachChild</code> attaches a child to this node. This node
      * becomes the child's parent. The current number of children maintained
      * is returned.
@@ -97,12 +97,13 @@ public class Node extends Spatial implements Serializable {
             child.setParent(this);
             children.add(child);
         }
-        LoggingSystem.getLogger().log(Level.INFO, "Child (" + child.getName() + ") attached to this" +            " node (" + name + ")");
+        LoggingSystem.getLogger().log(Level.INFO, "Child (" + child.getName() + ") attached to this" +
+            " node (" + name + ")");
         return children.size();
     }
-    
+
     /**
-     * 
+     *
      * <code>detachChild</code> removes a given child from the node's list.
      * This child will no longe be maintained.
      * @param child the child to remove.
@@ -116,10 +117,10 @@ public class Node extends Spatial implements Serializable {
         }
         return index;
     }
-    
+
     /**
-     * 
-     * <code>detachChildAt</code> removes a child at a given index. That 
+     *
+     * <code>detachChildAt</code> removes a child at a given index. That
      * child is returned for saving purposes.
      * @param index the index of the child to be removed.
      * @return the child at the supplied index.
@@ -128,18 +129,18 @@ public class Node extends Spatial implements Serializable {
         LoggingSystem.getLogger().log(Level.INFO, "Child removed.");
         return (Spatial)children.remove(index);
     }
-    
+
     /**
-     * 
-     * <code>detachAllChildren</code> removes all children attached to this node. 
+     *
+     * <code>detachAllChildren</code> removes all children attached to this node.
      */
     public void detachAllChildren() {
         LoggingSystem.getLogger().log(Level.INFO, "All children removed.");
         children.clear();
     }
-    
+
     /**
-     * 
+     *
      * <code>setChild</code> places a child at a given index. If a child is
      * already set to that index the old child is returned.
      * @param i the index to set the child to.
@@ -153,9 +154,9 @@ public class Node extends Spatial implements Serializable {
                     " node");
         return old;
     }
-    
+
     /**
-     * 
+     *
      * <code>getChild</code> returns a child at a given index.
      * @param i the index to retrieve the child from.
      * @return the child at a specified index.
@@ -172,14 +173,14 @@ public class Node extends Spatial implements Serializable {
     public void updateWorldData(float time) {
         super.updateWorldData(time);
 
-        for (int i = 0; i < children.size(); i++) {
+        for (int i = 0, cSize = children.size(); i < cSize; i++) {
             Spatial child = (Spatial) children.get(i);
             if (child != null) {
                 child.updateGeometricState(time, false);
             }
         }
     }
-   
+
     /**
      * <code>draw</code> calls the onDraw method for each child maintained
      * by this node.
@@ -187,7 +188,7 @@ public class Node extends Spatial implements Serializable {
      * @param r the renderer to draw to.
      */
     public void draw(Renderer r) {
-        for (int i = 0; i < children.size(); i++) {
+        for (int i = 0, cSize = children.size(); i < cSize ; i++) {
             Spatial child = (Spatial) children.get(i);
             if (child != null)
                 child.onDraw(r);
@@ -200,15 +201,15 @@ public class Node extends Spatial implements Serializable {
      * @see com.jme.scene.Spatial#updateWorldBound()
      */
     public void updateWorldBound() {
-    	
+
         boolean foundFirstBound = false;
-        for (int i = 0; i < children.size(); i++) {
+        for (int i = 0, cSize = children.size(); i < cSize ; i++) {
             Spatial child = (Spatial) children.get(i);
             if (child != null) {
                 if (foundFirstBound) {
-                	// merge current world bound with child world bound
+                    // merge current world bound with child world bound
                     worldBound = worldBound.merge(child.getWorldBound());
-            
+
                 } else {
                     // set world bound to first non-null child world bound
                     worldBound = child.getWorldBound();
