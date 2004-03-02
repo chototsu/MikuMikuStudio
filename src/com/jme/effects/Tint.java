6ba517bf1719d07796485bf80b6de6e669d136e8
darkprophet <darkprophet@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-03-02 19:40:32 +0000

    initial commit of Tint
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@507 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/com/jme/effects/Tint.java
@@ -0,0 +1,110 @@
+/* 
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding 
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. 
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution. 
+ * 
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
+ * names of its contributors may be used to endorse or promote products derived 
+ * from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE. 
+ * 
+ */
+package com.jme.effects;
+
+import com.jme.math.Vector2f;
+import com.jme.renderer.ColorRGBA;
+import com.jme.renderer.Renderer;
+import com.jme.scene.Geometry;
+
+/**
+ * <code>Tint</code>
+ * @author Ahmed
+ * @version $Id: Tint.java,v 1.1 2004-03-02 19:40:32 darkprophet Exp $
+ */
+public class Tint extends Geometry {
+	
+	private ColorRGBA tintColor;
+	private ColorRGBA textureColor;
+	private boolean useTexture;
+	private Vector2f location, size;
+	
+	public Tint(String name, ColorRGBA c) {
+		super(name);
+		tintColor = c;
+		useTexture = false;
+	}
+	public Tint(String name, ColorRGBA c, ColorRGBA t) {
+		super(name);
+		tintColor = new ColorRGBA(0, 0, 0, 0);
+		tintColor.r = c.r;
+		tintColor.g = c.g;
+		tintColor.b = c.b;
+		tintColor.a = c.a;
+		textureColor = t;
+		useTexture = true;
+	}
+	
+	public Tint(String name) {
+		super(name);
+		tintColor = new ColorRGBA(0, 0, 0, 0);
+		textureColor = new ColorRGBA(0, 0, 0, 0);
+		useTexture = false;
+	}
+	
+	public void draw(Renderer r) {
+		super.draw(r);
+		r.draw(this);
+	}
+	
+	public ColorRGBA getTintColor() {
+		return tintColor;
+	}
+	public ColorRGBA getTextureColor() {
+		return textureColor;
+	}
+	public Vector2f getTextureLocation() {
+		return location;
+	}
+	public Vector2f getTextureSize() {
+		return size;
+	}
+	public boolean useTexture() {
+		return useTexture;
+	}
+	
+	public void setTintColor(ColorRGBA c) {
+		tintColor = c;
+	}
+	public void setTextureColor(ColorRGBA t) {
+		textureColor = t;
+		useTexture = true;
+	}
+	public void setTextureLocation(Vector2f loc) {
+		location = loc;
+		useTexture = true;
+	}
+	public void setTextureSize(Vector2f s) {
+		this.size = s;
+		useTexture = true;
+	}
+}
