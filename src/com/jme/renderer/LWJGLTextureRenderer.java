8b7f87ab153804b80a248f7f57753f4aa5c7f7d7
renanse <renanse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-03-04 19:37:33 +0000

    first version
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@539 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/com/jme/renderer/LWJGLTextureRenderer.java
@@ -0,0 +1,268 @@
+/*
+ * Copyright (c) 2004, jMonkeyEngine - Mojo Monkey Coding
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package com.jme.renderer;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+import java.util.logging.Level;
+
+import javax.imageio.ImageIO;
+
+import org.lwjgl.opengl.*;
+import org.lwjgl.opengl.GLU;
+import org.lwjgl.opengl.Window;
+
+import com.jme.curve.Curve;
+import com.jme.effects.Tint;
+import com.jme.input.Mouse;
+import com.jme.math.Quaternion;
+import com.jme.math.Vector2f;
+import com.jme.math.Vector3f;
+import com.jme.scene.Clone;
+import com.jme.scene.CloneNode;
+import com.jme.scene.Line;
+import com.jme.scene.Point;
+import com.jme.scene.Spatial;
+import com.jme.scene.Text;
+import com.jme.scene.TriMesh;
+import com.jme.scene.state.AlphaState;
+import com.jme.scene.state.CullState;
+import com.jme.scene.state.DitherState;
+import com.jme.scene.state.FogState;
+import com.jme.scene.state.LWJGLAlphaState;
+import com.jme.scene.state.LWJGLCullState;
+import com.jme.scene.state.LWJGLDitherState;
+import com.jme.scene.state.LWJGLFogState;
+import com.jme.scene.state.LWJGLLightState;
+import com.jme.scene.state.LWJGLMaterialState;
+import com.jme.scene.state.LWJGLShadeState;
+import com.jme.scene.state.LWJGLTextureState;
+import com.jme.scene.state.LWJGLWireframeState;
+import com.jme.scene.state.LWJGLZBufferState;
+import com.jme.scene.state.LightState;
+import com.jme.scene.state.MaterialState;
+import com.jme.scene.state.ShadeState;
+import com.jme.scene.state.TextureState;
+import com.jme.scene.state.WireframeState;
+import com.jme.scene.state.ZBufferState;
+import com.jme.system.JmeException;
+import com.jme.util.LoggingSystem;
+import com.jme.widget.WidgetRenderer;
+
+import com.jme.image.Texture;
+import com.jme.scene.Spatial;
+
+/**
+ * @author Joshua Slack
+ * @version $Id: LWJGLTextureRenderer.java,v 1.1 2004-03-04 19:37:32 renanse Exp $
+ */
+public class LWJGLTextureRenderer implements TextureRenderer {
+
+    private LWJGLCamera camera;
+    private ColorRGBA backgroundColor = new ColorRGBA(1,1,1,1);
+
+    private int PBUFFER_WIDTH = 512;
+    private int PBUFFER_HEIGHT = 512;
+
+    /** Pbuffer instance */
+    private Pbuffer pbuffer;
+
+    private LWJGLRenderer parentRenderer;
+
+    public LWJGLTextureRenderer(LWJGLRenderer parentRenderer) {
+        if (!isSupported()) {
+            System.err.println("No Pbuffer support!");
+            System.exit(1);  // Clean this up.
+        } else
+            System.err.println("Pbuffer support detected");
+
+        this.parentRenderer = parentRenderer;
+        initPbuffer();
+    }
+
+    public static boolean isSupported() { // Perhaps we should move this to Renderer?
+        if ((Pbuffer.getPbufferCaps() & Pbuffer.PBUFFER_SUPPORTED) == 0)
+            return false;
+        else return true;
+    }
+
+    /**
+     * <code>getCamera</code> retrieves the camera this renderer is using.
+     * @return the camera this renderer is using.
+     */
+    public Camera getCamera() {
+        return camera;
+    }
+
+    /**
+     * <code>setBackgroundColor</code> sets the OpenGL clear color to the
+     * color specified.
+     *
+     * @see com.jme.renderer.TextureRenderer#setBackgroundColor(com.jme.renderer.ColorRGBA)
+     * @param c
+     *            the color to set the background color to.
+     */
+    public void setBackgroundColor(ColorRGBA c) {
+        //if color is null set background to white.
+        if (c == null) {
+            backgroundColor.a = 1.0f;
+            backgroundColor.b = 1.0f;
+            backgroundColor.g = 1.0f;
+            backgroundColor.r = 1.0f;
+        } else {
+            backgroundColor = c;
+        }
+
+        activate();
+        GL.glClearColor(
+            backgroundColor.r,
+            backgroundColor.g,
+            backgroundColor.b,
+            backgroundColor.a);
+        deactivate();
+    }
+
+    /**
+     * <code>getBackgroundColor</code> retrieves the clear color of the
+     * current OpenGL context.
+     *
+     * @see com.jme.renderer.Renderer#getBackgroundColor()
+     * @return the current clear color.
+     */
+    public ColorRGBA getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    /**
+     * <code>setupTexture</code> generates a new Texture object for use with
+     * TextureRenderer.  Generates a valid gl texture id for this texture.
+     * @return the new Texture
+     */
+    public Texture setupTexture() {
+        IntBuffer ibuf =
+            ByteBuffer
+                .allocateDirect(4)
+                .order(ByteOrder.nativeOrder())
+                .asIntBuffer();
+
+        //Create the texture
+        GL.glGenTextures(ibuf);
+        int glTextureID = ibuf.get(0);
+
+        return setupTexture(glTextureID);
+    }
+
+    /**
+     * <code>setupTexture</code> retrieves the color used for the
+     * window background.
+     * @param glTextureID a valid gl texture id to use
+     * @return the new Texture
+     */
+    public Texture setupTexture(int glTextureID) {
+        GL.glBindTexture(GL.GL_TEXTURE_2D, glTextureID);
+        GL.glCopyTexImage2D(GL.GL_TEXTURE_2D, 0, GL.GL_RGBA, 0, 0, 512, 512, 0);
+
+        Texture rVal = new Texture();
+        rVal.setTextureId(glTextureID);
+        rVal.setBlendColor(new ColorRGBA(1, 1, 1, 1f));
+        return rVal;
+    }
+
+    /**
+     * <code>render</code> renders a scene. As it recieves a base class of
+     * <code>Spatial</code> the renderer hands off management of the
+     * scene to spatial for it to determine when a <code>Geometry</code>
+     * leaf is reached.  All of this is done in the context of the underlying
+     * texture buffer.
+     * @param spat the scene to render.
+     * @param tex the Texture to render it to.
+     */
+    public void render(Spatial spat, Texture tex) {
+        try {
+            if (pbuffer.isBufferLost()) {
+                System.out.println("Buffer contents lost - will recreate the buffer");
+                deactivate();
+                pbuffer.destroy();
+                initPbuffer();
+            }
+
+            activate();
+            parentRenderer.clearBuffers();
+            parentRenderer.draw(spat);
+            GL.glBindTexture(GL.GL_TEXTURE_2D, tex.getTextureId());
+            GL.glCopyTexSubImage2D(GL.GL_TEXTURE_2D, 0, 0, 0, 0, 0, PBUFFER_WIDTH, PBUFFER_HEIGHT);
+            pbuffer.releaseContext();
+        } catch (Exception e) {
+            LoggingSystem.getLogger().throwing(this.getClass().toString(), "render(Spatial, Texture)", e);
+        }
+    }
+
+
+    private void initPbuffer() {
+        if (camera == null) initCamera();
+        try {
+            pbuffer = new Pbuffer(PBUFFER_WIDTH, PBUFFER_HEIGHT, 32, 0, 8, 0);
+            activate();
+
+            GL.glClearColor(backgroundColor.r, backgroundColor.g, backgroundColor.b, backgroundColor.a);
+            camera.update();
+
+            deactivate();
+            parentRenderer.getCamera().update();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void activate() {
+        pbuffer.makeCurrent();
+    }
+
+    private void deactivate() {
+        Pbuffer.releaseContext();
+    }
+
+    private void initCamera() {
+        camera = new LWJGLCamera(PBUFFER_WIDTH, PBUFFER_HEIGHT);
+        camera.setFrustum(1.0f, 1000.0f, -0.55f, 0.55f, 0.4125f, -0.4125f);
+        Vector3f loc = new Vector3f(0.0f, 0.0f, 75.0f);
+        Vector3f left = new Vector3f(-1.0f, 0.0f, 0.0f);
+        Vector3f up = new Vector3f(0.0f, 1.0f, 0.0f);
+        Vector3f dir = new Vector3f(0.0f, 0f, -1.0f);
+        camera.setFrame(loc, left, up, dir);
+    }
+}
---
+++ src/com/jme/renderer/TextureRenderer.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2004, jMonkeyEngine - Mojo Monkey Coding
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package com.jme.renderer;
+
+import com.jme.image.Texture;
+import com.jme.scene.Spatial;
+
+/**
+ * <code>TextureRenderer</code> defines an interface that handles rendering
+ * a scene to a buffer and copying it to a texture. Creation of this object is typically
+ * handled via a call to a <code>DisplaySystem</code> subclass.
+ *
+ * Example Usage:<br>
+ * NOTE: This example uses the <code>DisplaySystem</code> class to obtain the
+ * <code>TextureRenderer</code>.
+ *
+ * <code>
+ * </code>
+ * @see com.jme.system.DisplaySystem
+ * @author Joshua Slack
+ * @version $Id: TextureRenderer.java,v 1.1 2004-03-04 19:37:33 renanse Exp $
+ */
+public interface TextureRenderer {
+
+    /**
+     * <code>getCamera</code> retrieves the camera this renderer is using.
+     * @return the camera this renderer is using.
+     */
+    public Camera getCamera();
+
+    /**
+     * <code>render</code> renders a scene. As it recieves a base class of
+     * <code>Spatial</code> the renderer hands off management of the
+     * scene to spatial for it to determine when a <code>Geometry</code>
+     * leaf is reached.  All of this is done in the context of the underlying
+     * texture buffer.
+     * @param spat the scene to render.
+     * @param tex the Texture to render it to.
+     */
+    public void render(Spatial spat, Texture tex);
+
+
+    /**
+     * <code>setBackgroundColor</code> sets the color of window. This color
+     * will be shown for any pixel that is not set via typical rendering
+     * operations.
+     * @param c the color to set the background to.
+     */
+    public void setBackgroundColor(ColorRGBA c);
+
+    /**
+     * <code>getBackgroundColor</code> retrieves the color used for the
+     * window background.
+     * @return the background color that is currently set to the background.
+     */
+    public ColorRGBA getBackgroundColor();
+
+    /**
+     * <code>setupTexture</code> generates a new Texture object for use with
+     * TextureRenderer.  Generates a valid gl texture id for this texture.
+     * @return the new Texture
+     */
+    public Texture setupTexture();
+
+    /**
+     * <code>setupTexture</code> retrieves the color used for the
+     * window background.
+     * @param glTextureID a valid gl texture id to use
+     * @return the new Texture
+     */
+    public Texture setupTexture(int glTextureID);
+
+}
