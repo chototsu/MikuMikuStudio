d2777169accfa9d77c7bd2d556e8847fecaef041
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-03-05 16:50:19 +0000

    Initial DLOD check-in
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@554 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/com/jme/scene/DistanceSwitchModel.java
@@ -0,0 +1,106 @@
+/* 
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding 
+ * All rights reserved. 
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. 
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution. 
+ * 
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
+ * names of its contributors may be used to endorse or promote products derived 
+ * from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE. 
+ * 
+ */
+package com.jme.scene;
+
+import com.jme.math.Vector3f;
+
+/**
+ * <code>DistanceSwitchModel</code>
+ * @author Mark Powell
+ * @version $Id: DistanceSwitchModel.java,v 1.1 2004-03-05 16:50:18 mojomonkey Exp $
+ */
+public class DistanceSwitchModel implements SwitchModel {
+	private float[] modelMin;
+	private float[] modelMax;
+	private float[] worldMin;
+	private float[] worldMax;
+	
+	private int numChildren;
+	
+	private float worldScaleSquared;
+	private Vector3f diff;
+	
+	public DistanceSwitchModel(int numChildren) {
+		this.numChildren = numChildren;
+		modelMin = new float[numChildren];
+		modelMax = new float[numChildren];
+		worldMin = new float[numChildren];
+		worldMax = new float[numChildren];
+	}
+	
+	public void setModelMinSqrDistance (int index, float minSqrDist) {
+		
+		modelMin[index] = minSqrDist;
+	}
+	
+	public void setModelMaxSqrDistance (int index, float maxSqrDist) {
+		modelMax[index] = maxSqrDist;
+	}
+	
+	public void setModelSqrDistance (int index, float minSqrDist, float maxSqrDist) {
+		
+		modelMin[index] = minSqrDist;;
+		modelMax[index] = maxSqrDist;
+	}
+	
+	public void set(float value) {
+		worldScaleSquared = value;
+	}
+	
+	public void set(Vector3f value) {
+		diff = value;
+	}
+	
+	public int getSwitchChild(){
+		for ( int i = 0; i < numChildren; i++) {
+			float value = modelMin[i];
+			worldMin[i] = worldScaleSquared*value*value;
+
+			value = modelMax[i];
+			worldMax[i] = worldScaleSquared*value*value;
+		}
+
+		// select the LOD child
+		if ( numChildren > 0 ) {
+			float fSqrDist = diff.lengthSquared();
+
+			for ( int i = 0; i < numChildren; i++) {
+				if ( worldMin[i] <= fSqrDist
+						&&   fSqrDist < worldMax[i] ) {
+					return i;
+				}
+			}
+		}
+		
+		return SwitchNode.SN_INVALID_CHILD;
+	}
+}
