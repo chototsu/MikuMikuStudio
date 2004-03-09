0dc6e98e1c3570fbb244f1b5eb91a56d40b71121
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-03-09 01:09:21 +0000

    test scene graph
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@590 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/jmetest/renderer/TestScenegraph.java
@@ -0,0 +1,352 @@
+/*
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package jmetest.renderer;
+
+import com.jme.app.SimpleGame;
+import com.jme.image.Texture;
+import com.jme.input.FirstPersonController;
+import com.jme.input.InputController;
+import com.jme.input.KeyBindingManager;
+import com.jme.input.KeyInput;
+import com.jme.input.NodeController;
+import com.jme.math.Vector3f;
+import com.jme.renderer.Camera;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.Box;
+import com.jme.scene.Node;
+import com.jme.scene.Text;
+import com.jme.scene.state.AlphaState;
+import com.jme.scene.state.CullState;
+import com.jme.scene.state.TextureState;
+import com.jme.scene.state.ZBufferState;
+import com.jme.system.DisplaySystem;
+import com.jme.system.JmeException;
+import com.jme.util.TextureManager;
+import com.jme.util.Timer;
+
+/**
+ * <code>TestRenderToTexture</code>
+ * @author Joshua Slack
+ */
+public class TestScenegraph extends SimpleGame {
+	private Camera cam;
+	private Node root, scene;
+	private NodeController input, nc1, nc2, nc3, nc4, nc5, nc6;
+	private Thread thread;
+	private Timer timer;
+	private Box box1, box2, box3, box4, box5, box6;
+	private Node node1, node2, node3, node4, node5, node6;
+	private Text text;
+
+	/**
+	 * Entry point for the test,
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestScenegraph app = new TestScenegraph();
+		app.setDialogBehaviour(ALWAYS_SHOW_PROPS_DIALOG);
+		app.start();
+	}
+
+	/**
+	 * Not used in this test.
+	 * @see com.jme.app.SimpleGame#update()
+	 */
+	protected void update(float interpolation) {
+		timer.update();
+		input.update(timer.getTimePerFrame());
+		scene.updateGeometricState(0.0f, true);
+	}
+
+	/**
+	 * clears the buffers and then draws the TriMesh.
+	 * @see com.jme.app.SimpleGame#render()
+	 */
+	protected void render(float interpolation) {
+		display.getRenderer().clearBuffers();
+		display.getRenderer().draw(root);
+	}
+
+	/**
+	 * creates the displays and sets up the viewport.
+	 * @see com.jme.app.SimpleGame#initSystem()
+	 */
+	protected void initSystem() {
+		try {
+			display = DisplaySystem.getDisplaySystem(properties.getRenderer());
+			display.createWindow(
+				properties.getWidth(),
+				properties.getHeight(),
+				properties.getDepth(),
+				properties.getFreq(),
+				properties.getFullscreen());
+			cam =
+				display.getRenderer().getCamera(
+					properties.getWidth(),
+					properties.getHeight());
+
+		} catch (JmeException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		ColorRGBA blackColor = new ColorRGBA(0, 0, 0, 1);
+		display.getRenderer().setBackgroundColor(blackColor);
+
+		// setup our camera
+		cam.setFrustum(1.0f, 1000.0f, -0.55f, 0.55f, 0.4125f, -0.4125f);
+		Vector3f loc = new Vector3f(0.0f, 0.0f, -100.0f);
+		Vector3f left = new Vector3f(1.0f, 0.0f, 0.0f);
+		Vector3f up = new Vector3f(0.0f, 1.0f, 0.0f);
+		Vector3f dir = new Vector3f(0.0f, 0f, 1.0f);
+		cam.setFrame(loc, left, up, dir);
+		display.getRenderer().setCamera(cam);
+
+		// Setup the input controller and timer
+		//input = new FirstPersonController(this, cam, "LWJGL");
+		//input.setKeySpeed(10f);
+		//input.setMouseSpeed(1f);
+		timer = Timer.getTimer("LWJGL");
+
+		display.setTitle("Test Scene Graph");
+		display.getRenderer().enableStatistics(true);
+	}
+
+	/**
+	 * builds the trimesh.
+	 * @see com.jme.app.SimpleGame#initGame()
+	 */
+	protected void initGame() {
+		Vector3f min = new Vector3f(-5, -5, -5);
+		Vector3f max = new Vector3f(5, 5, 5);
+		AlphaState as1 = display.getRenderer().getAlphaState();
+		as1.setBlendEnabled(true);
+		as1.setSrcFunction(AlphaState.SB_SRC_ALPHA);
+		as1.setDstFunction(AlphaState.DB_ONE);
+		as1.setTestEnabled(true);
+		as1.setTestFunction(AlphaState.TF_GREATER);
+		as1.setEnabled(true);
+		TextureState font = display.getRenderer().getTextureState();
+		font.setTexture(
+			TextureManager.loadTexture(
+				TestScenegraph.class.getClassLoader().getResource(
+					"jmetest/data/font/font.png"),
+				Texture.MM_LINEAR,
+				Texture.FM_LINEAR,
+				true));
+		font.setEnabled(true);
+		text = new Text("Selected Node", "Selected Node: Node 1");
+		text.setRenderState(font);
+		text.setRenderState(as1);
+
+		scene = new Node("3D Scene Node");
+		root = new Node("Root Scene Node");
+		root.attachChild(text);
+
+		CullState cs = display.getRenderer().getCullState();
+		cs.setCullMode(CullState.CS_BACK);
+		cs.setEnabled(true);
+		scene.setRenderState(cs);
+
+		// Setup our params for the depth buffer
+		ZBufferState buf = display.getRenderer().getZBufferState();
+		buf.setEnabled(true);
+		buf.setFunction(ZBufferState.CF_LEQUAL);
+
+		scene.setRenderState(buf);
+
+		node1 = new Node("Node 1");
+		box1 = new Box("Box 1", min, max);
+		node1.attachChild(box1);
+		node1.setLocalTranslation(new Vector3f(0, 30, 0));
+
+		node2 = new Node("Node 2");
+		box2 = new Box("Box 2", min, max);
+		node2.attachChild(box2);
+		node1.attachChild(node2);
+		node2.setLocalTranslation(new Vector3f(-20, -20, 0));
+
+		node3 = new Node("Node 3");
+		box3 = new Box("Box 3", min, max);
+		node3.attachChild(box3);
+		node1.attachChild(node3);
+		node3.setLocalTranslation(new Vector3f(20, -20, 0));
+
+		node4 = new Node("Node 4");
+		box4 = new Box("Box 4", min, max);
+		node4.attachChild(box4);
+		node2.attachChild(node4);
+		node4.setLocalTranslation(new Vector3f(-20, -20, 0));
+
+		node5 = new Node("Node 5");
+		box5 = new Box("Box 5", min, max);
+		node5.attachChild(box5);
+		node2.attachChild(node5);
+		node5.setLocalTranslation(new Vector3f(20, -20, 0));
+
+		node6 = new Node("Node 6");
+		box6 = new Box("Box 6", min, max);
+		node6.attachChild(box6);
+		node3.attachChild(node6);
+		node6.setLocalTranslation(new Vector3f(0, -20, 0));
+
+		scene.attachChild(node1);
+		root.attachChild(scene);
+		cam.update();
+		scene.updateGeometricState(0.0f, true);
+
+		nc1 = new NodeController(this, node1, "LWJGL");
+		nc2 = new NodeController(this, node2, "LWJGL");
+		nc3 = new NodeController(this, node3, "LWJGL");
+		nc4 = new NodeController(this, node4, "LWJGL");
+		nc5 = new NodeController(this, node5, "LWJGL");
+		nc6 = new NodeController(this, node6, "LWJGL");
+		nc1.setKeySpeed(5);
+		nc1.setMouseSpeed(1);
+		nc2.setKeySpeed(5);
+		nc2.setMouseSpeed(1);
+		nc3.setKeySpeed(5);
+		nc3.setMouseSpeed(1);
+		nc4.setKeySpeed(5);
+		nc4.setMouseSpeed(1);
+		nc5.setKeySpeed(5);
+		nc5.setMouseSpeed(1);
+		nc6.setKeySpeed(5);
+		nc6.setMouseSpeed(1);
+
+		input = nc1;
+
+		KeyBindingManager keyboard = KeyBindingManager.getKeyBindingManager();
+		keyboard.set("node1", KeyInput.KEY_1);
+		keyboard.set("node2", KeyInput.KEY_2);
+		keyboard.set("node3", KeyInput.KEY_3);
+		keyboard.set("node4", KeyInput.KEY_4);
+		keyboard.set("node5", KeyInput.KEY_5);
+		keyboard.set("node6", KeyInput.KEY_6);
+		TestNodeSelectionAction s1 = new TestNodeSelectionAction(this, 1);
+		s1.setKey("node1");
+		nc1.addAction(s1);
+        nc2.addAction(s1);
+        nc3.addAction(s1);
+        nc4.addAction(s1);
+        nc5.addAction(s1);
+        nc6.addAction(s1);
+		TestNodeSelectionAction s2 = new TestNodeSelectionAction(this, 2);
+		s2.setKey("node2");
+        nc1.addAction(s2);
+        nc2.addAction(s2);
+        nc3.addAction(s2);
+        nc4.addAction(s2);
+        nc5.addAction(s2);
+        nc6.addAction(s2);
+		TestNodeSelectionAction s3 = new TestNodeSelectionAction(this, 3);
+		s3.setKey("node3");
+        nc1.addAction(s3);
+        nc2.addAction(s3);
+        nc3.addAction(s3);
+        nc4.addAction(s3);
+        nc5.addAction(s3);
+        nc6.addAction(s3);
+		TestNodeSelectionAction s4 = new TestNodeSelectionAction(this, 4);
+		s4.setKey("node4");
+        nc1.addAction(s4);
+        nc2.addAction(s4);
+        nc3.addAction(s4);
+        nc4.addAction(s4);
+        nc5.addAction(s4);
+        nc6.addAction(s4);
+		TestNodeSelectionAction s5 = new TestNodeSelectionAction(this, 5);
+		s5.setKey("node5");
+        nc1.addAction(s5);
+        nc2.addAction(s5);
+        nc3.addAction(s5);
+        nc4.addAction(s5);
+        nc5.addAction(s5);
+        nc6.addAction(s5);
+		TestNodeSelectionAction s6 = new TestNodeSelectionAction(this, 6);
+		s6.setKey("node6");
+        nc1.addAction(s6);
+        nc2.addAction(s6);
+        nc3.addAction(s6);
+        nc4.addAction(s6);
+        nc5.addAction(s6);
+        nc6.addAction(s6);
+
+	}
+
+	public void setSelectedNode(int node) {
+		System.out.println("Number pressed");
+		switch (node) {
+			case 1 :
+				input = nc1;
+				text.print("Selected Node: Node 1");
+				break;
+			case 2 :
+				input = nc2;
+				text.print("Selected Node: Node 2");
+				break;
+			case 3 :
+				input = nc3;
+				text.print("Selected Node: Node 3");
+				break;
+			case 4 :
+				input = nc4;
+				text.print("Selected Node: Node 4");
+				break;
+			case 5 :
+				input = nc5;
+				text.print("Selected Node: Node 5");
+				break;
+			case 6 :
+				input = nc6;
+				text.print("Selected Node: Node 6");
+				break;
+		}
+	}
+
+	/**
+	 * not used.
+	 * @see com.jme.app.SimpleGame#reinit()
+	 */
+	protected void reinit() {
+
+	}
+
+	/**
+	 * Not used.
+	 * @see com.jme.app.SimpleGame#cleanup()
+	 */
+	protected void cleanup() {
+
+	}
+
+}
