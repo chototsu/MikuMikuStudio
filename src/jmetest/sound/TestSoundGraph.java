7979a5f756cae959a8f0eddb45363b3d67a159dd
renanse <renanse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-03-13 05:59:10 +0000

    no message
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@635 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/sound/TestSoundGraph.java
+++ src/jmetest/sound/TestSoundGraph.java
@@ -2,30 +2,30 @@
  * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- * Redistributions of source code must retain the above copyright notice, this 
- * list of conditions and the following disclaimer. 
- * 
- * Redistributions in binary form must reproduce the above copyright notice, 
- * this list of conditions and the following disclaimer in the documentation 
- * and/or other materials provided with the distribution. 
- * 
- * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
- * names of its contributors may be used to endorse or promote products derived 
- * from this software without specific prior written permission. 
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
  */
@@ -66,7 +66,7 @@ public class TestSoundGraph extends SimpleGame {
 	private Camera cam;
 	SphericalSound footsteps;
 	Box box;
-	
+
 	private InputController input;
 
 	/* (non-Javadoc)
@@ -119,9 +119,11 @@ public class TestSoundGraph extends SimpleGame {
 		cam.setFrame(loc, left, up, dir);
 		display.getRenderer().setCamera(cam);
 		input= new FirstPersonController(this, cam, "LWJGL");
+		input.setMouseSpeed(0.2f);
+		input.setKeySpeed(.1f);
 		SoundAPIController.getSoundSystem("LWJGL");
 		SoundAPIController.getRenderer().setCamera(cam);
-		
+
 
 	}
 
@@ -135,11 +137,15 @@ public class TestSoundGraph extends SimpleGame {
 		box= new Box("Box", min, max);
 		box.setModelBound(new BoundingSphere());
 		box.updateModelBound();
-		box.setLocalTranslation(new Vector3f(0, 0, -100));
+		box.setLocalTranslation(new Vector3f(0, 0, -50));
 		TextureState tst= display.getRenderer().getTextureState();
 		tst.setEnabled(true);
 		tst.setTexture(
-			TextureManager.loadTexture("data/Images/Monkey.jpg", Texture.MM_LINEAR, Texture.FM_LINEAR, true));
+			TextureManager.loadTexture(
+                TestSoundGraph.class.getClassLoader().getResource("jmetest/data/images/Monkey.jpg"),
+                Texture.MM_LINEAR,
+                Texture.FM_LINEAR,
+                true));
 		scene.setRenderState(tst);
 		ZBufferState buf= display.getRenderer().getZBufferState();
 		buf.setEnabled(true);
@@ -147,15 +153,16 @@ public class TestSoundGraph extends SimpleGame {
 		scene.setRenderState(buf);
 		scene.attachChild(box);
 		cam.update();
-		
+
 		snode=new SoundNode();
-		footsteps=new SphericalSound("data/sound/Footsteps.wav");
+		footsteps=new SphericalSound(TestSoundGraph.class.getClassLoader().getResource("jmetest/data/sound/Footsteps.wav"));
 		footsteps.getSource().setMaxDistance(100);
-		footsteps.getSource().setPosition(box.getLocalTranslation());				
+		footsteps.getSource().setRolloffFactor(.1f);
+		footsteps.getSource().setPosition(box.getLocalTranslation());
 		footsteps.getSource().setGain(1.0f);
 		snode.attachChild(footsteps);
-		
-		
+
+
 
 		scene.updateGeometricState(0.0f, true);
 		snode.updateGeometricState(0.0f, true);
