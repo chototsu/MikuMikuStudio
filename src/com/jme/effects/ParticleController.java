7ce418cce8b34858c0fda4cedf0156b50a08da82
darkprophet <darkprophet@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-03-13 02:07:13 +0000

    fixed RT_CLAMP issue
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@629 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/effects/ParticleController.java
+++ src/com/jme/effects/ParticleController.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * Redistributions of source code must retain the above copyright notice, this
  * list of conditions and the following disclaimer.
- *
+ * 
  * Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- *
+ * 
  * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
  * names of its contributors may be used to endorse or promote products derived
  * from this software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -26,7 +26,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
+ *  
  */
 package com.jme.effects;
 
@@ -36,219 +36,222 @@ import com.jme.system.DisplaySystem;
 import com.jme.renderer.Camera;
 
 /*
- * NOTE: 2/15/04 - Fixed random point using line. MP
- * 		 2/24/04 - Removed initialization of particles and place in particle
- *                  constructor. MP
- *       2/26/04 - Documentation. - MP
+ * NOTE: 2/15/04 - Fixed random point using line. MP 2/24/04 - Removed
+ * initialization of particles and place in particle constructor. MP 2/26/04 -
+ * Documentation. - MP
  */
 
 /**
  * <code>ParticleController</code> updates the particles from a given
  * particle system.
- *
+ * 
  * @author Ahmed
  * @version $Id: ParticleController.java,v 1.3 2004/02/03 22:44:13 darkprophet
  *          Exp $
  */
 public class ParticleController extends Controller {
 
-    private ParticleSystem ps;
-    private float time;
-    private Particle currentP;
+	private ParticleSystem ps;
+	private float time;
+	private Particle currentP;
+	private boolean isFirst;
 
-    private Matrix3f rotationalM;
+	private Matrix3f rotationalM;
 
-    public ParticleController(ParticleSystem p) {
-        super();
-        ps = p;
-        time = 0.0f;
-        rotationalM = new Matrix3f();
-    }
+	public ParticleController(ParticleSystem p) {
+		super();
+		ps = p;
+		time = 0.0f;
+		rotationalM = new Matrix3f();
+		isFirst = true;
+	}
 
-    public void update(float timeF) {
-        time = timeF * ps.getSpeed();
-        for (int i = 0; i < ps.getParticles().length; i++) {
-            currentP = ps.getParticles()[i];
+	public void update(float timeF) {
+		time = timeF * ps.getSpeed();
+		for (int i = 0; i < ps.getParticles().length; i++) {
+			currentP = ps.getParticles()[i];
+			
+			if (isFirst == true) {
+				regenerateParticle();
+			}
 
-            if (getRepeatType() == RT_WRAP) {
-                // check if dead
-                if (currentP.life <= 0.0f) {
-                    regenerateParticle();
-                }
+			if (getRepeatType() == RT_WRAP) {
+				// check if dead
+				if (currentP.life <= 0.0f) {
+					regenerateParticle();
+				}
 
-                // update the particle
-                updateParticle();
+				// update the particle
+				updateParticle();
 
-            } else if (getRepeatType() == RT_CLAMP) {
-                // if its the first time, generate
-                    if (currentP.life <= 0.0f) {
-                    // if particle is dead, bury it
-                    killParticle();
-                }
+			} else if (getRepeatType() == RT_CLAMP) {
+				// if its the first time, generate
+				if (currentP.life <= 0.0f) {
+					// if particle is dead, bury it
+					killParticle();
+				}
 
-                // update the particle
-                updateParticle();
+				// update the particle
+				updateParticle();
 
-            } else if (getRepeatType() == RT_CYCLE) {
-                setRepeatType(RT_WRAP);
-            }
-        }
-    }
+			} else if (getRepeatType() == RT_CYCLE) {
+				setRepeatType(RT_WRAP);
+			}
+		}
+		isFirst = false;
+	}
 
-    public void updateParticle() {
-        // update life
-        currentP.life -= currentP.fade * time;
+	public void updateParticle() {
+		// update life
+		currentP.life -= currentP.fade * time;
 
-        // update position by velocity;
-        currentP.getLocalTranslation().x
-            += (currentP.velocity.x / (ps.getFriction() * 1000))
-            * time;
-        currentP.getLocalTranslation().y
-            += (currentP.velocity.y / (ps.getFriction() * 1000))
-            * time;
-        currentP.getLocalTranslation().z
-            += (currentP.velocity.z / (ps.getFriction() * 1000))
-            * time;
-        //currentP.setLocalTranslation(currentP.position);
+		// update position by velocity;
+		currentP.getLocalTranslation().x
+			+= (currentP.velocity.x / (ps.getFriction() * 1000))
+			* time;
+		currentP.getLocalTranslation().y
+			+= (currentP.velocity.y / (ps.getFriction() * 1000))
+			* time;
+		currentP.getLocalTranslation().z
+			+= (currentP.velocity.z / (ps.getFriction() * 1000))
+			* time;
+		//currentP.setLocalTranslation(currentP.position);
 
-        // update velocity by gravity;
-        currentP.velocity.x += ps.getGravity().x * time;
-        currentP.velocity.y += ps.getGravity().y * time;
-        currentP.velocity.z += ps.getGravity().z * time;
+		// update velocity by gravity;
+		currentP.velocity.x += ps.getGravity().x * time;
+		currentP.velocity.y += ps.getGravity().y * time;
+		currentP.velocity.z += ps.getGravity().z * time;
 
-        // interpolate colors
-        currentP.color.r =
-            (ps.getStartColor().r * currentP.life)
-                + (ps.getEndColor().r * (1 - currentP.life));
-        currentP.color.g =
-            (ps.getStartColor().g * currentP.life)
-                + (ps.getEndColor().g * (1 - currentP.life));
-        currentP.color.b =
-            (ps.getStartColor().b * currentP.life)
-                + (ps.getEndColor().b * (1 - currentP.life));
-        currentP.color.a =
-            (ps.getStartColor().a * currentP.life)
-                + (ps.getEndColor().a * (1 - currentP.life));
-        currentP.updateColor();
+		// interpolate colors
+		currentP.color.r =
+			(ps.getStartColor().r * currentP.life)
+				+ (ps.getEndColor().r * (1 - currentP.life));
+		currentP.color.g =
+			(ps.getStartColor().g * currentP.life)
+				+ (ps.getEndColor().g * (1 - currentP.life));
+		currentP.color.b =
+			(ps.getStartColor().b * currentP.life)
+				+ (ps.getEndColor().b * (1 - currentP.life));
+		currentP.color.a =
+			(ps.getStartColor().a * currentP.life)
+				+ (ps.getEndColor().a * (1 - currentP.life));
+		currentP.updateColor();
 
-        // update the size, currently, the size
-        // updates both the x and y values. So you always
-        // get a square
-        currentP.size =
-            (ps.getStartSize() * currentP.life)
-                + (ps.getEndSize() * (1 - currentP.life));
-        currentP.setLocalScale(currentP.size);
+		// update the size, currently, the size
+		// updates both the x and y values. So you always
+		// get a square
+		currentP.size =
+			(ps.getStartSize() * currentP.life)
+				+ (ps.getEndSize() * (1 - currentP.life));
+		currentP.setLocalScale(currentP.size);
 
-        Camera cam = DisplaySystem
-                        .getDisplaySystem()
-                        .getRenderer()
-                        .getCamera();
+		Camera cam = DisplaySystem.getDisplaySystem().getRenderer().getCamera();
 
-        rotationalM.setColumn(0, cam.getLeft());
-        rotationalM.setColumn(1, cam.getUp());
-        rotationalM.setColumn(2, cam.getDirection());
-        currentP.setLocalRotation(rotationalM);
+		rotationalM.setColumn(0, cam.getLeft());
+		rotationalM.setColumn(1, cam.getUp());
+		rotationalM.setColumn(2, cam.getDirection());
+		currentP.setLocalRotation(rotationalM);
 
-    }
+	}
 
-    private void killParticle() {
-        currentP.life = 0f;
-        currentP.fade = 0f;
+	private void killParticle() {
+		currentP.life = 0f;
+		currentP.fade = 0f;
 
-        currentP.color.r = 0;
-        currentP.color.g = 0;
-        currentP.color.b = 0;
-        currentP.color.a = 0;
+		currentP.color.r = 0;
+		currentP.color.g = 0;
+		currentP.color.b = 0;
+		currentP.color.a = 0;
 
-        currentP.size = 0;
+		currentP.size = 0;
 
-        currentP.velocity.x = 1;
-        currentP.velocity.y = 1;
-        currentP.velocity.z = 1;
-    }
+		currentP.velocity.x = 1;
+		currentP.velocity.y = 1;
+		currentP.velocity.z = 1;
+	}
 
-    private void regenerateParticle() {
-        currentP.life = 1.0f;
-        currentP.fade = (float) (ps.getFade() * Math.random() + ps.getFade());
+	private void regenerateParticle() {
+		currentP.life = 1.0f;
+		currentP.fade = (float) (ps.getFade() * Math.random() + ps.getFade());
 
-        switch (ps.getGeometry()) {
-            case 1 :
-                if (ps == ps.getParticleParent()) {
-                    currentP.setLocalTranslation(ps.getLine().random());
-                    break;
-                } else {
-                    currentP.setLocalTranslation(ps.getLine().random());
-                    currentP.getLocalTranslation().x
-                        += ps.getLocalTranslation().x;
-                    currentP.getLocalTranslation().y
-                        += ps.getLocalTranslation().y;
-                    currentP.getLocalTranslation().z
-                        += ps.getLocalTranslation().z;
-                    break;
-                }
-            case 2 :
-                if (ps == ps.getParticleParent()) {
-                    currentP.setLocalTranslation(ps.getRectangle().random());
-                    break;
-                } else {
-                    currentP.setLocalTranslation(ps.getRectangle().random());
-                    currentP.getLocalTranslation().x
-                        += ps.getLocalTranslation().x;
-                    currentP.getLocalTranslation().y
-                        += ps.getLocalTranslation().y;
-                    currentP.getLocalTranslation().z
-                        += ps.getLocalTranslation().z;
-                    break;
-                }
-            default :
-                if (ps == ps.getParticleParent()) {
-                    currentP.getLocalTranslation().x = ps.getStartPosition().x;
-                    currentP.getLocalTranslation().y = ps.getStartPosition().y;
-                    currentP.getLocalTranslation().z = ps.getStartPosition().z;
-                    break;
-                } else {
-                    currentP.getLocalTranslation().x =
-                        ps.getStartPosition().x + ps.getLocalTranslation().x;
-                    currentP.getLocalTranslation().y =
-                        ps.getStartPosition().y + ps.getLocalTranslation().y;
-                    currentP.getLocalTranslation().z =
-                        ps.getStartPosition().z + ps.getLocalTranslation().z;
-                    break;
-                }
+		switch (ps.getGeometry()) {
+			case 1 :
+				if (ps == ps.getParticleParent()) {
+					currentP.setLocalTranslation(ps.getLine().random());
+					break;
+				} else {
+					currentP.setLocalTranslation(ps.getLine().random());
+					currentP.getLocalTranslation().x
+						+= ps.getLocalTranslation().x;
+					currentP.getLocalTranslation().y
+						+= ps.getLocalTranslation().y;
+					currentP.getLocalTranslation().z
+						+= ps.getLocalTranslation().z;
+					break;
+				}
+			case 2 :
+				if (ps == ps.getParticleParent()) {
+					currentP.setLocalTranslation(ps.getRectangle().random());
+					break;
+				} else {
+					currentP.setLocalTranslation(ps.getRectangle().random());
+					currentP.getLocalTranslation().x
+						+= ps.getLocalTranslation().x;
+					currentP.getLocalTranslation().y
+						+= ps.getLocalTranslation().y;
+					currentP.getLocalTranslation().z
+						+= ps.getLocalTranslation().z;
+					break;
+				}
+			default :
+				if (ps == ps.getParticleParent()) {
+					currentP.getLocalTranslation().x = ps.getStartPosition().x;
+					currentP.getLocalTranslation().y = ps.getStartPosition().y;
+					currentP.getLocalTranslation().z = ps.getStartPosition().z;
+					break;
+				} else {
+					currentP.getLocalTranslation().x =
+						ps.getStartPosition().x + ps.getLocalTranslation().x;
+					currentP.getLocalTranslation().y =
+						ps.getStartPosition().y + ps.getLocalTranslation().y;
+					currentP.getLocalTranslation().z =
+						ps.getStartPosition().z + ps.getLocalTranslation().z;
+					break;
+				}
 
-        }
+		}
 
-        currentP.color.r = ps.getStartColor().r;
-        currentP.color.g = ps.getStartColor().g;
-        currentP.color.b = ps.getStartColor().b;
-        currentP.color.a = ps.getStartColor().a;
+		currentP.color.r = ps.getStartColor().r;
+		currentP.color.g = ps.getStartColor().g;
+		currentP.color.b = ps.getStartColor().b;
+		currentP.color.a = ps.getStartColor().a;
 
-        currentP.size = ps.getStartSize();
+		currentP.size = ps.getStartSize();
 
-        currentP.velocity.x =
-            (float) (((Math.random() * 32767) % 50) - 26) * 10;
-        currentP.velocity.y =
-            (float) (((Math.random() * 32767) % 50) - 26) * 10;
-        currentP.velocity.z =
-            (float) (((Math.random() * 32767) % 50) - 26) * 10;
-    }
-    
-    public boolean getParticlesAlive() {
-    	boolean success = false;
-    	for (int i = 0; i < ps.getParticles().length; i++) {
-    		if (ps.getParticles()[i].life > 0) {
-    			success = false;
-    			break;
-    		}else {
-    			success = true;
-    		}
-    	}
-    	
-    	if (success) {
-    		return true;
-    	}else {
-    		return false;
-    	}
-    	
-    }
+		currentP.velocity.x =
+			(float) (((Math.random() * 32767) % 50) - 26) * 10;
+		currentP.velocity.y =
+			(float) (((Math.random() * 32767) % 50) - 26) * 10;
+		currentP.velocity.z =
+			(float) (((Math.random() * 32767) % 50) - 26) * 10;
+	}
+
+	public boolean getParticlesAlive() {
+		boolean success = false;
+		for (int i = 0; i < ps.getParticles().length; i++) {
+			if (ps.getParticles()[i].life > 0) {
+				success = false;
+				break;
+			} else {
+				success = true;
+			}
+		}
+
+		if (success) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
 }
