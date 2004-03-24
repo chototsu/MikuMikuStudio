9db8359a5db978a18c77b67031f44ab1bed5ec8d
renanse <renanse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-03-24 01:04:13 +0000

    updates
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@682 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/effects/RenParticleControlFrame.java
+++ src/jmetest/effects/RenParticleControlFrame.java
@@ -1,5 +1,43 @@
+/*
+ * Created on Jan 20, 2004
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 package jmetest.effects;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.SystemColor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
@@ -15,18 +53,8 @@ import javax.swing.border.TitledBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import com.jme.math.FastMath;
 import com.jme.renderer.ColorRGBA;
-import com.jme.math.*;
-import java.awt.*;
-
-/**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2004</p>
- * <p>Company: </p>
- * @author not attributable
- * @version 1.0
- */
 
 public class RenParticleControlFrame extends JFrame {
   BorderLayout borderLayout1 = new BorderLayout();
@@ -103,7 +131,15 @@ public class RenParticleControlFrame extends JFrame {
   JLabel randomLabel = new JLabel();
   JSlider randomSlider = new JSlider();
 
-  public RenParticleControlFrame() {
+  /**
+  * <code>RenParticleControlFrame</code>
+  *
+  * @author Joshua Slack
+  * @version $Id: RenParticleControlFrame.java,v 1.3 2004-03-24 01:04:13 renanse Exp $
+  *
+  */
+
+ public RenParticleControlFrame() {
     try {
       jbInit();
     }
