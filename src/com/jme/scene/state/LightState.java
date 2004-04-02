fdcb0c598619794f1ec31ca582c7fd06ecda36a1
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-04-02 21:05:47 +0000

    added static counter for the number of lights.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@767 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/scene/state/LightState.java
+++ src/com/jme/scene/state/LightState.java
@@ -41,7 +41,7 @@ import com.jme.light.Light;
  * be added to the light state. Each light is processed and used to modify
  * the color of the scene.
  * @author Mark Powell
- * @version $Id: LightState.java,v 1.2 2004-02-27 00:18:09 mojomonkey Exp $
+ * @version $Id: LightState.java,v 1.3 2004-04-02 21:05:47 mojomonkey Exp $
  */
 public abstract class LightState extends RenderState {
     /**
@@ -53,7 +53,9 @@ public abstract class LightState extends RenderState {
     //holds the lights
     private ArrayList lightList;
     protected boolean twoSidedOn;
-
+    
+    protected static int numLights;
+    
     /**
      * Constructor instantiates a new <code>LightState</code> object. Initially
      * there are no lights set.
