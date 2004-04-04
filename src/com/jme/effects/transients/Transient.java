df71d3f601ce10662ed5db95a6401197092b3ceb
darkprophet <darkprophet@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-04-04 18:03:08 +0000

    new Transient base code
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@786 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/effects/transients/Transient.java
+++ src/com/jme/effects/transients/Transient.java
@@ -29,116 +29,54 @@
  *  
  */
 package com.jme.effects.transients;
-
-import com.jme.renderer.TextureRenderer;
+import com.jme.image.Texture;
 import com.jme.scene.Controller;
 import com.jme.scene.Node;
-import com.jme.system.DisplaySystem;
-
 /**
  * <code>Transient</code>
  * 
  * @author Ahmed
- * @version $Id: Transient.java,v 1.1 2004-04-04 16:48:10 darkprophet Exp $ provides a base at which furthur effects can be made of
+ * @version $Id: Transient.java,v 1.2 2004-04-04 18:03:08 darkprophet Exp $
+ *          provides a base at which furthur effects can be made of
  */
-public abstract class Transient extends Node {
-
-	public Transient(
-		String name,
-		DisplaySystem game,
-		Node root,
-		Node from,
-		Node to,
-		TextureRenderer textureR) {
+public class Transient extends Node {
+	
+	private int maxNumOfStages, currentStage;
+	private Texture inTex, outTex;
+	
+	public Transient(String name, Texture out, Texture in) {
 		super(name);
 	}
 
-	/**
-	 * 
-	 * <code>getCurrentStage</code> returns the current stage which the
-	 * effect is at.
-	 * 
-	 * @return currentStage
-	 */
-	public abstract int getCurrentStage();
+	protected int getCurrentStage() {
+		return currentStage;
+	}
 
-	/**
-	 * 
-	 * <code>setCurrentStage</code> sets the stage at which the effect is at.
-	 * 
-	 * @param stage
-	 */
-	public abstract void setCurrentStage(int stage);
+	protected void setCurrentStage(int stage) {
+		currentStage = stage;
+	}
 
-	/**
-	 * 
-	 * <code>getNumOfStages</code> returns the maximum number of stages that
-	 * the current effect has
-	 * 
-	 * @return numOfStages
-	 */
-	public abstract int getNumOfStages();
+	protected int getNumOfStages() {
+		return maxNumOfStages;
+	}
 
-	/**
-	 * 
-	 * <code>setNumOfStages</code> sets the total number of stages the
-	 * current effect has
-	 * 
-	 * @param num
-	 */
-	public abstract void setNumOfStages(int num);
+	protected void setNumOfStages(int num) {
+		maxNumOfStages = num;
+	}
 
-	/**
-	 * <code>getRootNode</code>
-	 * 
-	 * @return rootNode, where the effect will be attached to
-	 */
-	public abstract Node getRootNode();
-
-	/**
-	 * <code>setRootNode</code>
-	 * 
-	 * @param rootNode,
-	 *            the root where the effect will be attached to
-	 */
-	public abstract void setRootNode(Node root);
-
-	/**
-	 * <code>getRemoveNode</code>
-	 * 
-	 * @return removeNode, the node that will be removed after effect has taken
-	 *         place
-	 */
-	public abstract Node getRemoveNode();
-
-	/**
-	 * <code>setRemoveNode</code>
-	 * 
-	 * @param out,
-	 *            the node that will be removed after effect has taken place
-	 */
-	public abstract void setRemoveNode(Node out);
-
-	/**
-	 * <code>getInsertNode</code>
-	 * 
-	 * @return InsertNode, this is the node that will be placed when the remove
-	 *         node has been removed
-	 */
-	public abstract Node getInsertNode();
-
-	/**
-	 * <code>setInsertNode</code> sets the node that will be attached to the
-	 * root after effect has taken place
-	 */
-	public abstract void setInsertNode(Node attach);
-
-	/**
-	 * <code>getEffectController</code> returns the controller that is
-	 * controlling the animation's update.
-	 * 
-	 * @return the controller that defines the effect
-	 */
-	public abstract Controller getEffectController();
-
+	protected Texture getOutTexture() {
+		return outTex;
+	}
+	
+	protected void setOutTexture(Texture out) {
+		outTex = out;
+	}
+	
+	protected Texture getInTexture() {
+		return inTex;
+	}
+	
+	protected void setInTexture(Texture in) {
+		inTex = in;
+	}
 }
