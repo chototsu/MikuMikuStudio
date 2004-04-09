3f33939b6178990ae0bed47c27829570003cae10
renanse <renanse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-04-09 22:51:03 +0000

    more efficient... doesn't check under certain distance conditions
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@813 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/scene/lod/AreaClodMesh.java
+++ src/com/jme/scene/lod/AreaClodMesh.java
@@ -47,7 +47,7 @@ import com.jme.system.DisplaySystem;
  * originally ported from David Eberly's c++, modifications and
  * enhancements made from there.
  * @author Joshua Slack
- * @version $Id: AreaClodMesh.java,v 1.1 2004-04-09 22:39:25 renanse Exp $
+ * @version $Id: AreaClodMesh.java,v 1.2 2004-04-09 22:51:03 renanse Exp $
  */
 public class AreaClodMesh extends ClodMesh {
   float trisPerPixel = 1f;
@@ -81,8 +81,13 @@ public class AreaClodMesh extends ClodMesh {
       return 0;
     }
     float newDistance = getModelBound().distanceTo(r.getCamera().getLocation());
+    if (lastDistance > newDistance && targetRecord == 0)
+      return targetRecord; // we're already at the lowest setting and we just got closer to the model, no need to keep trying.
+    if (lastDistance < newDistance && targetRecord == records.length-1)
+      return targetRecord; // we're already at the highest setting and we just got further from the model, no need to keep trying.
     if (Math.abs(newDistance - lastDistance) <= distTolerance)
       return targetRecord; // we haven't moved, send the old measurement back.
+
 
     lastDistance = newDistance;
 
