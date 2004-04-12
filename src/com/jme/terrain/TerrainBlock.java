8bcdc2040711fd0394736a4aeeaa4d4620c21e4f
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-04-12 00:30:47 +0000

    Initial check in of height map generators. As well as an updated brute force (block) version.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@818 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/terrain/TerrainBlock.java
+++ src/com/jme/terrain/TerrainBlock.java
@@ -1,98 +1,153 @@
 /*
- * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without 
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this 
- * list of conditions and the following disclaimer. 
  * 
- * Redistributions in binary form must reproduce the above copyright notice, 
- * this list of conditions and the following disclaimer in the documentation 
- * and/or other materials provided with the distribution. 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
  * 
- * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
- * names of its contributors may be used to endorse or promote products derived 
- * from this software without specific prior written permission. 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
+ *  
  */
 
 package com.jme.terrain;
 
 import com.jme.scene.TriMesh;
+import com.jme.math.Vector2f;
 import com.jme.math.Vector3f;
 
 /**
- *
- * @author  Mark Powell
+ * @author Mark Powell
  */
 public class TerrainBlock extends TriMesh {
+
     private int size;
+
     private float stepScale;
+
     /** Creates a new instance of TerrainBlock */
-    public TerrainBlock(String name, int size, float stepScale, float[] heightMap, Vector3f origin) {
+    public TerrainBlock(String name, int size, float stepScale,
+            int[] heightMap, Vector3f origin) {
         super(name);
         this.size = size;
         this.stepScale = stepScale;
         setLocalTranslation(origin);
         buildVertices(heightMap);
+        buildTextureCoordinates();
+        buildNormals();
     }
-    
+
     /**
-     * <code>buildVertices</code> sets up the vertex and index arrays of the 
-     *TriMesh. 
-     *@param heightMap the raw data.
+     * <code>buildVertices</code> sets up the vertex and index arrays of the
+     * TriMesh.
+     * 
+     * @param heightMap
+     *            the raw data.
      */
-    private void buildVertices(float[] heightMap) {
+    private void buildVertices(int[] heightMap) {
         vertex = new Vector3f[heightMap.length];
-        for(int x = 0; x < size; x++) {
-            for(int y = 0; y < size; y++) {
-                vertex[x + (y * size)] = new Vector3f(x*stepScale,heightMap[x + (y * size)],y*stepScale);
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                vertex[x + (y * size)] = new Vector3f(x * stepScale,
+                        heightMap[x + (y * size)], y * stepScale);
             }
         }
-        
+
         //set up the indices
-        int value = ((size - 1)*(size - 1)) * 6;
+        int value = ((size - 1) * (size - 1)) * 6;
         indices = new int[value];
-        
+
         int count = 0;
-        
+
         //go through entire array up to the second to last column.
-        for(int i = 0; i < (size * (size-1)); i++) {
+        for (int i = 0; i < (size * (size - 1)); i++) {
             //we want to skip the top row.
-            if(i % ((size*(i / size + 1)) - 1) == 0 && i != 0) {
+            if (i % ((size * (i / size + 1)) - 1) == 0 && i != 0) {
                 continue;
             }
-            //set the bottom left corner.
-            indices[count++] = i;
-            //set the top right corner.
-            indices[count++] = ((1 + size) + i);
             //set the top left corner.
-            indices[count++] = (size + i);
-            //set the bottom left corner
             indices[count++] = i;
-            //set the bottom right corner
-            indices[count++] = 1 + i;
-            //set the upper right corner
+            //set the bottom right corner.
             indices[count++] = ((1 + size) + i);
-            
+            //set the top right corner.
+            indices[count++] = (1 + i);
+            //set the top left corner
+            indices[count++] = i;
+            //set the bottom left corner
+            indices[count++] = size + i;
+            //set the bottom right corner
+            indices[count++] = ((1 + size) + i);
+
         }
-        
+
         setVertices(vertex);
         setIndices(indices);
     }
-    
+
+    /**
+     * <code>buildTextureCoordinates</code> calculates the texture coordinates
+     * of the terrain.
+     *  
+     */
+    private void buildTextureCoordinates() {
+        texture[0] = new Vector2f[vertex.length];
+
+        for (int i = 0; i < texture[0].length; i++) {
+            texture[0][i] = new Vector2f(
+                    vertex[i].x / (stepScale * (size - 1)),
+                    1.0f - (vertex[i].z / (stepScale * (size - 1))));
+        }
+
+        setTextures(texture[0]);
+    }
+
+    /**
+     * 
+     * <code>buildNormals</code> calculates the normals of each vertex that
+     * makes up the block of terrain.
+     * 
+     *  
+     */
+    private void buildNormals() {
+        Vector3f[] normal = new Vector3f[vertex.length];
+
+        //get the first and last normals taken care of.
+        normal[0] = vertex[size].cross(vertex[1]).normalize();
+        normal[normal.length - 1] = vertex[normal.length - 1 - size]
+                .cross(vertex[normal.length - 2]).normalize();
+
+        for (int i = 1; i < normal.length - 1; i++) {
+            if (i % ((size * (i / size + 1)) - 1) == 0) {
+                //right hand normal
+                normal[i] = vertex[i - 1].cross(vertex[i + size]).normalize();
+            } else if (i >= size * (size - 1)) {
+                //bottom row
+                normal[i] = vertex[i + 1].cross(vertex[i - size]).normalize();
+            } else {
+                //interior
+                normal[i] = vertex[i + size].cross(vertex[i + 1]).normalize();
+            }
+        }
+
+        setNormals(normal);
+    }
 }
---
+++ src/com/jme/terrain/util/RawHeightMap.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2003, jMonkeyEngine - Mojo Monkey Coding
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. 
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution. 
+ * 
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the 
+ * names of its contributors may be used to endorse or promote products derived 
+ * from this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package com.jme.terrain.util;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import com.jme.system.JmeException;
+import com.jme.util.LoggingSystem;
+
+/**
+ * <code>RawHeightMap</code> creates a height map from a RAW image file. The
+ * greyscale image denotes height based on the value of the pixel for each
+ * point. Where pure black denotes 0 and pure white denotes 255.
+ * 
+ * @author Mark Powell
+ * @version $Id: RawHeightMap.java,v 1.1 2004-04-12 00:30:46 mojomonkey Exp $
+ */
+public class RawHeightMap extends AbstractHeightMap {
+    private String filename;
+
+    /**
+     * Constructor creates a new <code>RawHeightMap</code> object and
+     * loads a RAW image file to use as a height
+     * field. The greyscale image denotes the height of the terrain, where
+     * dark is low point and bright is high point. The values of the RAW
+     * correspond directly with the RAW values or 0 - 255. 
+     * 
+     * @param filename the RAW file to use as the heightmap.
+     * @param size the size of the RAW (must be square).
+     * 
+     * @throws MonkeyRuntimeException if the filename is null or not RAW, and
+     *      if the size is 0 or less.
+     */
+    public RawHeightMap(String filename, int size) {
+        //varify that filename and size are valid.
+        if (null == filename || size <= 0) {
+            throw new JmeException(
+                "Must supply valid filename and " + "size (> 0)");
+        }
+
+        //make sure it's a raw file.
+        if (!filename.endsWith(".raw")) {
+            throw new JmeException("Height data must be RAW format");
+        }
+
+        this.filename = filename;
+        this.size = size;
+        load();
+    }
+
+    /**
+     * <code>load</code> fills the height data array with the appropriate
+     * data from the set RAW image. If the RAW image has not been set a 
+     * JmeException will be thrown.
+     * 
+     * @return true if the load is successfull, false otherwise.
+     */
+    public boolean load() {
+        //confirm data has been set. Redundant check...
+        if (null == filename || size <= 0) {
+            throw new JmeException(
+                "Must supply valid filename and " + "size (> 0)");
+        }
+        
+        //clean up
+        if (null != heightData) {
+            unloadHeightMap();
+        }
+
+        //initialize the height data attributes
+        heightData = new int[size*size];
+
+        //attempt to connect to the supplied file.
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream(filename);
+            
+            DataInputStream bis = new DataInputStream(fis);
+            //read in each byte from the raw file.
+            for (int i = 0; i < size; i++) {
+                for(int j = 0; j < size; j++) {
+                    heightData[i + (j*size)] = bis.readUnsignedByte();
+                }
+            }
+
+            bis.close();
+            fis.close();
+
+        } catch (FileNotFoundException e) {
+            LoggingSystem.getLogger().log(
+                Level.WARNING,
+                "Heightmap file" + filename + " not found.");
+            return false;
+        } catch (IOException e1) {
+            LoggingSystem.getLogger().log(
+                Level.WARNING,
+                "Error reading data from " + filename);
+            return false;
+        }
+
+        LoggingSystem.getLogger().log(
+            Level.WARNING,
+            "Successfully loaded " + filename);
+        return true;
+    }
+
+    /**
+     * <code>setFilename</code> sets the file to use for the RAW data. A
+     * call to <code>load</code> is required to put the changes into effect.
+     *
+     * @param filename the new file to use for the height data.
+     * 
+     * @throws JmeException if the file is null or not RAW.
+     */
+    public void setFilename(String filename) {
+        if (null == filename) {
+            throw new JmeException("Must supply valid filename");
+        }
+
+        if (null == filename || size <= 0) {
+            throw new JmeException(
+                "Must supply valid filename and " + "size (> 0)");
+        }
+
+        this.filename = filename;
+    }
+}
