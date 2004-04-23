dc9da98aa384ceebca36d997797fbaae44557c2a
renanse <renanse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-04-23 03:42:26 +0000

    moved to using SimpleGame
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@929 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/TestBezierMesh.java
+++ src/jmetest/renderer/TestBezierMesh.java
@@ -31,259 +31,143 @@
  */
 package jmetest.renderer;
 
-import com.jme.app.BaseGame;
+import com.jme.app.SimpleGame;
 import com.jme.bounding.BoundingSphere;
 import com.jme.image.Texture;
-import com.jme.input.FirstPersonHandler;
-import com.jme.input.InputHandler;
 import com.jme.light.LightNode;
 import com.jme.light.PointLight;
 import com.jme.math.Vector3f;
-import com.jme.renderer.Camera;
 import com.jme.renderer.ColorRGBA;
 import com.jme.scene.BezierMesh;
 import com.jme.scene.BezierPatch;
-import com.jme.scene.Node;
 import com.jme.scene.TriMesh;
 import com.jme.scene.shape.Box;
-import com.jme.scene.state.AlphaState;
-import com.jme.scene.state.LightState;
 import com.jme.scene.state.MaterialState;
 import com.jme.scene.state.TextureState;
-import com.jme.scene.state.WireframeState;
-import com.jme.scene.state.ZBufferState;
-import com.jme.system.DisplaySystem;
-import com.jme.system.JmeException;
 import com.jme.util.TextureManager;
-import com.jme.util.Timer;
+import com.jme.math.Quaternion;
+import com.jme.math.FastMath;
 
 /**
  * <code>TestLightState</code>
  * @author Mark Powell
- * @version $Id: TestBezierMesh.java,v 1.15 2004-04-22 22:27:38 renanse Exp $
+ * @version $Id: TestBezierMesh.java,v 1.16 2004-04-23 03:42:26 renanse Exp $
  */
-public class TestBezierMesh extends BaseGame {
-    private TriMesh t;
-    private Camera cam;
-    private Node root;
-    private Node scene;
-    private InputHandler input;
-    private Thread thread;
-    private LightState lightstate;
-    private PointLight pl;
-    private LightNode lightNode;
-    private Vector3f currentPos;
-    private Vector3f newPos;
-    private Timer timer;
-    private WireframeState wf;
+public class TestBezierMesh extends SimpleGame {
+  private TriMesh t;
+  private PointLight pl;
+  private LightNode lightNode;
+  private Vector3f currentPos;
+  private Vector3f newPos;
 
-    /**
-     * Entry point for the test,
-     * @param args
-     */
-    public static void main(String[] args) {
-        TestBezierMesh app = new TestBezierMesh();
-        app.setDialogBehaviour(ALWAYS_SHOW_PROPS_DIALOG);
-        app.start();
+  /**
+   * Entry point for the test,
+   * @param args
+   */
+  public static void main(String[] args) {
+    TestBezierMesh app = new TestBezierMesh();
+    app.setDialogBehaviour(ALWAYS_SHOW_PROPS_DIALOG);
+    app.start();
 
+  }
+
+  protected void simpleUpdate() {
+    // update light pos
+    if ( (int) currentPos.x == (int) newPos.x
+        && (int) currentPos.y == (int) newPos.y
+        && (int) currentPos.z == (int) newPos.z) {
+      newPos.x = (float) Math.random() * 10 - 5;
+      newPos.y = (float) Math.random() * 10 - 5;
+      newPos.z = (float) Math.random() * 10 - 5;
     }
 
-    /**
-     * Not used in this test.
-     * @see com.jme.app.SimpleGame#update()
-     */
-    protected void update(float interpolation) {
-//      update world data
-        timer.update();
-        input.update(timer.getTimePerFrame() * 10);
+    currentPos.x -= (currentPos.x - newPos.x)
+        / (timer.getFrameRate() / 2);
+    currentPos.y -= (currentPos.y - newPos.y)
+        / (timer.getFrameRate() / 2);
+    currentPos.z -= (currentPos.z - newPos.z)
+        / (timer.getFrameRate() / 2);
 
+    lightNode.setLocalTranslation(currentPos);
+  }
 
-        //update individual sprites
-         if ((int) currentPos.x == (int) newPos.x
-             && (int) currentPos.y == (int) newPos.y
-             && (int) currentPos.z == (int) newPos.z) {
-             newPos.x = (float) Math.random() * 10 - 5;
-             newPos.y = (float) Math.random() * 10 - 5;
-             newPos.z = (float) Math.random() * 10 - 5;
-         }
+  protected void simpleInitGame() {
+    display.setTitle("Bezier Mesh Test");
+    cam.setLocation(new Vector3f(0, 0, 5));
+    cam.update();
+    currentPos = new Vector3f();
+    newPos = new Vector3f();
 
-         currentPos.x -= (currentPos.x - newPos.x)
-             / (timer.getFrameRate() / 2);
-         currentPos.y -= (currentPos.y - newPos.y)
-             / (timer.getFrameRate() / 2);
-         currentPos.z -= (currentPos.z - newPos.z)
-             / (timer.getFrameRate() / 2);
+    BezierPatch bp = new BezierPatch();
+    bp.setAnchor(0, 0, new Vector3f( -0.75f, -0.75f, -0.5f));
+    bp.setAnchor(0, 1, new Vector3f( -0.25f, -0.75f, 0.0f));
+    bp.setAnchor(0, 2, new Vector3f(0.25f, -0.75f, 0.5f));
+    bp.setAnchor(0, 3, new Vector3f(0.75f, -0.75f, 0.5f));
+    bp.setAnchor(1, 0, new Vector3f( -0.75f, -0.25f, -0.75f));
+    bp.setAnchor(1, 1, new Vector3f( -0.25f, -0.25f, 0.5f));
+    bp.setAnchor(1, 2, new Vector3f(0.25f, -0.25f, 0.5f));
+    bp.setAnchor(1, 3, new Vector3f(0.75f, -0.25f, 0.75f));
+    bp.setAnchor(2, 0, new Vector3f( -0.75f, 0.25f, -0.5f));
+    bp.setAnchor(2, 1, new Vector3f( -0.25f, 0.25f, -0.5f));
+    bp.setAnchor(2, 2, new Vector3f(0.25f, 0.25f, -0.5f));
+    bp.setAnchor(2, 3, new Vector3f(0.75f, 0.25f, 0.0f));
+    bp.setAnchor(3, 0, new Vector3f( -0.75f, 0.75f, -0.5f));
+    bp.setAnchor(3, 1, new Vector3f( -0.25f, 0.75f, -1.0f));
+    bp.setAnchor(3, 2, new Vector3f(0.25f, 0.75f, -1.0f));
+    bp.setAnchor(3, 3, new Vector3f(0.75f, 0.75f, -0.5f));
+    bp.setDetailLevel(64);
 
-         lightNode.setLocalTranslation(currentPos);
+    BezierMesh bez = new BezierMesh("Bezier Mesh");
+    bez.setPatch(bp);
+    Quaternion rot = new Quaternion();
+    rot.fromAngleAxis(FastMath.PI, new Vector3f(0,0,1));
+    bez.setLocalRotation(rot);
+    bez.setModelBound(new BoundingSphere());
+    bez.updateModelBound();
+    rootNode.attachChild(bez);
 
-         scene.updateGeometricState(timer.getTimePerFrame(), true);
-    }
+    MaterialState ms = display.getRenderer().getMaterialState();
+    ms.setEmissive(new ColorRGBA(0.1f, 0.1f, 0.1f, 1.0f));
+//    ms.setAmbient(new ColorRGBA(0.5f, 0.5f, 0.5f, 1.0f));
+//    ms.setDiffuse(new ColorRGBA(1.0f, 0.85f, 0.75f, 1.0f));
+//    ms.setSpecular(new ColorRGBA(0.8f, 0.8f, 0.8f, 1.0f));
+    ms.setShininess(128.0f);
+    ms.setEnabled(true);
+    bez.setRenderState(ms);
 
-    /**
-     * clears the buffers and then draws the TriMesh.
-     * @see com.jme.app.SimpleGame#render()
-     */
-    protected void render(float interpolation) {
-        display.getRenderer().clearBuffers();
+    pl = new PointLight();
+    pl.setAmbient(new ColorRGBA(0, 0, 0, 1));
+    pl.setDiffuse(new ColorRGBA(1, 1, 1, 1));
+    pl.setSpecular(new ColorRGBA(.2f, .2f, .2f, 1));
+    pl.setEnabled(true);
 
-        display.getRenderer().draw(scene);
+    lightState.detachAll();
+    lightState.setTwoSidedLighting(true);
+    lightNode = new LightNode("Light Node", lightState);
+    lightNode.setLight(pl);
+    lightNode.setTarget(bez);
 
+    Vector3f min = new Vector3f( -0.15f, -0.15f, -0.15f);
+    Vector3f max = new Vector3f(0.15f, 0.15f, 0.15f);
+    Box lightBox = new Box("box", min, max);
+    lightBox.setModelBound(new BoundingSphere());
+    lightBox.updateModelBound();
 
-    }
+    lightNode.attachChild(lightBox);
+    lightNode.setForceView(true);
 
-    /**
-     * creates the displays and sets up the viewport.
-     * @see com.jme.app.SimpleGame#initSystem()
-     */
-    protected void initSystem() {
+    rootNode.attachChild(lightNode);
 
-        currentPos = new Vector3f();
-        newPos = new Vector3f();
-        try {
-            display = DisplaySystem.getDisplaySystem(properties.getRenderer());
-            display.createWindow(
-                properties.getWidth(),
-                properties.getHeight(),
-                properties.getDepth(),
-                properties.getFreq(),
-                properties.getFullscreen());
-            cam =
-                display.getRenderer().getCamera(
-                    properties.getWidth(),
-                    properties.getHeight());
+    TextureState ts = display.getRenderer().getTextureState();
+    ts.setEnabled(true);
+    ts.setTexture(
+        TextureManager.loadTexture(
+        TestBezierMesh.class.getClassLoader().getResource(
+        "jmetest/data/images/Monkey.jpg"),
+        Texture.MM_LINEAR,
+        Texture.FM_LINEAR,
+        true));
 
-        } catch (JmeException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        ColorRGBA blackColor = new ColorRGBA(0, 0, 0, 1);
-        display.getRenderer().setBackgroundColor(blackColor);
-        cam.setFrustum(1.0f, 1000.0f, -0.55f, 0.55f, 0.4125f, -0.4125f);
-        Vector3f loc = new Vector3f(0.0f, 0.0f, 5.0f);
-        Vector3f left = new Vector3f(1.0f, 0.0f, 0.0f);
-        Vector3f up = new Vector3f(0.0f, -1.0f, 0.0f);
-        Vector3f dir = new Vector3f(0.0f, 0f, -1.0f);
-        cam.setFrame(loc, left, up, dir);
-        display.getRenderer().setCamera(cam);
-
-        input = new FirstPersonHandler(this, cam, "LWJGL");
-        timer = Timer.getTimer("LWJGL");
-        display.setTitle("Bezier Mesh Test");
-    }
-
-    /**
-     * builds the trimesh.
-     * @see com.jme.app.SimpleGame#initGame()
-     */
-    protected void initGame() {
-
-        AlphaState as1 = display.getRenderer().getAlphaState();
-        as1.setBlendEnabled(true);
-        as1.setSrcFunction(AlphaState.SB_SRC_ALPHA);
-        as1.setDstFunction(AlphaState.DB_ONE);
-        as1.setTestEnabled(true);
-        as1.setTestFunction(AlphaState.TF_GREATER);
-
-        scene = new Node("3D Scene Node");
-        root = new Node("Scene Root Node");
-        root.attachChild(scene);
-
-        ZBufferState buf = display.getRenderer().getZBufferState();
-        buf.setEnabled(true);
-        buf.setFunction(ZBufferState.CF_LEQUAL);
-
-        scene.setRenderState(buf);
-        cam.update();
-
-        BezierPatch bp = new BezierPatch();
-        bp.setAnchor(0, 0, new Vector3f(-0.75f, -0.75f, -0.5f));
-        bp.setAnchor(0, 1, new Vector3f(-0.25f, -0.75f, 0.0f));
-        bp.setAnchor(0, 2, new Vector3f(0.25f, -0.75f, 0.5f));
-        bp.setAnchor(0, 3, new Vector3f(0.75f, -0.75f, 0.5f));
-        bp.setAnchor(1, 0, new Vector3f(-0.75f, -0.25f, -0.75f));
-        bp.setAnchor(1, 1, new Vector3f(-0.25f, -0.25f, 0.5f));
-        bp.setAnchor(1, 2, new Vector3f(0.25f, -0.25f, 0.5f));
-        bp.setAnchor(1, 3, new Vector3f(0.75f, -0.25f, 0.75f));
-        bp.setAnchor(2, 0, new Vector3f(-0.75f, 0.25f, -0.5f));
-        bp.setAnchor(2, 1, new Vector3f(-0.25f, 0.25f, -0.5f));
-        bp.setAnchor(2, 2, new Vector3f(0.25f, 0.25f, -0.5f));
-        bp.setAnchor(2, 3, new Vector3f(0.75f, 0.25f, 0.0f));
-        bp.setAnchor(3, 0, new Vector3f(-0.75f, 0.75f, -0.5f));
-        bp.setAnchor(3, 1, new Vector3f(-0.25f, 0.75f, -1.0f));
-        bp.setAnchor(3, 2, new Vector3f(0.25f, 0.75f, -1.0f));
-        bp.setAnchor(3, 3, new Vector3f(0.75f, 0.75f, -0.5f));
-        bp.setDetailLevel(100);
-
-        BezierMesh bez = new BezierMesh("Bezier Mesh");
-        bez.setPatch(bp);
-        bez.setModelBound(new BoundingSphere());
-        bez.updateModelBound();
-        scene.attachChild(bez);
-
-        MaterialState ms = display.getRenderer().getMaterialState();
-        ms.setEmissive(new ColorRGBA(0,0,0.4f,1.0f));
-        ms.setAmbient(new ColorRGBA(0.5f,0.5f,0.5f,1.0f));
-        ms.setDiffuse(new ColorRGBA(1.0f,0.85f,0.75f,1.0f));
-        ms.setSpecular(new ColorRGBA(0.8f,0.8f,0.8f,1.0f));
-        ms.setShininess(128.0f);
-        ms.setEnabled(true);
-        bez.setRenderState(ms);
-
-        pl = new PointLight();
-        pl.setAmbient(new ColorRGBA(0, 0, 0, 1));
-        pl.setDiffuse(new ColorRGBA(1, 1, 1, 1));
-        pl.setSpecular(new ColorRGBA(0, 0, 1, 1));
-        pl.setEnabled(true);
-
-        lightstate = display.getRenderer().getLightState();
-        lightstate.setTwoSidedLighting(true);
-        lightstate.setEnabled(true);
-        lightNode = new LightNode("Light Node",lightstate);
-        lightNode.setLight(pl);
-        lightNode.setTarget(bez);
-
-        Vector3f min = new Vector3f(-0.15f, -0.15f, -0.15f);
-        Vector3f max = new Vector3f(0.15f,0.15f,0.15f);
-        Box lightBox = new Box("box", min,max);
-        lightBox.setModelBound(new BoundingSphere());
-        lightBox.updateModelBound();
-
-
-        lightNode.attachChild(lightBox);
-        lightNode.setForceView(true);
-
-        scene.attachChild(lightNode);
-
-        TextureState ts = display.getRenderer().getTextureState();
-        ts.setEnabled(true);
-        ts.setTexture(
-            TextureManager.loadTexture(
-                TestBezierMesh.class.getClassLoader().getResource("jmetest/data/images/Monkey.jpg"),
-                Texture.MM_LINEAR,
-                Texture.FM_LINEAR,
-                true));
-
-        bez.setRenderState(ts);
-
-        scene.updateGeometricState(0.0f, true);
-        scene.updateRenderState();
-        scene.updateRenderState();
-
-    }
-    /**
-     * not used.
-     * @see com.jme.app.SimpleGame#reinit()
-     */
-    protected void reinit() {
-
-    }
-
-    /**
-     * Not used.
-     * @see com.jme.app.SimpleGame#cleanup()
-     */
-    protected void cleanup() {
-
-    }
-
+    bez.setRenderState(ts);
+  }
 }
