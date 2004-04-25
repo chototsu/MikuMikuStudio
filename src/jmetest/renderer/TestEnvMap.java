74bc2841e08c5188ff1da50bca8fc75919619e3c
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-04-25 03:06:42 +0000

    Test for environmental mapping
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@956 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/jmetest/renderer/TestEnvMap.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2003-2004, jMonkeyEngine - Mojo Monkey Coding All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
+ * names of its contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package jmetest.renderer;
+
+import com.jme.app.AbstractGame;
+import com.jme.app.SimpleGame;
+import com.jme.image.Texture;
+import com.jme.input.NodeHandler;
+import com.jme.light.PointLight;
+import com.jme.math.Vector3f;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.model.ase.ASEModel;
+import com.jme.scene.shape.Torus;
+import com.jme.scene.state.TextureState;
+import com.jme.util.TextureManager;
+
+/**
+ * <code>TestBackwardAction</code>
+ *
+ * @author Mark Powell
+ * @version $Id: TestEnvMap.java,v 1.1 2004-04-25 03:06:42 mojomonkey Exp $
+ */
+public class TestEnvMap extends SimpleGame {
+
+  private ASEModel model;
+
+  public static void main(String[] args) {
+    TestEnvMap app = new TestEnvMap();
+    app.setDialogBehaviour(AbstractGame.ALWAYS_SHOW_PROPS_DIALOG);
+    app.start();
+  }
+
+  protected void simpleInitGame() {
+    display.setTitle("Environmental Maps");
+    
+    cam.setLocation(new Vector3f(0, 0, 100));
+    cam.update();
+    input = new NodeHandler(this, rootNode, properties.getRenderer());
+    input.setKeySpeed(10);
+    input.setMouseSpeed(2);
+    
+    Torus torus = new Torus("Torus", 50, 50, 10, 20);
+    torus.copyTextureCoords(0,1);
+    
+    TextureState ts = display.getRenderer().getTextureState();
+    //Base texture, not environmental map.
+    Texture t0 = TextureManager.loadTexture(
+            TestEnvMap.class.getClassLoader().getResource(
+            "jmetest/data/images/Monkey.jpg"),
+        Texture.MM_LINEAR,
+        Texture.FM_LINEAR,
+        true);
+    //Environmental Map (reflection of clouds)
+    Texture t = TextureManager.loadTexture(
+            TestEnvMap.class.getClassLoader().getResource(
+            "jmetest/data/texture/clouds.png"),
+        Texture.MM_LINEAR,
+        Texture.FM_LINEAR,
+        true);
+    t.setApply(Texture.AM_DECAL);
+    t.setEnvironmentalMapMode(Texture.EM_SPHERE);
+    ts.setTexture(t0);
+    ts.setTexture(t,1);
+    ts.setEnabled(true);
+    
+    PointLight pl = new PointLight();
+    pl.setAmbient(new ColorRGBA(0.75f,0.75f,0.75f,1));
+    pl.setDiffuse(new ColorRGBA(1,0,0,1));
+    pl.setLocation(new Vector3f(50,0,0));
+    pl.setEnabled(true);
+    
+    lightState.attach(pl);
+    
+    torus.setRenderState(ts);
+    rootNode.attachChild(torus);
+  }
+}
