031cd2609d4a2f4b7e29f665a33b0b95470c3a31
mojomonkey <mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-04-26 14:39:57 +0000

    Cleaned up formatting
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@977 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/scene/BezierMesh.java
+++ src/com/jme/scene/BezierMesh.java
@@ -1,21 +1,21 @@
 /*
- * Copyright (c) 2003-2004, jMonkeyEngine - Mojo Monkey Coding
- * All rights reserved.
- *
+ * Copyright (c) 2003-2004, jMonkeyEngine - Mojo Monkey Coding All rights
+ * reserved.
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * Redistributions of source code must retain the above copyright notice, this
  * list of conditions and the following disclaimer.
- *
+ * 
  * Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- *
+ * 
  * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
  * names of its contributors may be used to endorse or promote products derived
  * from this software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,7 +27,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
+ *  
  */
 package com.jme.scene;
 
@@ -36,32 +36,39 @@ import com.jme.math.Vector3f;
 import com.jme.system.JmeException;
 
 /**
- * <code>BezierMesh</code> is defined by a collection of <code>BezierPatch</code>
- * objects that define a 4x4 patch of control anchors. These anchors define the
- * curve the surface of the mesh will take. The patch also contains information
- * about it's detail level, which defines how smooth the mesh will be.
+ * <code>BezierMesh</code> is defined by a collection of
+ * <code>BezierPatch</code> objects that define a 4x4 patch of control
+ * anchors. These anchors define the curve the surface of the mesh will take.
+ * The patch also contains information about it's detail level, which defines
+ * how smooth the mesh will be.
+ * 
  * @author Mark Powell
- * @version $Id: BezierMesh.java,v 1.10 2004-04-22 22:26:43 renanse Exp $
+ * @version $Id: BezierMesh.java,v 1.11 2004-04-26 14:39:57 mojomonkey Exp $
  */
 public class BezierMesh extends TriMesh {
+
     private BezierPatch patch;
 
     /**
      * Constructor creates a default <code>BezierMesh</code> object.
-     * @param name the name of the scene element. This is required for identification and
-     * 		comparision purposes.
+     * 
+     * @param name
+     *            the name of the scene element. This is required for
+     *            identification and comparision purposes.
      */
     public BezierMesh(String name) {
         super(name);
     }
 
     /**
-     * Constructor creates a new <code>BezierMesh</code> object with the
-     * given <code>BezierPatch</code>. The mesh is then automatically
-     * tessellated.
-     * @param name the name of the scene element. This is required for identification and
-     * 		comparision purposes.
-     * @param patch the <code>BezierPatch</code> used to define this mesh.
+     * Constructor creates a new <code>BezierMesh</code> object with the given
+     * <code>BezierPatch</code>. The mesh is then automatically tessellated.
+     * 
+     * @param name
+     *            the name of the scene element. This is required for
+     *            identification and comparision purposes.
+     * @param patch
+     *            the <code>BezierPatch</code> used to define this mesh.
      */
     public BezierMesh(String name, BezierPatch patch) {
         super(name);
@@ -70,10 +77,12 @@ public class BezierMesh extends TriMesh {
     }
 
     /**
-     *
+     * 
      * <code>setPatch</code> sets the <code>BezierPatch</code> of the mesh.
      * It is then tessellated.
-     * @param patch the patch to use for this mesh.
+     * 
+     * @param patch
+     *            the patch to use for this mesh.
      */
     public void setPatch(BezierPatch patch) {
         this.patch = patch;
@@ -81,18 +90,16 @@ public class BezierMesh extends TriMesh {
     }
 
     /**
-     *
-     * <code>tessellate</code> generates the <code>BezierMesh</code> vertices
-     * from the supplied patch and detail level. This method is called when
-     * patch is set, and therefore, should normally have to be called. However,
-     * if patch is changed externally, and you wish to update the mesh, a
-     * call to <code>tessellate</code> is appropriate.
-     *
+     * 
+     * <code>tessellate</code> generates the <code>BezierMesh</code>
+     * vertices from the supplied patch and detail level. This method is called
+     * when patch is set, and therefore, should normally have to be called.
+     * However, if patch is changed externally, and you wish to update the mesh,
+     * a call to <code>tessellate</code> is appropriate.
+     *  
      */
     public void tessellate() {
-        if(patch == null) {
-            return;
-        }
+        if (patch == null) { return; }
         int u = 0, v;
         float py, px, pyold;
         int detailLevel = patch.getDetailLevel();
@@ -112,8 +119,7 @@ public class BezierMesh extends TriMesh {
 
         u = 1;
         Vector3f[] vertex = new Vector3f[((detailLevel * 2) + 2) * detailLevel];
-        Vector2f[] texture =
-            new Vector2f[((detailLevel * 2) + 2) * detailLevel];
+        Vector2f[] texture = new Vector2f[((detailLevel * 2) + 2) * detailLevel];
         Vector3f[] normal = new Vector3f[vertex.length];
         int[] indices = new int[detailLevel * detailLevel * 6];
 
@@ -164,32 +170,40 @@ public class BezierMesh extends TriMesh {
                     if (i == 0) {
                         if (j < (detailLevel * 2)) {
                             //right cross up
-                            normal[normalIndex] =
-                                vertex[normalIndex + 1].subtract(vertex[normalIndex])
-                                        .cross(vertex[normalIndex + 2].subtract(vertex[normalIndex]))
-                                        .normalizeLocal();
+                            normal[normalIndex] = vertex[normalIndex + 1]
+                                    .subtract(vertex[normalIndex])
+                                    .cross(
+                                            vertex[normalIndex + 2]
+                                                    .subtract(vertex[normalIndex]))
+                                    .normalizeLocal();
                         } else {
                             //down cross right
-                            normal[normalIndex] =
-                                vertex[normalIndex - 2].subtract(vertex[normalIndex])
-                                        .cross(vertex[normalIndex + 1].subtract(vertex[normalIndex]))
-                                        .normalizeLocal();
+                            normal[normalIndex] = vertex[normalIndex - 2]
+                                    .subtract(vertex[normalIndex])
+                                    .cross(
+                                            vertex[normalIndex + 1]
+                                                    .subtract(vertex[normalIndex]))
+                                    .normalizeLocal();
                         }
                     } else {
-                        normal[normalIndex] = (Vector3f)
-                            normal[normalIndex - (detailLevel * 2 + 1)].clone();
+                        normal[normalIndex] = (Vector3f) normal[normalIndex
+                                - (detailLevel * 2 + 1)].clone();
                     }
                 } else {
                     if (j < (detailLevel * 2) + 1) {
                         //up cross left
-                        normal[normalIndex] =
-                            vertex[normalIndex + 2].subtract(vertex[normalIndex])
-                                .cross(vertex[normalIndex - 1].subtract(vertex[normalIndex])).normalizeLocal();
+                        normal[normalIndex] = vertex[normalIndex + 2].subtract(
+                                vertex[normalIndex]).cross(
+                                vertex[normalIndex - 1]
+                                        .subtract(vertex[normalIndex]))
+                                .normalizeLocal();
                     } else {
                         //left cross down
-                        normal[normalIndex] =
-                            vertex[normalIndex - 1].subtract(vertex[normalIndex])
-                                .cross(vertex[normalIndex - 2].subtract(vertex[normalIndex])).normalizeLocal();
+                        normal[normalIndex] = vertex[normalIndex - 1].subtract(
+                                vertex[normalIndex]).cross(
+                                vertex[normalIndex - 2]
+                                        .subtract(vertex[normalIndex]))
+                                .normalizeLocal();
                     }
                 }
                 normalIndex++;
@@ -203,17 +217,19 @@ public class BezierMesh extends TriMesh {
     }
 
     /**
-     *
-     * <code>calcBerstein</code> calculates the Berstein number for the
-     * given u and control points.
-     * @param u the u value.
-     * @param p the control points.
+     * 
+     * <code>calcBerstein</code> calculates the Berstein number for the given
+     * u and control points.
+     * 
+     * @param u
+     *            the u value.
+     * @param p
+     *            the control points.
      * @return the Berstein number.
      */
     private Vector3f calcBerstein(float u, Vector3f[] p) {
-        if (p.length != 4) {
-            throw new JmeException("Point parameter must be length 4.");
-        }
+        if (p.length != 4) { throw new JmeException(
+                "Point parameter must be length 4."); }
         Vector3f a = p[0].mult((float) Math.pow(u, 3));
         Vector3f b = p[1].mult(3 * (float) Math.pow(u, 2) * (1 - u));
         Vector3f c = p[2].mult(3 * u * (float) Math.pow((1 - u), 2));
