7bae0a9d24e31e34dca42d0eff767a50eaaf8ca7
renanse <renanse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-05-01 03:48:47 +0000

    added linear interpolation function
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1012 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/math/FastMath.java
+++ src/com/jme/math/FastMath.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (c) 2003-2004, jMonkeyEngine - Mojo Monkey Coding All rights
  * reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * Redistributions of source code must retain the above copyright notice, this
  * list of conditions and the following disclaimer.
- * 
+ *
  * Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- * 
+ *
  * Neither the name of the Mojo Monkey Coding, jME, jMonkey Engine, nor the
  * names of its contributors may be used to endorse or promote products derived
  * from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,7 +27,7 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *  
+ *
  */
 
 package com.jme.math;
@@ -36,9 +36,9 @@ import java.util.Random;
 
 /**
  * <code>FastMath</code>
- * 
+ *
  * @author Various
- * @version $Id: FastMath.java,v 1.9 2004-04-29 15:33:09 mojomonkey Exp $
+ * @version $Id: FastMath.java,v 1.10 2004-05-01 03:48:47 renanse Exp $
  */
 
 public class FastMath {
@@ -72,6 +72,11 @@ public class FastMath {
     public static boolean isPowerOfTwo(int number) {
         return (number > 0) && (number & (number - 1)) == 0;
     }
+
+    public static float LERP(float f, float v0, float v1) {
+      return ( (1 - (f)) * (v0) + (f) * (v1));
+    }
+
 
     /** */
     public static float acos(float fValue) {
@@ -388,7 +393,7 @@ public class FastMath {
 
     /**
      * FastTrig
-     * 
+     *
      * @Author Erikd
      */
     static public class FastTrig {
