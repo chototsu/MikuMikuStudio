14f32d854f43f0d7c0822f529bd6a19d03de80fd
darkprophet <darkprophet@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-05-04 16:40:05 +0000

    FadeInOutController to control FadeInOut, this one works! :)
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1027 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/effects/transients/FadeInOutController.java
+++ src/com/jme/effects/transients/FadeInOutController.java
@@ -44,28 +44,31 @@ public class FadeInOutController extends Controller {
 
     private FadeInOut fio;
     private ColorRGBA color;
-
+    
     public FadeInOutController(FadeInOut f) {
         fio = f;
         color = (ColorRGBA)fio.getFadeColor().clone();
     }
+    
+    public String getStats() {
+    	return ("Alpha: " + fio.getFadeQuad().getColors()[0].a);
+    }
 
     public void update(float timeF) {
-        System.out.println("Alpha: " + fio.getFadeColor().a);
         float time = timeF * fio.getSpeed();
         color = fio.getFadeColor();
         if (fio.getCurrentStage() == 0) {
-            color.a -= time;
-            fio.setFadeColor(color);
-            if (fio.getFadeColor().a <= 0.0f) {
-                fio.detachChild(fio.getOutQuad());
-                fio.attachChild(fio.getInQuad());
-                fio.setCurrentStage(fio.getCurrentStage() + 1);
-            }
-        }else if (fio.getCurrentStage() == 1) {
             color.a += time;
             fio.setFadeColor(color);
             if (fio.getFadeColor().a >= 1.0f) {
+                fio.detachChild(fio.getFadeOutNode());
+                fio.attachChild(fio.getFadeInNode());
+                fio.setCurrentStage(fio.getCurrentStage() + 1);
+            }
+        }else if (fio.getCurrentStage() == 1) {
+            color.a -= time;
+            fio.setFadeColor(color);
+            if (fio.getFadeColor().a <= 0.0f) {
                 fio.setCurrentStage(fio.getCurrentStage() + 1);
             }
         }
