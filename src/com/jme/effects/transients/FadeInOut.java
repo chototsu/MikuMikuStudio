7a6198b573f5aef162518c10fab1256143ea1721
darkprophet <darkprophet@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-05-04 16:39:39 +0000

    new FadeInOut effect. This one works! :)
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1026 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/effects/transients/FadeInOut.java
+++ src/com/jme/effects/transients/FadeInOut.java
@@ -34,11 +34,10 @@
  */
 package com.jme.effects.transients;
 
-import com.jme.math.Vector3f;
 import com.jme.renderer.ColorRGBA;
-import com.jme.scene.shape.Quad;
+import com.jme.scene.Geometry;
+import com.jme.scene.Node;
 import com.jme.scene.state.AlphaState;
-import com.jme.scene.state.TextureState;
 import com.jme.system.DisplaySystem;
 
 /**
@@ -46,92 +45,77 @@ import com.jme.system.DisplaySystem;
  */
 public class FadeInOut extends Transient {
 
-    private Quad fadeQ, outQ, inQ;
-    private ColorRGBA fadeColors[];
+    private Geometry fadeQ;
+    private Node fadeInNode, fadeOutNode;
+    private ColorRGBA fadeColor;
     private float speed;
 
-    public FadeInOut(String name, TextureState out, TextureState in, ColorRGBA c) {
+    public FadeInOut(String name, Geometry fade, Node out, Node in, ColorRGBA c) {
         super(name);
-        initialise(out, in, c, 0.01f);
+        initialise(fade, out, in, c, 0.01f);
     }
 
-    public FadeInOut(String name, TextureState out, TextureState in, ColorRGBA c, float s) {
+    public FadeInOut(String name, Geometry fade, Node out, Node in, ColorRGBA c, float s) {
         super(name);
-        initialise(out, in, c, s);
+        initialise(fade, out, in, c, s);
     }
 
-    private void initialise(TextureState out, TextureState in, ColorRGBA c, float speed) {
+    private void initialise(Geometry fade, Node out, Node in, ColorRGBA c, float speed) {
         setMaxNumOfStages(2);
         setCurrentStage(0);
         setSpeed(speed);
 
-        fadeColors = new ColorRGBA[4];
-        for (int i = 0; i < fadeColors.length; i++) {
-            fadeColors[i] = new ColorRGBA();
-            fadeColors[i].r = c.r;
-            fadeColors[i].g = c.g;
-            fadeColors[i].b = c.b;
-            fadeColors[i].a = 1;
-        }
+        fadeColor = (ColorRGBA)c.clone();
+        fadeColor.a = 0;
+        
+        fadeInNode = in;
+        fadeOutNode = out;
+        fadeQ = fade;
 
-        Vector3f origin = new Vector3f(0, 0, 0);
-
-        AlphaState as = DisplaySystem.getDisplaySystem().getRenderer().getAlphaState();
-        as.setBlendEnabled(true);
-        as.setEnabled(true);
-
-        fadeQ = new Quad(getName() + " Fade Quad");
-        fadeQ.initialize(5, 5);
-        fadeQ.setColors(fadeColors);
-        fadeQ.setRenderState(as);
-        fadeQ.setLocalTranslation(origin);
-
-        outQ = new Quad(getName() + " Out Quad");
-        outQ.initialize(5, 5);
-        outQ.setRenderState(out);
-        fadeQ.setLocalTranslation(origin);
-
-        inQ = new Quad(getName() + " In Quad");
-        inQ.initialize(5, 5);
-        inQ.setRenderState(in);
-        inQ.setLocalTranslation(origin);
-
-        this.attachChild(outQ);
-        this.attachChild(fadeQ);
+        AlphaState fadeAS = DisplaySystem.getDisplaySystem().getRenderer().getAlphaState();
+		fadeAS.setBlendEnabled(true);
+		fadeAS.setSrcFunction(AlphaState.SB_SRC_ALPHA);
+		fadeAS.setDstFunction(AlphaState.DB_ONE_MINUS_SRC_ALPHA);
+		fadeAS.setTestEnabled(false);
+		fadeAS.setTestFunction(AlphaState.TF_GEQUAL);
+		fadeAS.setEnabled(true);
+		
+		fadeQ.setRenderState(fadeAS);
+        
+        this.attachChild(fadeOutNode);
     }
-
-    public Quad getFadeQuad() {
+    
+    // getters/setters for the fadeQuad
+    public Geometry getFadeQuad() {
         return fadeQ;
     }
-    public void setFadeQuad(Quad f) {
+    public void setFadeQuad(Geometry f) {
         fadeQ = f;
     }
-
-    public Quad getOutQuad() {
-        return outQ;
+    
+    // getters/setters for fadeInNode
+    public Node getFadeInNode() {
+    	return fadeInNode;
     }
-    public void setOutQuad(Quad o) {
-        outQ = o;
+    public void setFadeInNode(Node fade) {
+    	fadeInNode = fade;
     }
-
-    public Quad getInQuad() {
-        return inQ;
+    
+    // getters/setters for fadeOutNode
+    public Node getFadeOutNode() {
+    	return fadeOutNode;
     }
-    public void setInQuad(Quad i) {
-        inQ = i;
+    public void setFadeOutNode(Node fade) {
+    	fadeOutNode = fade;
     }
 
     public ColorRGBA getFadeColor() {
-        return fadeColors[0];
+        return fadeColor;
     }
 
     public void setFadeColor(ColorRGBA c) {
-        for (int i = 0; i < fadeColors.length; i++) {
-            if (fadeColors[i] == null) {
-                fadeColors[i] = new ColorRGBA();
-            }
-            fadeColors[i] = (ColorRGBA)c.clone();
-        }
+    	fadeColor = (ColorRGBA)c.clone();
+    	fadeQ.setSolidColor(fadeColor);
     }
 
     public float getSpeed() {
@@ -142,8 +126,10 @@ public class FadeInOut extends Transient {
     }
 
     public void updateWorldData(float time) {
-        if (getController(0) != null) {
-            getController(0).update(time);
+        if (getControllers().size() != 0) {
+        	for (int i = 0; i < getControllers().size(); i++) {
+        		(getController(i)).update(time);
+        	}
         }
     }
 
