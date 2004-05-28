43e707bf8a79f0ef1b42c373b8767e81f2b4880c
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-05-28 04:12:41 +0000

    Added more graceful way to handle missing texture files.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1133 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/scene/model/ms3d/MilkshapeMaterial.java
+++ src/com/jme/scene/model/ms3d/MilkshapeMaterial.java
@@ -8,9 +8,12 @@ import com.jme.scene.state.TextureState;
 import com.jme.scene.TriMesh;
 import com.jme.image.Texture;
 import com.jme.util.TextureManager;
+import com.jme.util.LoggingSystem;
 
 import java.net.URL;
 import java.net.MalformedURLException;
+import java.io.File;
+import java.util.logging.Level;
 
 
 /**
@@ -48,12 +51,23 @@ class MilkshapeMaterial {
         if (myTex!=null || texture.length()==0) return myTex;
         myTex=DisplaySystem.getDisplaySystem().getRenderer().getTextureState();
         try {
+            URL textureLocation;
+            if (new File(texture).isAbsolute())
+                textureLocation=new File(texture).toURL();
+            else
+                textureLocation=new URL(baseURL,texture);
             myTex.setTexture(TextureManager.loadTexture(new URL(baseURL,texture),
                 Texture.MM_LINEAR,
                 Texture.FM_LINEAR,
                 true));
         } catch (MalformedURLException e) {
-            throw new JmeException(e.toString()+"\nbase directory wrong(?)"+baseURL.toString());
+            LoggingSystem.getLogger().log(
+                Level.WARNING,
+                "Couldn't load texture: BaseURL=" + baseURL.toString() + ":" +
+                    "Texture name:" + texture);
+            myTex=null;
+            texture="";
+            return null;
         }
         if (myTex==null) throw new JmeException("Problem loading file " + texture);
         myTex.setEnabled(true);
