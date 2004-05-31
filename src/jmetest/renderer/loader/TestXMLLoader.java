d22402b86015d5142ab1eb17b909e657b8419fc3
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-05-31 07:59:27 +0000

    Alpha version of XML loader test class
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1141 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/jmetest/renderer/loader/TestXMLLoader.java
@@ -0,0 +1,33 @@
+package jmetest.renderer.loader;
+
+import com.jme.scene.model.SAXReader;
+import com.jme.app.SimpleGame;
+
+import java.io.File;
+import java.io.IOException;
+
+
+/**
+ *
+ * Test class for XML loading with jME
+ * Started Date: May 30, 2004
+ * @author Jack Lindamood
+ */
+public class TestXMLLoader extends SimpleGame{
+
+    SAXReader r=new SAXReader();
+
+    public static void main(String[] args){
+        new TestXMLLoader().start();
+    }
+
+    protected void simpleInitGame() {
+        try {
+            r.loadXML(new File("CVS root/data/XML docs/SampleXMLScene.xml").toURL().openStream());
+        } catch (IOException e) {
+            System.out.println("bad File exception" + e.getCause() + "*" + e.getMessage());
+        }
+        rootNode.attachChild(r.fetchCopy());
+
+    }
+}
