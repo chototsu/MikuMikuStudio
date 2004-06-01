16e0d0deec7c225dd714852c72d483b0c5214a0f
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-01 20:57:06 +0000

    Added Specular to view XML file better
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1153 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/loader/TestXMLLoader.java
+++ src/jmetest/renderer/loader/TestXMLLoader.java
@@ -2,6 +2,7 @@ package jmetest.renderer.loader;
 
 import com.jme.scene.model.SAXReader;
 import com.jme.app.SimpleGame;
+import com.jme.renderer.ColorRGBA;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,6 +22,7 @@ public class TestXMLLoader extends SimpleGame{
     }
 
     protected void simpleInitGame() {
+        lightState.get(0).setSpecular(new ColorRGBA(1,1,1,1));
         SAXReader r=new SAXReader();
         try {
             r.loadXML(new File("CVS root/data/XML docs/SampleXMLScene.xml").toURL().openStream());
