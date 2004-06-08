dd3d9dff79fcc0648ce9f189f1406a1149ae92dd
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-08 22:03:15 +0000

    Cleaned up the test.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1197 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/loader/TestMilkLoader.java
+++ src/jmetest/renderer/loader/TestMilkLoader.java
@@ -4,6 +4,7 @@ import com.jme.app.SimpleGame;
 import com.jme.scene.shape.Box;
 import com.jme.scene.Node;
 import com.jme.math.Vector3f;
+import com.jme.math.Quaternion;
 import com.jme.scene.model.ms3d.MilkLoader;
 import com.jme.scene.model.ms3d.MilkAnimation;
 import com.jme.scene.model.Loader;
@@ -26,7 +27,7 @@ public class TestMilkLoader extends SimpleGame{
     }
 
     // The new loader
-    protected void simpleInitGame1() {
+    protected void simpleInitGame() {
         //  Idealy this could be replaced by
         // Loader mi=new AutoCadLoader(); ect
         Loader mi=new MilkLoader();
@@ -38,42 +39,18 @@ public class TestMilkLoader extends SimpleGame{
         "jmetest/data/model/msascii/"));
         Node mi1=mi.load(MSFile);
         mi1.setLocalScale(.1f);
-        mi1.getController(0).setSpeed(5f);
+        mi1.getController(0).setSpeed(10f);
         ((MilkAnimation) mi1.getController(0)).setSkipRate(.01f);
         rootNode.attachChild(mi1);
 
         Node mi2=mi.fetchCopy();
-        mi2.setLocalTranslation(new Vector3f(3,3,-200));
+        mi2.setLocalTranslation(new Vector3f(0,0,-20));
         ((MilkAnimation) mi2.getController(0)).setSkipRate(.05f);
         mi2.setLocalScale(.5f);
         rootNode.attachChild(mi2);
-    }
-
-    // The old loader
-    protected void simpleInitGame2() {
-
-        MilkshapeASCIIModel mi1=new MilkshapeASCIIModel("runguy");
-        mi1.load("models/run.txt","models");
-       ( (DeformationJointController) mi1.getAnimationController()).
-            setUpdateModelBounds(true);
-        mi1.setLocalScale(.1f);
-        rootNode.attachChild(mi1);
-
-//        MilkshapeASCIIModel mi2=new MilkshapeASCIIModel("ninja");
-//        mi2.load("models/run.txt","models");
-//       ( (DeformationJointController) mi2.getAnimationController()).
-//            setUpdateModelBounds(true);
-//        mi2.setLocalTranslation(new Vector3f(3,3,3));
-//        mi2.setLocalScale(.5f);
-//        rootNode.attachChild(mi2);
-    }
-
-    protected void simpleInitGame() {
-        long time=System.currentTimeMillis();
-        simpleInitGame1();
         rootNode.attachChild(new Box("axisX",new Vector3f(5,0,0),5f,.1f,.1f));
         rootNode.attachChild(new Box("axisY",new Vector3f(0,5,0),.1f,5f,.1f));
         rootNode.attachChild(new Box("axisZ",new Vector3f(0,0,5),.1f,.1f,5f));
-        System.out.println("TIME:" + (System.currentTimeMillis()-time));
+
     }
 }
