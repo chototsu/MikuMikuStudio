558d7e7cd5d201639a1101f45908b2350114422e
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-12 21:09:02 +0000

    Program to test .ms3d load/save to XML
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1220 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/jmetest/renderer/loader/TestMilkXMLWrite.java
@@ -0,0 +1,51 @@
+package jmetest.renderer.loader;
+
+import com.jme.app.SimpleGame;
+import com.jme.scene.model.XMLparser.MilkToXML;
+import com.jme.scene.model.XMLparser.SAXReader;
+import com.jme.scene.Node;
+import com.jme.scene.shape.Box;
+import com.jme.math.Vector3f;
+
+import java.net.URL;
+import java.io.*;
+
+/**
+ * Started Date: Jun 8, 2004
+ * This class test the ability to correctly read and write .ms3d scenegraph files.
+ * 
+ * @author Jack Lindamood
+ */
+public class TestMilkXMLWrite extends SimpleGame{
+    public static void main(String[] args) {
+        new TestMilkXMLWrite().start();
+    }
+
+    protected void simpleInitGame() {
+
+        MilkToXML converter=new MilkToXML();
+        URL MSFile=TestMilkLoader.class.getClassLoader().getResource(
+        "jmetest/data/model/msascii/run.ms3d");
+        ByteArrayOutputStream blah=new ByteArrayOutputStream();
+
+        try {
+            converter.writeFiletoStream(MSFile,blah);
+        } catch (IOException e) {
+            System.out.println("IO problem writting the file!!!");
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        System.out.println(blah);
+        SAXReader toScreen=new SAXReader();
+        Node mi=toScreen.loadXML(new ByteArrayInputStream(blah.toByteArray()));
+        mi.setLocalScale(.1f);
+        rootNode.attachChild(mi);
+//        drawAxis();
+    }
+
+    private void drawAxis() {
+        rootNode.attachChild(new Box("axisX",new Vector3f(5,0,0),5f,.1f,.1f));
+        rootNode.attachChild(new Box("axisY",new Vector3f(0,5,0),.1f,5f,.1f));
+        rootNode.attachChild(new Box("axisZ",new Vector3f(0,0,5),.1f,.1f,5f));
+    }
+}
