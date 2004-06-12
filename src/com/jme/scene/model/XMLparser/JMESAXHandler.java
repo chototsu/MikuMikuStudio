0759ec09e2e7d097af250a9ef3f444666b7eaa35
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-12 21:10:04 +0000

    Added DEBUG flag and better javadoc
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1222 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/scene/model/XMLparser/JMESAXHandler.java
+++ src/com/jme/scene/model/XMLparser/JMESAXHandler.java
@@ -11,13 +11,16 @@ import java.util.logging.Level;
 /**
  * Started Date: Jun 6, 2004
  *
- * This class does the actual SAX Processing for SAXReader
+ * This class does the actual SAX Processing for SAXReader.  The basic order of things is to increase
+ * its SAXStackProcessor whenever a begining element is encountered and decrease its processor whenever an
+ * ending element is encountered
  * 
  * @author Jack Lindamood
  */
 class JMESAXHandler extends DefaultHandler{
     private StringBuffer currentData;
     SAXStackProcessor myComputer;
+    private static final boolean DEBUG = false;
 
     public JMESAXHandler(SAXStackProcessor s){
         super();
@@ -41,15 +44,15 @@ class JMESAXHandler extends DefaultHandler{
     public void startElement(String uri,String localName,String qName, Attributes atts) throws SAXException{
         currentData.setLength(0);
         currentData.setLength(16);
-        System.out.print("Start: " + qName);
+        if (DEBUG) System.out.print("Start: " + qName);
         myComputer.increaseStack(qName,atts);
-        System.out.println("*done-increaseStack");
+        if (DEBUG) System.out.println("*done-increaseStack");
     }
 
     public void endElement(String uri,String localName, String qName) throws SAXException{
-        System.out.print("End: " + qName);
+        if (DEBUG) System.out.print("End: " + qName);
         myComputer.decreaseStack(qName,currentData);
-        System.out.println("*done-decreaseStack*");
+        if (DEBUG) System.out.println("*done-decreaseStack*");
     }
 
     public void characters(char[] ch, int start,int length) throws SAXException{
