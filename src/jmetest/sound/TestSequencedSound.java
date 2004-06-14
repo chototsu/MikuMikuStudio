dda7a2f734ea2ed0c03f4c6ebbc87667fb474602
anakan <anakan@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-14 23:02:15 +0000

    new tests with several samples
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1255 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/sound/TestSequencedSound.java
+++ src/jmetest/sound/TestSequencedSound.java
@@ -51,7 +51,7 @@ import com.jme.util.Timer;
 
 /**
  * @author Arman Ozcelik
- * @version $Id: TestSequencedSound.java,v 1.2 2004-06-12 23:51:14 anakan Exp $
+ * @version $Id: TestSequencedSound.java,v 1.3 2004-06-14 23:02:15 anakan Exp $
  */
 public class TestSequencedSound extends SimpleGame {
 
@@ -59,7 +59,16 @@ public class TestSequencedSound extends SimpleGame {
 
     SphericalSound sequenced;
 
-    Box box;
+    SphericalSound sequenced2;
+    
+    SphericalSound sequenced3;
+    
+    SphericalSound sequenced4;
+    
+
+    SphericalSound background;
+
+    Box box, box2, box3, box4;
 
     Timer timer;
 
@@ -72,9 +81,8 @@ public class TestSequencedSound extends SimpleGame {
     protected void simpleUpdate() {
         float time = timer.getTimeInSeconds();
         snode.updateGeometricState(time, true);
-        fps.print("Playing seqence:" + sequenced.getActiveSequence()
-                + " Duration: " + sequenced.getPlayingTime()
-                + " millis.(Please go near the box)");
+        //fps.print("Playing seqence: Duration: " + sequenced.getPlayingTime()
+        //        + " millis.(Please go near the box)");
 
     }
 
@@ -90,6 +98,39 @@ public class TestSequencedSound extends SimpleGame {
      */
     protected void simpleInitGame() {
         timer = Timer.getTimer("LWJGL");
+        
+        
+        URL[] urls = new URL[2];
+        urls[0] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/foot1.wav");
+        urls[1] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/foot2.wav");
+
+        URL[] urls2 = new URL[2];
+        urls2[0] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/iagree.wav");
+        urls2[1] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/medical.wav");
+
+        URL[] urls3 = new URL[4];
+        urls3[0] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/ccday01.ogg");
+        urls3[1] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/ccday02.ogg");
+        urls3[2] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/ccday03.ogg");
+        urls3[3] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/ccday04.ogg");
+        
+        
+        URL[] urls4 = new URL[1];
+        urls4[0] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/CHAR_CRE_1.ogg");
+        
+        URL[] urls5 = new URL[1];
+        urls5[0] = TestSequencedSound.class.getClassLoader().getResource(
+                "jmetest/data/sound/CHAR_CRE_11.ogg");
+        
 
         display.setTitle("Test Sound Graph");
         SoundAPIController.getSoundSystem(properties.getRenderer());
@@ -99,7 +140,24 @@ public class TestSequencedSound extends SimpleGame {
         box = new Box("Box", min, max);
         box.setModelBound(new BoundingSphere());
         box.updateModelBound();
-        box.setLocalTranslation(new Vector3f(0, 0, -50));
+        box.setLocalTranslation(new Vector3f(0, 0, -100));
+
+        box2 = new Box("Box", min, max);
+        box2.setModelBound(new BoundingSphere());
+        box2.updateModelBound();
+        box2.setLocalTranslation(new Vector3f(20, 0, -25));
+        
+        box3 = new Box("Box", min, max);
+        box3.setModelBound(new BoundingSphere());
+        box3.updateModelBound();
+        box3.setLocalTranslation(new Vector3f(10, 0, -150));
+        
+        box4 = new Box("Box", min, max);
+        box4.setModelBound(new BoundingSphere());
+        box4.updateModelBound();
+        box4.setLocalTranslation(new Vector3f(50, 0, -200));
+        
+
         TextureState tst = display.getRenderer().getTextureState();
         tst.setEnabled(true);
         tst.setTexture(TextureManager.loadTexture(
@@ -108,23 +166,55 @@ public class TestSequencedSound extends SimpleGame {
                 Texture.FM_LINEAR, true));
         rootNode.setRenderState(tst);
         rootNode.attachChild(box);
+        rootNode.attachChild(box2);
+        rootNode.attachChild(box3);
+        rootNode.attachChild(box4);
         snode = new SoundNode();
-        URL[] urls = new URL[2];
-        urls[0] = TestSequencedSound.class.getClassLoader().getResource(
-                "jmetest/data/sound/Footsteps.wav");
-        urls[1] = TestSequencedSound.class.getClassLoader().getResource(
-                "jmetest/data/sound/test.ogg");
-        int[] times = { 3000, 5000};
+        
 
-        sequenced = new SphericalSound(urls, times);
-
-        sequenced.setMaxDistance(100);
+        sequenced = new SphericalSound(urls4);
+        sequenced.setQueueCheckPercentage(1);
+        sequenced.setMaxDistance(10);
         sequenced.setRolloffFactor(.1f);
         sequenced.setPosition(box.getLocalTranslation());
         sequenced.setGain(1.0f);
         sequenced.setLoopingEnabled(true);
-        sequenced.setLooping(true);
+        
+        
+        sequenced3 = new SphericalSound(urls5);
+        sequenced3.setQueueCheckPercentage(1);
+        sequenced3.setMaxDistance(20);
+        sequenced3.setRolloffFactor(.1f);
+        sequenced3.setPosition(box3.getLocalTranslation());
+        sequenced3.setGain(1.0f);
+        sequenced3.setLoopingEnabled(false);
+        
+        sequenced4 = new SphericalSound(urls);
+        sequenced4.setQueueCheckPercentage(1);
+        sequenced4.setMaxDistance(25);
+        sequenced4.setRolloffFactor(.1f);
+        sequenced4.setPosition(box4.getLocalTranslation());
+        sequenced4.setGain(1.0f);
+        sequenced4.setLoopingEnabled(false);
+        
+        
+        //sequenced.setLooping(true);
         snode.attachChild(sequenced);
+        sequenced2 = new SphericalSound(urls2);
+        sequenced2.setQueueCheckPercentage(1);
+        sequenced2.setMaxDistance(10);
+        sequenced2.setRolloffFactor(.1f);
+        sequenced2.setPosition(box2.getLocalTranslation());
+        sequenced2.setGain(1.0f);
+        sequenced2.setLoopingEnabled(true);
+
+        background = new SphericalSound(urls3);
+        background.setLoopingEnabled(true);
+        background.setGain(0.5f);
+        snode.attachChild(sequenced2);
+        snode.attachChild(sequenced3);
+        snode.attachChild(sequenced4);
+        snode.attachChild(background);
 
     }
 }
