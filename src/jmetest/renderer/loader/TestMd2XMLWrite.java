c81e66ae3708184633dd70da724f2c3a92180dfb
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-21 02:32:29 +0000

    Adds smooth transform test
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1270 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/loader/TestMd2XMLWrite.java
+++ src/jmetest/renderer/loader/TestMd2XMLWrite.java
@@ -1,17 +1,16 @@
 package jmetest.renderer.loader;
 
 import com.jme.app.SimpleGame;
-import com.jme.scene.model.XMLparser.Md2ToXML;
-import com.jme.scene.model.XMLparser.SAXReader;
-import com.jme.scene.model.md2.Md2Model;
+import com.jme.scene.model.XMLparser.*;
 import com.jme.scene.Node;
 import com.jme.scene.state.TextureState;
 import com.jme.image.Texture;
 import com.jme.util.TextureManager;
+import com.jme.input.KeyBindingManager;
+import com.jme.input.KeyInput;
+import com.jme.math.Vector3f;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ByteArrayInputStream;
+import java.io.*;
 import java.net.URL;
 
 /**
@@ -23,8 +22,12 @@ import java.net.URL;
 public class TestMd2XMLWrite extends SimpleGame{
     float totalFPS;
     long totalCounts;
+    private KeyframeController kc;
+
     public static void main(String[] args) {
-        new TestMd2XMLWrite().start();
+        TestMd2XMLWrite app=new TestMd2XMLWrite();
+        app.setDialogBehaviour(TestMd2XMLWrite.FIRSTRUN_OR_NOCONFIGFILE_SHOW_PROPS_DIALOG);
+        app.start();
     }
     protected void simpleUpdate() {
         totalFPS+=timer.getFrameRate();
@@ -33,78 +36,46 @@ public class TestMd2XMLWrite extends SimpleGame{
             System.out.println("FPS: " + (totalFPS/totalCounts));
             totalFPS = totalCounts = 0;
         }
+     if (KeyBindingManager
+             .getKeyBindingManager()
+             .isValidCommand("smoothTrans", false)) {
+         kc.smoothTransform(1f,kc.getMaxTime(),25);
+     }
+
     }
-    protected void simpleInitGame1() {  // New one
+    protected void simpleInitGame() {
         Md2ToXML converter=new Md2ToXML();
         ByteArrayOutputStream BO=new ByteArrayOutputStream();
-        URL freak=TestMd2XMLWrite.class.getClassLoader().getResource("jmetest/data/model/drfreak.md2");
-        URL textu=TestMd2XMLWrite.class.getClassLoader().getResource("jmetest/data/model/drfreak.jpg");
-        try {
-            converter.writeFiletoStream(freak,BO);
-        } catch (IOException e) {
-            System.out.println("Damn exceptions!");
-        }
-        SAXReader sr=new SAXReader();
-        Node freakmd2=sr.loadXML(new ByteArrayInputStream(BO.toByteArray()));
-        TextureState ts = display.getRenderer().getTextureState();
-        ts.setEnabled(true);
-        ts.setTexture(
-        TextureManager.loadTexture(
-            textu,
-            Texture.MM_LINEAR,
-            Texture.FM_LINEAR,
-            true));
-        freakmd2.setRenderState(ts);
-        freakmd2.setLocalScale(.5f);
-        freakmd2.getChild(0).getController(0).setSpeed(10);
-        rootNode.attachChild(freakmd2);
-    }
-    protected void simpleInitGame2() {  // Old one
-        URL freak=TestMd2XMLWrite.class.getClassLoader().getResource("jmetest/data/model/drfreak.md2");
-        URL textu=TestMd2XMLWrite.class.getClassLoader().getResource("jmetest/data/model/drfreak.jpg");
-        Md2Model freakmd2=new Md2Model("freak");
-        long time=System.currentTimeMillis();
-        freakmd2.load(freak);
-        System.out.println("Load time: " + (System.currentTimeMillis()-time));
-        TextureState ts = display.getRenderer().getTextureState();
-        ts.setEnabled(true);
-        ts.setTexture(
-        TextureManager.loadTexture(
-            textu,
-            Texture.MM_LINEAR,
-            Texture.FM_LINEAR,
-            true));
-        freakmd2.setRenderState(ts);
-        freakmd2.setLocalScale(.5f);
-        rootNode.attachChild(freakmd2);
-    }
 
-    protected void simpleInitGame() {
-        simpleInitGame1();
+        URL textu=TestMd2XMLWrite.class.getClassLoader().getResource("jmetest/data/model/drfreak.jpg");
+        URL freak=TestMd2XMLWrite.class.getClassLoader().getResource("jmetest/data/model/drfreak.md2");
+        Node freakmd2=null;
+
+        try {
+            converter.writeFiletoStream(freak,new OutputStreamWriter(BO));
+        } catch (IOException e) {
+            System.out.println("damn exceptions!");
+        }
+        SAXReader s=new SAXReader();
+        freakmd2=s.loadXML(new ByteArrayInputStream(BO.toByteArray()));
+
+        TextureState ts = display.getRenderer().getTextureState();
+        ts.setEnabled(true);
+        ts.setTexture(
+        TextureManager.loadTexture(
+            textu,
+            Texture.MM_LINEAR,
+            Texture.FM_LINEAR,
+            true));
+        freakmd2.setRenderState(ts);
+        freakmd2.setLocalTranslation(new Vector3f(0,0,-20));
+        freakmd2.setLocalScale(.5f);
+        kc=(KeyframeController) freakmd2.getChild(0).getController(0);
+        kc.setSpeed(10);
+
+        rootNode.attachChild(freakmd2);
+        KeyBindingManager.getKeyBindingManager().set(
+            "smoothTrans",
+            KeyInput.KEY_T);
     }
 }
-
-/*
-For old loader:
-FPS: 381.7512
-FPS: 428.86276
-FPS: 478.78525
-FPS: 479.9176
-FPS: 476.76544
-FPS: 478.65497
-FPS: 482.09097
-FPS: 480.20566
-FPS: 480.8201
-Load time: 4246
-
-For new loader:
-FPS: 397.99054
-FPS: 450.28546
-FPS: 489.75085
-FPS: 491.8328
-FPS: 489.68756
-FPS: 488.69788
-FPS: 490.11353
-FPS: 492.1093
-Total load time: 3104
-*/
