82485aa2d4e3f756bc69c6b5e933713802a7f4e5
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-23 03:51:46 +0000

    Use classloader to find XML.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1277 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/loader/TestXMLLoader.java
+++ src/jmetest/renderer/loader/TestXMLLoader.java
@@ -7,12 +7,14 @@ import com.jme.scene.model.XMLparser.XMLWriter;
 import com.jme.scene.Node;
 
 import java.io.*;
+import java.net.URL;
 
 
 /**
  *
- * Test class for XML loading/writting with jME
- * Started Date: May 30, 2004
+ * Started Date: May 30, 2004 <br><br>
+ * Test class for XML loading/writting with jME.  I load an XML doc, write the node I loaded, then load that write and
+ * finally attach that to the SceneGraph
  * @author Jack Lindamood
  */
 public class TestXMLLoader extends SimpleGame{
@@ -26,9 +28,14 @@ public class TestXMLLoader extends SimpleGame{
         lightState.get(0).setSpecular(new ColorRGBA(1,1,1,1));
 
         SAXReader r=new SAXReader();
+        URL xmldoc=TestXMLLoader.class.getClassLoader().getResource("jmetest/data/XML docs/newSampleScene.xml");
+        if (xmldoc==null){
+            System.out.println("Error locating XML document.  Try including jmetest-data.jar in classpath");
+            System.exit(0);
+        }
         Node mi1=null;
         try {
-            mi1=r.loadXML(new File("data/XML docs/newSampleScene.xml").toURL().openStream());
+            mi1=r.loadXML(xmldoc.openStream());
         } catch (IOException e) {
             System.out.println("bad File exception" + e.getCause() + "*" + e.getMessage());
         }
