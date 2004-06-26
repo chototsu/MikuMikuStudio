6e229681b1329b5e2fe05b432812001cf6183304
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-26 04:38:45 +0000

    uses some more fun stuff
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1301 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/loader/TestKeyframeController.java
+++ src/jmetest/renderer/loader/TestKeyframeController.java
@@ -3,7 +3,10 @@ package jmetest.renderer.loader;
 import com.jme.app.SimpleGame;
 import com.jme.scene.shape.Sphere;
 import com.jme.scene.model.XMLparser.KeyframeController;
+import com.jme.scene.Controller;
 import com.jme.renderer.ColorRGBA;
+import com.jme.input.KeyBindingManager;
+import com.jme.input.KeyInput;
 
 
 /**
@@ -13,8 +16,35 @@ import com.jme.renderer.ColorRGBA;
  * @author Jack Lindamood
  */
 public class TestKeyframeController extends SimpleGame{
+    KeyframeController kc;
     public static void main(String[] args) {
         new TestKeyframeController().start();
+    }
+
+    protected void simpleUpdate(){
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("start_middle", false)) {
+            kc.setNewAnimationTimes(.5f,2.75f);
+        }
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("start_total", false)) {
+            kc.setNewAnimationTimes(0,3);
+        }
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("start_end", false)) {
+            kc.setNewAnimationTimes(3,3);
+        }
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("toggle_wrap", false)) {
+            if (kc.getRepeatType()==KeyframeController.RT_CYCLE)
+                kc.setRepeatType(KeyframeController.RT_WRAP);
+            else
+                kc.setRepeatType(KeyframeController.RT_CYCLE);
+        }
     }
 
     protected void simpleInitGame() {
@@ -25,7 +55,7 @@ public class TestKeyframeController extends SimpleGame{
         Sphere big=new Sphere("big",9,15,10);
         big.setSolidColor(ColorRGBA.blue);
         Sphere thisone=new Sphere("blarg",9,15,1);
-        KeyframeController kc=new KeyframeController();
+        kc=new KeyframeController();
         kc.setMorphingMesh(thisone);
         kc.setKeyframe(0,small);
         kc.setKeyframe(2.5f,medium);
@@ -34,5 +64,10 @@ public class TestKeyframeController extends SimpleGame{
         thisone.addController(kc);
         rootNode.attachChild(thisone);
         lightState.setEnabled(false);
+        // Note: T L B C Already used
+        KeyBindingManager.getKeyBindingManager().set("start_middle",KeyInput.KEY_Q);
+        KeyBindingManager.getKeyBindingManager().set("start_total",KeyInput.KEY_A);
+        KeyBindingManager.getKeyBindingManager().set("toggle_wrap",KeyInput.KEY_Z);
+        KeyBindingManager.getKeyBindingManager().set("start_end",KeyInput.KEY_E);
     }
 }
