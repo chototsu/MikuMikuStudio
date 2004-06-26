b89d580621a97c6e0710381a3727fe291af346ae
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-26 04:39:01 +0000

    Added some fun functions to play with
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1302 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/loader/TestMd2JmeWrite.java
+++ src/jmetest/renderer/loader/TestMd2JmeWrite.java
@@ -10,6 +10,7 @@ import com.jme.input.KeyBindingManager;
 import com.jme.input.KeyInput;
 import com.jme.math.Vector3f;
 
+import javax.swing.*;
 import java.io.*;
 import java.net.URL;
 
@@ -23,8 +24,16 @@ public class TestMd2JmeWrite extends SimpleGame{
     float totalFPS;
     long totalCounts;
     private KeyframeController kc;
+    private static final String helpMessage="Fun with KeyframeController and md2 models.  Keys are:\n" +
+            "R: Make drFreak run\n" +
+            "H: Make drFreak attack\n" +
+            "Z: Toggle repeat type wrap and cycle\n" +
+            "E: Do a quick transform to the begining\n" +
+            "B: Do a smooth transform to the begining\n" +
+            "Q: Do a smooth transform to drfreak's death\n";
 
     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,helpMessage);
         TestMd2JmeWrite app=new TestMd2JmeWrite();
         app.setDialogBehaviour(TestMd2JmeWrite.FIRSTRUN_OR_NOCONFIGFILE_SHOW_PROPS_DIALOG);
         app.start();
@@ -39,13 +48,17 @@ public class TestMd2JmeWrite extends SimpleGame{
         Node freakmd2=null;
 
         try {
+            long time = System.currentTimeMillis();
             converter.writeFiletoStream(freak,BO);
+            System.out.println("Time to convert from md2 to .jme:"+ ( System.currentTimeMillis()-time));
         } catch (IOException e) {
             System.out.println("damn exceptions:" + e.getMessage());
         }
         JmeBinaryReader jbr=new JmeBinaryReader();
         try {
+            long time=System.currentTimeMillis();
             freakmd2=jbr.loadBinaryFormat(new ByteArrayInputStream(BO.toByteArray()));
+            System.out.println("Time to convert from .jme to SceneGraph:"+ ( System.currentTimeMillis()-time));
         } catch (IOException e) {
             System.out.println("damn exceptions:" + e.getMessage());
         }
@@ -63,10 +76,48 @@ public class TestMd2JmeWrite extends SimpleGame{
         freakmd2.setLocalScale(.5f);
         kc=(KeyframeController) freakmd2.getChild(0).getController(0);
         kc.setSpeed(10);
-
+        // Note: W S A D Left Down Up Right F12 ESC T L B C Already used
+        KeyBindingManager.getKeyBindingManager().set("start_run",KeyInput.KEY_R);
+        KeyBindingManager.getKeyBindingManager().set("start_hit",KeyInput.KEY_H);
+        KeyBindingManager.getKeyBindingManager().set("toggle_wrap",KeyInput.KEY_Z);
+        KeyBindingManager.getKeyBindingManager().set("start_end",KeyInput.KEY_E);
+        KeyBindingManager.getKeyBindingManager().set("start_smoothbegin",KeyInput.KEY_B);
+        KeyBindingManager.getKeyBindingManager().set("start_smoothdeath",KeyInput.KEY_Q);
         rootNode.attachChild(freakmd2);
-        KeyBindingManager.getKeyBindingManager().set(
-            "smoothTrans",
-            KeyInput.KEY_T);
      }
+    protected void simpleUpdate(){
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("start_run", false)) {
+            kc.setNewAnimationTimes(39,44);
+        }
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("start_hit", false)) {
+            kc.setNewAnimationTimes(45,52);
+        }
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("start_end", false)) {
+            kc.setNewAnimationTimes(0,196);
+        }
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("start_smoothbegin", false)) {
+            kc.setSmoothTranslation(0,25,0,196);
+        }
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("start_smoothdeath", false)) {
+            kc.setSmoothTranslation(175,25,175,182);
+        }
+        if (KeyBindingManager
+            .getKeyBindingManager()
+            .isValidCommand("toggle_wrap", false)) {
+            if (kc.getRepeatType()==KeyframeController.RT_CYCLE)
+                kc.setRepeatType(KeyframeController.RT_WRAP);
+            else
+                kc.setRepeatType(KeyframeController.RT_CYCLE);
+        }
+    }
  }
