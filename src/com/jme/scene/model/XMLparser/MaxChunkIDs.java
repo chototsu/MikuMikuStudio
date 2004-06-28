58774899bd071a602f971bbf93a06da21ee1d603
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-28 03:01:22 +0000

    id's for max
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1319 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/scene/model/XMLparser/MaxChunkIDs.java
+++ src/com/jme/scene/model/XMLparser/MaxChunkIDs.java
@@ -9,14 +9,22 @@ package com.jme.scene.model.XMLparser;
 public interface MaxChunkIDs {
 
     // These must all be diffrent values
-    static final short MAIN_3DS     =0x4D4D;
-    static final short M3D_VERSION  =0x0002;
-    static final short NULL_CHUNK   =0x0000;
-    static final short MASTER_SCALE =0x0100;
-    static final short NAMED_OBJECT =0x4000;
-    static final short OBJ_TRIMESH  =0x4100;
-    static final short VERTEX_LIST  =0x4110;
-    static final short TEXT_COORDS  =0x4140;
-    static final short COORD_SYS    =0x4160;
-    static final short FACES_ARRAY  =0x4120;
+    static final int MAIN_3DS     =0x4D4D;
+    static final int M3D_VERSION  =0x0002;
+    static final int NULL_CHUNK   =0x0000;
+    static final int MASTER_SCALE =0x0100;
+    static final int NAMED_OBJECT =0x4000;
+    static final int OBJ_TRIMESH  =0x4100;
+    static final int VERTEX_LIST  =0x4110;
+    static final int TEXT_COORDS  =0x4140;
+    static final int COORD_SYS    =0x4160;
+    static final int FACES_ARRAY  =0x4120;
+    static final int SMOOTH_GROUP =0x4150;
+    static final int KEYFRAMES    =0xb000;
+    static final int KEYFRAME_HEAD=0xb00a;
+    static final int XDATA_SECTION=0x8000;
+    static final int ROT_TRACK_TAG=0xb021;
+    static final int SCALE_TRACK  =0xb022;
+    static final int EDIT_3DS     =0x3d3d;
+    static final int MESH_VERSION =0x3d3e;
 }
