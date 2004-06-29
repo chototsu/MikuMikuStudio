8f14f31f38981bb4629d1c858ad327a877799c56
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-06-29 07:10:02 +0000

    now shows XML
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1328 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/loader/TestMaxJmeWrite.java
+++ src/jmetest/renderer/loader/TestMaxJmeWrite.java
@@ -2,10 +2,16 @@ package jmetest.renderer.loader;
 
 import com.jme.app.SimpleGame;
 import com.jme.scene.model.XMLparser.MaxToJme;
+import com.jme.scene.model.XMLparser.JmeBinaryReader;
+import com.jme.scene.model.XMLparser.BinaryToXML;
+import com.jme.scene.Node;
+import com.jme.scene.shape.Box;
+import com.jme.math.Vector3f;
+import com.jme.bounding.BoundingSphere;
+import com.jme.renderer.ColorRGBA;
 
 
-import java.io.IOException;
-import java.io.ByteArrayOutputStream;
+import java.io.*;
 import java.net.URL;
 
 
@@ -27,10 +33,38 @@ public class TestMaxJmeWrite extends SimpleGame{
         MaxToJme C1=new MaxToJme();
         try {
             ByteArrayOutputStream BO=new ByteArrayOutputStream();
-            URL box=TestMaxJmeWrite.class.getClassLoader().getResource("jmetest/data/model/box.3DS");
-            C1.convert(box.openStream(),BO);
+            URL maxFile=TestMaxJmeWrite.class.getClassLoader().getResource("jmetest/data/model/sphere.3DS");
+//            URL maxFile = new File("3dsmodels/europe.3ds").toURI().toURL();
+            C1.convert(maxFile.openStream(),BO);
+            JmeBinaryReader jbr=new JmeBinaryReader();
+            BinaryToXML btx=new BinaryToXML();
+            StringWriter SW=new StringWriter();
+            btx.sendBinarytoXML(new ByteArrayInputStream(BO.toByteArray()),SW);
+            System.out.println(SW);
+            Node r=jbr.loadBinaryFormat(new ByteArrayInputStream(BO.toByteArray()));
+            r.setLocalScale(.1f);
+            rootNode.attachChild(r);
         } catch (IOException e) {
             System.out.println("damn exceptions:" + e);
+            e.printStackTrace();
         }
+        drawAxis();
+    }
+    private void drawAxis() {
+        Box Xaxis=new Box("axisX",new Vector3f(5,0,0),5f,.1f,.1f);
+        Xaxis.setModelBound(new BoundingSphere());
+        Xaxis.updateModelBound();
+        Xaxis.setSolidColor(ColorRGBA.blue);
+        Box Yaxis=new Box("axisY",new Vector3f(0,5,0),.1f,5f,.1f);
+        Yaxis.setModelBound(new BoundingSphere());
+        Yaxis.updateModelBound();
+        Yaxis.setSolidColor(ColorRGBA.red);
+        Box Zaxis=new Box("axisZ",new Vector3f(0,0,5),.1f,.1f,5f);
+        Zaxis.setSolidColor(ColorRGBA.green);
+        Zaxis.setModelBound(new BoundingSphere());
+        Zaxis.updateModelBound();
+        rootNode.attachChild(Xaxis);
+        rootNode.attachChild(Yaxis);
+        rootNode.attachChild(Zaxis);
     }
 }
