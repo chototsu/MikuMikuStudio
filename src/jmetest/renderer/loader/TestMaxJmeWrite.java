bc99dbe888063d811c7d4a082ab7e6e156e09b13
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-07-19 09:59:44 +0000

    Issue number: Obtained from: Submitted by: Reviewed by:
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1538 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/loader/TestMaxJmeWrite.java
+++ src/jmetest/renderer/loader/TestMaxJmeWrite.java
@@ -44,11 +44,8 @@ public class TestMaxJmeWrite extends SimpleGame{
         try {
             ByteArrayOutputStream BO=new ByteArrayOutputStream();
             URL maxFile=TestMaxJmeWrite.class.getClassLoader().getResource("jmetest/data/model/Character.3DS");
-//            URL maxFile=TestMaxJmeWrite.class.getClassLoader().getResource("jmetest/data/model/tpot.3ds");
-//            URL maxFile=new File("3dsmodels/tpot.3ds").toURL();
             C1.convert(new BufferedInputStream(maxFile.openStream()),BO);
             JmeBinaryReader jbr=new JmeBinaryReader();
-//            jbr.setProperty("texulr",new File("3dsmodels").toURL());
             BinaryToXML btx=new BinaryToXML();
             StringWriter SW=new StringWriter();
             btx.sendBinarytoXML(new ByteArrayInputStream(BO.toByteArray()),SW);
@@ -63,50 +60,9 @@ public class TestMaxJmeWrite extends SimpleGame{
             temp.fromAngleAxis(FastMath.PI/2,new Vector3f(-1,0,0));
             rootNode.setLocalRotation(temp);
             rootNode.attachChild(r);
-
         } catch (IOException e) {
             System.out.println("Damn exceptions:"+e);
             e.printStackTrace();
         }
-
-        drawAxis();
-        TextureState ts=display.getRenderer().getTextureState();
-        ts.setEnabled(true);
-        rootNode.setRenderState(ts);
-
-    }
-
-    private void drawAxis() {
-        Box Xaxis=new Box("axisX",new Vector3f(5,0,0),5f,.1f,.1f);
-        Xaxis.setModelBound(new BoundingBox());
-        Xaxis.updateModelBound();
-        Xaxis.setSolidColor(ColorRGBA.red);
-        MaterialState red=display.getRenderer().getMaterialState();
-        red.setEmissive(ColorRGBA.red);
-        red.setEnabled(true);
-        Xaxis.setRenderState(red);
-
-        Box Yaxis=new Box("axisY",new Vector3f(0,5,0),.1f,5f,.1f);
-        Yaxis.setModelBound(new BoundingBox());
-        Yaxis.updateModelBound();
-        Yaxis.setSolidColor(ColorRGBA.green);
-        MaterialState green=display.getRenderer().getMaterialState();
-        green.setEmissive(ColorRGBA.green);
-        green.setEnabled(true);
-        Yaxis.setRenderState(green);
-
-        Box Zaxis=new Box("axisZ",new Vector3f(0,0,5),.1f,.1f,5f);
-        Zaxis.setSolidColor(ColorRGBA.blue);
-        Zaxis.setModelBound(new BoundingBox());
-        Zaxis.updateModelBound();
-        MaterialState blue=display.getRenderer().getMaterialState();
-        blue.setEmissive(ColorRGBA.blue);
-        blue.setEnabled(true);
-        Zaxis.setRenderState(blue);
-
-        rootNode.attachChild(Xaxis);
-        rootNode.attachChild(Yaxis);
-        rootNode.attachChild(Zaxis);
-
     }
 }
