3b4213daba174fd210f1811c0189fd94aa07a038
guurk <guurk@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-07-21 22:09:24 +0000

    fixed back to 1.30 revision.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1552 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/renderer/lwjgl/LWJGLRenderer.java
+++ src/com/jme/renderer/lwjgl/LWJGLRenderer.java
@@ -128,7 +128,7 @@ import com.jme.widget.WidgetRenderer;
  * @see com.jme.renderer.Renderer
  * @author Mark Powell
  * @author Joshua Slack - Optimizations
- * @version $Id: LWJGLRenderer.java,v 1.31 2004-07-21 21:19:43 guurk Exp $
+ * @version $Id: LWJGLRenderer.java,v 1.32 2004-07-21 22:09:24 guurk Exp $
  */
 public class LWJGLRenderer implements Renderer {
 
@@ -996,10 +996,7 @@ public class LWJGLRenderer implements Renderer {
       numberOfTris += adder / 3;
       numberOfVerts += vertAdder;
     }
-
-    
-    //GL12.glDrawRangeElements(GL11.GL_TRIANGLES, 0, t.getVertQuantity(), indices);
-    GL11.glDrawElements(GL11.GL_TRIANGLES, indices);
+    GL12.glDrawRangeElements(GL11.GL_TRIANGLES, 0, t.getVertQuantity(), indices);
 
     GL11.glMatrixMode(GL11.GL_MODELVIEW);
     GL11.glPopMatrix();
