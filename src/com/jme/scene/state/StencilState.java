36f181a591bdbe16c33271b85056f53d52245b2c
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-08-02 18:29:19 +0000

    javadoc
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1662 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/scene/state/StencilState.java
+++ src/com/jme/scene/state/StencilState.java
@@ -33,26 +33,44 @@ package com.jme.scene.state;
 
 
 /**
- * <code>StencilState</code>
+ * The StencilState RenderState allows the user to set the attributes of the
+ * stencil buffer of the renderer. The Stenciling is similar to
+ * Z-Buffering in that it allows enabling and disabling drawing on a per pixel
+ * basis. You can use the stencil plane to mask out portions of the rendering
+ * to create special effects, such as outlining or planar shadows.
  * @author Mark Powell
  * @version $id$
  */
 public abstract class StencilState extends RenderState {
-    
+
+    /** A stencil function that never passes. */
     public static final int SF_NEVER = 0;
+    /** A stencil function that passes if (ref & mask) < (stencil & mask). */
     public static final int SF_LESS = 1;
+    /** A stencil function that passes if (ref & max) <= (stencil & mask). */
     public static final int SF_LEQUAL = 2;
+    /** A stencil function that passes if (ref & max) > (stencil & mask). */
     public static final int SF_GREATER = 3;
+    /** A stencil function that passes if (ref & max) >= (stencil & mask). */
     public static final int SF_GEQUAL = 4;
+    /** A stencil function that passes if (ref & max) == (stencil & mask). */
     public static final int SF_EQUAL = 5;
+    /** A stencil function that passes if (ref & max) != (stencil & mask). */
     public static final int SF_NOTEQUAL = 6;
+    /** A stencil function that always passes. */
     public static final int SF_ALWAYS = 7;
-    
+
+    /** A stencil function result that keeps the current value. */
     public static final int SO_KEEP = 0;
+    /** A stencil function result that sets the stencil buffer value to 0. */
     public static final int SO_ZERO = 1;
+    /** A stencil function result that sets the stencil buffer value to ref, as specified by stencil function. */
     public static final int SO_REPLACE = 2;
+    /** A stencil function result that increments the current stencil buffer value. */
     public static final int SO_INCR = 3;
+    /** A stencil function result that decrements the current stencil buffer value. */
     public static final int SO_DECR = 4;
+    /** A stencil function result that bitwise inverts the current stencil buffer value. */
     public static final int SO_INVERT = 5;
     
     private int stencilFunc;
@@ -62,57 +80,109 @@ public abstract class StencilState extends RenderState {
     private int stencilOpZFail;
     private int stencilOpZPass;
 
-    /* (non-Javadoc)
+    /**
+     * Returns RS_STENCIL
      * @see com.jme.scene.state.RenderState#getType()
      */
     public int getType() {
         return RS_STENCIL;
     }
-    
+
+    /**
+     * Sets the function that defines if a stencil test passes or not.
+     * @param func The new stencil function.
+     */
     public void setStencilFunc(int func) {
         this.stencilFunc = func;
     }
-    
+
+    /**
+     * Returns the currently set stencil function.
+     * @return The current stencil function.
+     */
     public int getStencilFunc() {
         return stencilFunc;
     }
-    
+
+    /**
+     * Sets the stencil reference to be used during the stencil function.
+     * @param ref The new stencil reference.
+     */
     public void setStencilRef(int ref) {
         this.stencilRef = ref;
     }
-    
+
+    /**
+     * Returns the currently set stencil reference.
+     * @return The current stencil reference.
+     */
     public int getStencilRef() {
         return stencilRef;
     }
-    
+
+    /**
+     * Sets the stencil mask to be used during the stencil function.
+     * @param mask The new stencil mask.
+     */
     public void setStencilMask(int mask) {
         this.stencilMask = mask;
     }
-    
+
+    /**
+     * Returns the currently set stencil mask.
+     * @return The current stencil mask.
+     */
     public int getStencilMask() {
         return stencilMask;
     }
-    
+
+    /**
+     * Specifies the aciton to take when the stencil test fails.  One of SO_KEEP, SO_ZERO, and so on.
+     * @param op The new stencil operation.
+     */
     public void setStencilOpFail(int op) {
         this.stencilOpFail = op;
     }
-    
+
+    /**
+     * Returns the current stencil operation.
+     * @return The current stencil operation.
+     */
     public int getStencilOpFail() {
         return stencilOpFail;
     }
-    
+
+    /**
+     * Specifies stencil action when the stencil test passes, but the depth test fails.
+     * One of SO_KEEP, SO_ZERO, and so on.
+     * @param op The Z test operation to set.
+     */
     public void setStencilOpZFail(int op) {
         this.stencilOpZFail = op;
     }
-    
+
+    /**
+     * Returns the current Z op fail function.
+     * @return The current Z op fail function.
+     */
     public int getStencilOpZFail() {
         return stencilOpZFail;
     }
-    
+
+    /**
+     * Specifies stencil action when both the stencil test and the depth test pass, or
+     * when the stencil test passes and either there is no depth buffer or depth
+     * testing is not enabled.  One of SO_KEEP, SO_ZERO, and so on.
+     * @param op The new Z test pass operation to set.
+     */
     public void setStencilOpZPass(int op) {
         this.stencilOpZPass = op;
     }
-    
+
+    /**
+     * Returns the current Z op pass function.
+     * @return The current Z op pass function.
+     */
     public int getStencilOpZPass() {
         return stencilOpZPass;
     }
