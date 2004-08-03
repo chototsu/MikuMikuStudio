81f7987380f03dbe370814329e348e622109740d
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-08-03 02:35:46 +0000

    javadoc
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1680 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/effects/Shake.java
+++ src/com/jme/effects/Shake.java
@@ -8,12 +8,20 @@ import com.jme.scene.Controller;
 import com.jme.scene.Spatial;
 
 /**
+ * <code>Shake</code> is a controller that allows a user to "Shake" a spatial's
+ * localTranslation.  The two flags signal if you want to either:<br>
+ * 1) Shake randomly every update, wandering in random directions.<br>
+ * 2) Always return back to the first location after a shake, so the Spatial
+ * doesn't wander in random directions.
  * @author Ahmed
+ * @author Jack Lindamood (javadoc only)
  */
 public class Shake extends Controller {
 
+    /** Returns the Spatial to the origin after a shake. */
     public static final int RETURN_TO_ORIGIN = 0;
 
+    /** Shakes the spatial to random locations every update. */
     public static final int RANDOMISE = 1;
 
     private int currentType;
@@ -26,17 +34,35 @@ public class Shake extends Controller {
 
     private Spatial child;
 
+    /**
+     * Creates a Shake controller.  The amount flag signals how much
+     * of a shake to create each update.  The Shake controller is disabled
+     * by default.  The default shake type is RETURN_TO_ORIGIN.
+     * @param spat The spatial to shake.
+     * @param amount The amount to posibly shake by each update.
+     */
     public Shake(Spatial spat, float amount) {
         this.amount = amount;
         this.setActive(false);
         goingBackToActualLock = true;
         this.child = spat;
+        currentType=RETURN_TO_ORIGIN;
     }
 
+    /**
+     * Sets the type of shake.  One of either RETURN_TO_ORIGIN or
+     * RANDOMISE.  If an invalid type is passed, nothing changes.
+     * @param type The type of shake to have.
+     */
     public void setType(int type) {
+        if (type!= RETURN_TO_ORIGIN && type!=RANDOMISE) return;
         currentType = type;
     }
 
+    /**
+     * Updates the shake.
+     * @param time Ignored
+     */
     public void update(float time) {
         if (isActive()) {
 
