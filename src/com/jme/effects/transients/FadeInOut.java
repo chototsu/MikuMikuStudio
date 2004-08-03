274e56b21e9f2021f90d3f38e885c89c71698957
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-08-03 03:31:26 +0000

    javadoc
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1686 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/effects/transients/FadeInOut.java
+++ src/com/jme/effects/transients/FadeInOut.java
@@ -41,7 +41,19 @@ import com.jme.scene.state.AlphaState;
 import com.jme.system.DisplaySystem;
 
 /**
+ * A <code>FadeInOut</code> object is made to be controlled by a <code>
+ * FadeInOutController</code>.  It has 3 parts:<br>
+ * 1) A Geometry who's per vertex color is to be changed by a <code>FadeInOutController</code>.
+ * 2) A begining (fadeOutNode) node that represents the <code>FadeInOut</code> at the begining.
+ * 3) An ending (fadeInNode) node that represents the <code>FadeInOut</code> when the <code>
+ * FadeInOutController</code> has reached the next stage.<br>
+ * When the next stage is reached by the controller, the begining node is detached and
+ * the ending node is attached.  It is assumed the (1) geometry can fade to cover
+ * up the change.
+ *
+ * @see FadeInOutController
  * @author Ahmed
+ * @author Jack Lindamood (javadoc only)
  */
 public class FadeInOut extends Transient {
 
@@ -50,11 +62,28 @@ public class FadeInOut extends Transient {
     private ColorRGBA fadeColor;
     private float speed;
 
+    /**
+     * Creates a new FadeInOut node. The speed is by default .01f
+     * @param name The name of the node.
+     * @param fade The geometry whos per vertex color will fade over time.
+     * @param out The begining node that will fade out.
+     * @param in The ending node that will fade in.
+     * @param c The begining color of the fade Geometry.
+     */
     public FadeInOut(String name, Geometry fade, Node out, Node in, ColorRGBA c) {
         super(name);
         initialise(fade, out, in, c, 0.01f);
     }
 
+    /**
+     * Creates a new FadeInOut node.
+     * @param name The name of the node.
+     * @param fade The geometry whos per vertex color will fade over time.
+     * @param out The begining node that will fade out.
+     * @param in The ending node that will fade in.
+     * @param c The begining color of the fade geometry.
+     * @param s The speed at which the fade will take place.
+     */
     public FadeInOut(String name, Geometry fade, Node out, Node in, ColorRGBA c, float s) {
         super(name);
         initialise(fade, out, in, c, s);
@@ -85,46 +114,92 @@ public class FadeInOut extends Transient {
         this.attachChild(fadeOutNode);
     }
     
-    // getters/setters for the fadeQuad
+    /**
+     * Returns the Geometry that is fading.
+     * @return The currently fading geometry.
+     */
     public Geometry getFadeQuad() {
         return fadeQ;
     }
+
+    /**
+     * Sets the geometry that will fade.
+     * @param f The new geometry that will fade.
+     */
     public void setFadeQuad(Geometry f) {
         fadeQ = f;
     }
     
-    // getters/setters for fadeInNode
+    /**
+     * Returns the node this object is fading into.
+     * @return The current fade in node.
+     */
     public Node getFadeInNode() {
     	return fadeInNode;
     }
+
+    /**
+     * Sets the node that this object will fade into.
+     * @param fade The node to fade into.
+     */
     public void setFadeInNode(Node fade) {
     	fadeInNode = fade;
     }
     
-    // getters/setters for fadeOutNode
+    /**
+     * Returns the node this object is fading from.
+     * @return The current fade out node.
+     */
     public Node getFadeOutNode() {
     	return fadeOutNode;
     }
+
+    /**
+     * Sets the node this object will fade from.
+     * @param fade The new fade out node.
+     */
     public void setFadeOutNode(Node fade) {
     	fadeOutNode = fade;
     }
 
+    /**
+     * Returns the current color being applied to the fade quad.
+     * @return The current fade color.
+     */
     public ColorRGBA getFadeColor() {
         return fadeColor;
     }
 
+    /**
+     * Sets the current per vertex color of the fade quad, and updates the
+     * current fade color to c.
+     * @param c The new color to set the fade quad too.
+     */
     public void setFadeColor(ColorRGBA c) {
     	fadeColor = (ColorRGBA)c.clone();
     	fadeQ.setSolidColor(fadeColor);
     }
 
+    /**
+     * Returns the speed that this object should fade at.
+     * @return The current speed.
+     */ 
     public float getSpeed() {
         return speed;
     }
+
+    /**
+     * Sets the speed this object should fade at.
+     * @param s The new speed.
+     */
     public void setSpeed(float s) {
         speed = s;
     }
 
+    /**
+     * Ignoring children, this only updates all the controllers of this FadeInOut
+     * @param time the time to pass to update.
+     */
     public void updateWorldData(float time) {
         if (getControllers().size() != 0) {
         	for (int i = 0; i < getControllers().size(); i++) {
