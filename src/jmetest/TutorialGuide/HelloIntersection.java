69804865dd4050e9deb0cf8a3e9cbd9cbb625423
anakan <anakan@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-08-03 15:59:57 +0000

    modified sound playing rules
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1695 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/TutorialGuide/HelloIntersection.java
+++ src/jmetest/TutorialGuide/HelloIntersection.java
@@ -1,5 +1,6 @@
 package jmetest.TutorialGuide;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Random;
 
@@ -22,65 +23,81 @@ import com.jme.sound.SoundAPIController;
 import com.jme.util.TextureManager;
 import com.jme.sound.SoundPool;
 import com.jme.sound.scene.ProgrammableSound;
+import com.jme.sound.scene.SoundNode;
 
 /**
- * Started Date: Jul 24, 2004<br><br>
- *
+ * Started Date: Jul 24, 2004 <br>
+ * <br>
+ * 
  * Demonstrates intersection testing, sound, and making your own controller.
- *
+ * 
  * @author Jack Lindamood
  */
 public class HelloIntersection extends SimpleGame {
-    /** Material for my bullet*/
+    /** Material for my bullet */
     MaterialState bulletMaterial;
-    /** Target you're trying to hit*/
+
+    /** Target you're trying to hit */
     Sphere target;
+
     /** Location of laser sound */
     URL laserURL;
-    /** Used to move target location on a hit*/
+
+    /** Location of hit sound */
+    URL hitURL;
+
+    /** Used to move target location on a hit */
     Random r = new Random();
+
     /** A sky box for our scene. */
     Skybox sb;
 
-    /** The programmable sound that will be in charge of maintaining our sound effects. */
+    /**
+     * The programmable sound that will be in charge of maintaining our sound
+     * effects.
+     */
     ProgrammableSound programSound;
 
-    /** The ID of our laser shooting sound effect.  The value is not important.
-    It should just be unique in our game to this sound. */
+    /** The node where attached sounds will be propagated from */
+    SoundNode snode;
+
+    /**
+     * The ID of our laser shooting sound effect. The value is not important. It
+     * should just be unique in our game to this sound.
+     */
     private int laserEventID = 1;
+    
+    private int hitEventID = 2;
 
     public static void main(String[] args) {
         HelloIntersection app = new HelloIntersection();
         app.setDialogBehaviour(SimpleGame.ALWAYS_SHOW_PROPS_DIALOG);
         app.start();
-      }
+    }
 
     protected void simpleInitGame() {
         setupSound();
 
-        /** Create a + for the middle of the screen*/
+        /** Create a + for the middle of the screen */
         Text cross = new Text("Crosshairs", "+");
 
         // 8 is half the width of a font char
-        /** Move the + to the middle*/
+        /** Move the + to the middle */
         cross.setLocalTranslation(new Vector3f(display.getWidth() / 2f - 8f,
-                                               display.getHeight() / 2f - 8f,
-                                               0));
+                display.getHeight() / 2f - 8f, 0));
         fpsNode.attachChild(cross);
         target = new Sphere("my sphere", 15, 15, 1);
         target.setModelBound(new BoundingSphere());
         target.updateModelBound();
         rootNode.attachChild(target);
 
-        /** Create a skybox to suround our world*/
+        /** Create a skybox to suround our world */
         sb = new Skybox("skybox", 200, 200, 200);
         URL monkeyLoc = HelloIntersection.class.getClassLoader().getResource(
-            "jmetest/data/texture/clouds.png");
+                "jmetest/data/texture/clouds.png");
         TextureState ts = display.getRenderer().getTextureState();
-        ts.setTexture(
-            TextureManager.loadTexture(monkeyLoc, Texture.MM_LINEAR,
-                                       Texture.FM_LINEAR, true)
-            );
+        ts.setTexture(TextureManager.loadTexture(monkeyLoc, Texture.MM_LINEAR,
+                Texture.FM_LINEAR, true));
         sb.setRenderState(ts);
 
         // Attach the skybox to our root node, and force the rootnode to show
@@ -88,39 +105,49 @@ public class HelloIntersection extends SimpleGame {
         rootNode.attachChild(sb);
         rootNode.setForceView(true);
 
-        /** Set the action called "firebullet", bound to KEY_F, to performAction FireBullet*/
+        /**
+         * Set the action called "firebullet", bound to KEY_F, to performAction
+         * FireBullet
+         */
         input.addKeyboardAction("firebullet", KeyInput.KEY_F, new FireBullet());
 
-        /** Make bullet material*/
+        /** Make bullet material */
         bulletMaterial = display.getRenderer().getMaterialState();
         bulletMaterial.setEmissive(ColorRGBA.green);
 
-        /** Make target material*/
+        /** Make target material */
         MaterialState redMaterial = display.getRenderer().getMaterialState();
         redMaterial.setDiffuse(ColorRGBA.red);
         target.setRenderState(redMaterial);
     }
 
     private void setupSound() {
-        /** init sound API acording to the rendering enviroment you're using*/
+        /** init sound API acording to the rendering enviroment you're using */
         SoundAPIController.getSoundSystem(properties.getRenderer());
-        /** Set the 'ears' for the sound API*/
+        /** Set the 'ears' for the sound API */
         SoundAPIController.getRenderer().setCamera(cam);
-
-        /** Create program sound*/
-        programSound= new ProgrammableSound();
-        /** Make the sound softer*/
-        programSound.setGain(.5f);
+        snode = new SoundNode();
+        /** Create program sound */
+        programSound = new ProgrammableSound();
+        /** Make the sound softer */
+        programSound.setGain(1f);
         programSound.setLooping(false);
-        programSound.stop();
+       
+        programSound.setPosition(cam.getLocation());
+        /** locate laser and register it with the prog sound. */
 
-        /** locate laser and register it with the prog sound.*/
-        laserURL = HelloIntersection.class.getClassLoader().getResource("jmetest/data/sound/laser.ogg");
+        laserURL = HelloIntersection.class.getClassLoader().getResource(
+                "jmetest/data/sound/laser.ogg");
+        hitURL = HelloIntersection.class.getClassLoader().getResource(
+                "jmetest/data/sound/explosion.wav");
         // Ask the system for a program id for this resource
-        int programid = SoundPool.compile(new URL[] {laserURL});
+        int programid = SoundPool.compile(new URL[] { laserURL });
+        int hitid = SoundPool.compile(new URL[] { hitURL });
         // Then we bind the programid we received to our laser event id.
         programSound.bindEvent(laserEventID, programid);
-
+        programSound.bindEvent(hitEventID, hitid);
+        //        programSound.setNextProgram(programid);
+        snode.attachChild(programSound);
         //... repeat above 3 lines to register other sounds.
     }
 
@@ -133,78 +160,87 @@ public class HelloIntersection extends SimpleGame {
 
         public void performAction(float time) {
             System.out.println("BANG");
-            /** Create bullet*/
+            /** Create bullet */
             Sphere bullet = new Sphere("bullet" + numBullets++, 8, 8, .25f);
             bullet.setModelBound(new BoundingSphere());
             bullet.updateModelBound();
-            /** Move bullet to the camera location*/
+            /** Move bullet to the camera location */
             bullet.setLocalTranslation(new Vector3f(cam.getLocation()));
             bullet.setRenderState(bulletMaterial);
-            /** Update the new world locaion for the bullet before I add a controller*/
+            /**
+             * Update the new world locaion for the bullet before I add a
+             * controller
+             */
             bullet.updateGeometricState(0, true);
-            /** Add a movement controller to the bullet going in the camera's direction*/
-            bullet.addController(new BulletMover(bullet,
-                                               new Vector3f(cam.getDirection())));
+            /**
+             * Add a movement controller to the bullet going in the camera's
+             * direction
+             */
+            bullet.addController(new BulletMover(bullet, new Vector3f(cam
+                    .getDirection())));
             rootNode.attachChild(bullet);
             bullet.updateRenderState();
-            /** Signal our sound to play laser during rendering*/
-            programSound.fireEvent(laserEventID);
+            /** Signal our sound to play laser during rendering */
+            snode.onEvent(laserEventID);
         }
     }
 
     class BulletMover extends Controller {
-        /** Bullet that's moving*/
+        /** Bullet that's moving */
         TriMesh bullet;
-        /** Direciton of bullet*/
+
+        /** Direciton of bullet */
         Vector3f direction;
-        /** speed of bullet*/
+
+        /** speed of bullet */
         float speed = 10;
 
-        /** Seconds it will last before going away*/
+        /** Seconds it will last before going away */
         float lifeTime = 5;
+
         BulletMover(TriMesh bullet, Vector3f direction) {
-          this.bullet = bullet;
-          this.direction = direction;
-          this.direction.normalizeLocal();
+            this.bullet = bullet;
+            this.direction = direction;
+            this.direction.normalizeLocal();
         }
 
         public void update(float time) {
             lifeTime -= time;
-            /** If life is gone, remove it*/
+            /** If life is gone, remove it */
             if (lifeTime < 0) {
                 rootNode.detachChild(bullet);
                 bullet.removeController(this);
                 return;
             }
-            /** Move bullet*/
+            /** Move bullet */
             Vector3f bulletPos = bullet.getLocalTranslation();
             bulletPos.addLocal(direction.mult(time * speed));
             bullet.setLocalTranslation(bulletPos);
             /** Does the bullet intersect with target? */
-            if (Intersection.intersection(bullet.getWorldBound(),
-                                        target.getWorldBound())) {
+            if (Intersection.intersection(bullet.getWorldBound(), target
+                    .getWorldBound())) {
                 System.out.println("OWCH!!!");
-                target.setLocalTranslation(new Vector3f(r.nextFloat() * 10,
-                                                        r.nextFloat() * 10,
-                                                        r.nextFloat() * 10));
+                target.setLocalTranslation(new Vector3f(r.nextFloat() * 10, r
+                        .nextFloat() * 10, r.nextFloat() * 10));
                 lifeTime = 0;
+                snode.onEvent(hitEventID);
             }
         }
     }
 
     /**
-    * Called every frame for rendering
-    */
+     * Called every frame for rendering
+     */
     protected void simpleRender() {
         // Give control to the sound in case sound changes are needed.
-        SoundAPIController.getRenderer().draw(programSound);
+        SoundAPIController.getRenderer().draw(snode);
     }
 
     /**
-    * Called every frame for updating
-    */
+     * Called every frame for updating
+     */
     protected void simpleUpdate() {
         // Let the programmable sound update itself.
-        programSound.updateGeometricState(tpf, true);
+        snode.updateGeometricState(tpf, true);
     }
 }
