775e63a4e6e563272d141416850afafde1f851be
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-08-03 03:00:19 +0000

    javadoc
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1683 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/effects/transients/Transient.java
+++ src/com/jme/effects/transients/Transient.java
@@ -35,22 +35,51 @@
 package com.jme.effects.transients;
 import com.jme.scene.Node;
 /**
+ * A Transient is a node that has "stages" in its life.  The stages are set by
+ * the extended classes, as well as what going from one stage to another means.
  * @author Ahmed
+ * @author Jack Lindamood (javadoc only)
  */
-public class Transient extends Node {
+public abstract class Transient extends Node {
     private int maxNumOfStages, currentStage;
+    /**
+     * Creates a Transient Node.  Nothing is intialized by default.
+     * @param name The name of the node.
+     */
     public Transient(String name) {
         super(name);
     }
+    /**
+     * Returns the maximum number of stages for this node.
+     * @return The maximum number of stages for this node.
+     */
     public int getMaxNumOfStages() {
         return maxNumOfStages;
     }
+
+    /**
+     * Sets the maximum number of stages for this node.
+     * @param s The new maximum number of stages.
+     */
     public void setMaxNumOfStages(int s) {
         maxNumOfStages = s;
     }
+
+    /**
+     * Returns the current stage of this node.  Should be between 0 maximum
+     * number of stages.
+     * @return The current stage.
+     */
     public int getCurrentStage() {
         return currentStage;
     }
+
+    /**
+     * Sets the current stage to the integer given.  If the current stage value
+     * is greater than the maximum number of stages allowed, then it is trimmed
+     * to simply be the maximum number of stages.
+     * @param s The new current stage.
+     */
     public void setCurrentStage(int s) {
         if (s < getMaxNumOfStages()) {
             currentStage = s;
