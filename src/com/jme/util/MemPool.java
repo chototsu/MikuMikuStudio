d255fa7fd41b17a6e22f1fde4cde817b61d5bb47
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-08-17 22:15:07 +0000

    now inclues MemPool class.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1737 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/com/jme/util/MemPool.java
@@ -0,0 +1,25 @@
+package com.jme.util;
+
+import com.jme.math.Vector3f;
+import com.jme.math.Matrix3f;
+import com.jme.math.Vector2f;
+
+/**
+ *
+ * This is a class that contains temporary objects usable in functions during jME's
+ * execution.  This class and its variables are <b>NOT</b> thread safe.
+ *
+ * @author Jack Lindamood
+ */
+public class MemPool {
+    private MemPool(){}
+    public static Vector3f v3a=new Vector3f();
+    public static Vector3f v3b=new Vector3f();
+    public static Vector3f v3c=new Vector3f();
+    public static Vector3f v3d=new Vector3f();
+    public static Vector3f v3e=new Vector3f();
+    public static Matrix3f m3a=new Matrix3f();
+    public static Matrix3f m3b=new Matrix3f();
+    public static Vector2f v2a=new Vector2f();
+    public static Vector2f v2b=new Vector2f();
+}
