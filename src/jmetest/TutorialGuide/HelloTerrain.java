2be7bd7df2b48fb72c0f7773eea3164e63f6e885
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-08-19 22:24:07 +0000

    tut guide
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1754 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/jmetest/TutorialGuide/HelloTerrain.java
@@ -0,0 +1,137 @@
+package jmetest.TutorialGuide;
+
+import com.jme.app.SimpleGame;
+import com.jme.terrain.TerrainBlock;
+import com.jme.terrain.util.MidPointHeightMap;
+import com.jme.terrain.util.ImageBasedHeightMap;
+import com.jme.terrain.util.ProceduralTextureGenerator;
+import com.jme.math.Vector3f;
+import com.jme.bounding.BoundingBox;
+import com.jme.scene.state.TextureState;
+import com.jme.util.TextureManager;
+import com.jme.image.Texture;
+
+import java.net.URL;
+
+import javax.swing.*;
+
+/**
+ * Started Date: Aug 19, 2004<br><br>
+ *
+ * This program introduces jME's terrain utility classes and how they are used.  It
+ * goes over ProceduralTextureGenerator, ImageBasedHeightMap, MidPointHeightMap, and
+ * TerrainBlock.
+ * 
+ * @author Jack Lindamood
+ */
+public class HelloTerrain extends SimpleGame {
+    public static void main(String[] args) {
+        HelloTerrain app = new HelloTerrain();
+        app.setDialogBehaviour(SimpleGame.ALWAYS_SHOW_PROPS_DIALOG);
+        app.start();
+    }
+
+    protected void simpleInitGame() {
+        // First a hand made terrain
+        homeGrownHeightMap();
+        // Next an automatically generated terrain with a texture
+        generatedHeightMap();
+        // Finally a terrain loaded from a greyscale image with fancy textures on it.
+        complexTerrain();
+    }
+
+
+    private void homeGrownHeightMap() {
+        // The map for our terrain.  Each value is a height on the terrain
+        int[] map=new int[]{
+            1,2,3,4,
+            2,1,2,3,
+            3,2,1,2,
+            4,3,2,1
+        };
+
+        // Create a terrain block.  Our integer height values will scale on the map 2x larger x,
+        //   and 2x larger z.  Our map's origin will be the regular origin, and it won't create an
+        //   AreaClodMesh from it.
+        TerrainBlock tb=new TerrainBlock("block",4,
+                new Vector3f(2,1,2),
+                map,
+                new Vector3f(0,0,0),
+                false);
+        tb.setModelBound(new BoundingBox());
+        tb.updateModelBound();
+        rootNode.attachChild(tb);
+    }
+
+
+    private void generatedHeightMap() {
+        //  Use the helper class to create a terrain for us.  The terrain will be 64x64
+        MidPointHeightMap mph=new MidPointHeightMap(64,1.5f);
+        // Create a terrain block from the created terrain map.
+        TerrainBlock tb=new TerrainBlock("midpoint block",mph.getSize(),
+                new Vector3f(1,.11f,1),
+                mph.getHeightMap(),
+                new Vector3f(0,-25,0),false);
+        tb.setModelBound(new BoundingBox());
+        tb.updateModelBound();
+        rootNode.attachChild(tb);
+
+        URL grass=HelloTerrain.class.getClassLoader().getResource(
+            "jmetest/data/texture/grassb.png");
+        TextureState ts=display.getRenderer().createTextureState();
+        ts.setTexture(
+                TextureManager.loadTexture(grass,Texture.MM_LINEAR,Texture.FM_LINEAR,true)
+        );
+        tb.setRenderState(ts);
+        rootNode.attachChild(tb);
+    }
+
+    private void complexTerrain() {
+        URL greyScale=HelloTerrain.class.getClassLoader().getResource("jmetest/data/texture/bubble.jpg");
+
+        URL waterImage=HelloTerrain.class.getClassLoader().getResource("jmetest/data/texture/water.png");
+        URL dirtImage=HelloTerrain.class.getClassLoader().getResource("jmetest/data/texture/dirt.jpg");
+        URL highest=HelloTerrain.class.getClassLoader().getResource("jmetest/data/texture/highest.jpg");
+
+
+        //  Create an image height map based on the grey scale of our image.
+        ImageBasedHeightMap ib=new ImageBasedHeightMap(
+                new ImageIcon(greyScale).getImage()
+        );
+        // Create a terrain block from the image's grey scale
+        TerrainBlock tb=new TerrainBlock("image icon",ib.getSize(),
+                new Vector3f(.5f,.05f,.5f),ib.getHeightMap(),
+                new Vector3f(0,0,0),false);
+
+        //  Create an object to generate textured terrain from the image based height map.
+        ProceduralTextureGenerator pg=new ProceduralTextureGenerator(ib);
+        //  Look like water from height 0-60 with the strongest "water look" at 30
+        pg.addTexture(new ImageIcon(waterImage),0,30,60);
+        //  Look like dirt from height 40-120 with the strongest "dirt look" at 80
+        pg.addTexture(new ImageIcon(dirtImage),40,80,120);
+        //  Look like highest (pure white) from height 110-256 with the strongest "white look" at 130
+        pg.addTexture(new ImageIcon(highest),110,130,256);
+
+        //  Tell pg to create a texture from the ImageIcon's it has recieved.
+        pg.createTexture(256);
+        TextureState ts=display.getRenderer().createTextureState();
+        // Load the texture and assign it.
+        ts.setTexture(
+                TextureManager.loadTexture(
+                        pg.getImageIcon().getImage(),
+                        Texture.MM_LINEAR_LINEAR,
+                        Texture.FM_LINEAR,
+                        true,
+                        true
+                )
+        );
+        tb.setRenderState(ts);
+
+        tb.setModelBound(new BoundingBox());
+        tb.updateModelBound();
+
+        tb.setLocalTranslation(new Vector3f(0,0,-50));
+
+        rootNode.attachChild(tb);
+    }
+}
