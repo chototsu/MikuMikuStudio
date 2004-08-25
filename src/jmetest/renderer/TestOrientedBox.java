eca1c5a7d571fe0124e15a5ce503da35dd6c33cb
cep21 <cep21@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2004-08-25 03:31:04 +0000

    more descriptive test
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@1792 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/TestOrientedBox.java
+++ src/jmetest/renderer/TestOrientedBox.java
@@ -1,16 +1,14 @@
 package jmetest.renderer;
 
 import com.jme.app.SimpleGame;
-import com.jme.scene.model.XMLparser.JmeBinaryReader;
-import com.jme.scene.model.XMLparser.Converters.MaxToJme;
 import com.jme.scene.Node;
+import com.jme.scene.shape.Cylinder;
 import com.jme.math.Vector3f;
 import com.jme.math.Quaternion;
 import com.jme.math.FastMath;
+import com.jme.bounding.BoundingBox;
+import com.jme.bounding.OrientedBoundingBox;
 
-
-import java.io.*;
-import java.net.URL;
 
 
 /**
@@ -28,25 +26,56 @@ public class TestOrientedBox extends SimpleGame{
         app.start();
     }
 
+    Node AABBnode=new Node("AABBNode");
+    Node OBBnode=new Node("OBBNode");
+
     protected void simpleInitGame() {
-        try {
-            MaxToJme C1=new MaxToJme();
-            ByteArrayOutputStream BO=new ByteArrayOutputStream();
-            URL maxFile=TestOrientedBox.class.getClassLoader().getResource("jmetest/data/model/Character.3DS");
-            C1.convert(new BufferedInputStream(maxFile.openStream()),BO);
-            JmeBinaryReader jbr=new JmeBinaryReader();
-            jbr.setProperty("bound","obb");
-            Node r=jbr.loadBinaryFormat(new ByteArrayInputStream(BO.toByteArray()));
-            r.setLocalScale(.1f);
-            if (r.getChild(0).getControllers().size()!=0)
-                r.getChild(0).getController(0).setSpeed(20);
-            Quaternion temp=new Quaternion();
-            temp.fromAngleAxis(FastMath.PI/2,new Vector3f(-1,0,0));
-            rootNode.setLocalRotation(temp);
-            rootNode.attachChild(r);
-        } catch (IOException e) {
-            System.out.println("Damn exceptions:"+e);
-            e.printStackTrace();
+        {
+            Cylinder c=new Cylinder("cylinder",20,20,1,10);
+            c.setModelBound(new BoundingBox());
+            c.updateModelBound();
+            AABBnode.attachChild(c);
+        }
+        {
+            Cylinder c2=new Cylinder("cylinder2",20,20,1,10);
+            c2.setLocalTranslation(new Vector3f(5,10,0));
+            c2.setModelBound(new OrientedBoundingBox());
+            c2.updateModelBound();
+            OBBnode.attachChild(c2);
+        }
+        AABBnode.updateGeometricState(0,true);
+        AABBnode.updateRenderState();
+        OBBnode.updateGeometricState(0,true);
+        OBBnode.updateRenderState();
+
+        smallrotation=new Quaternion();
+        smallrotation.fromAngleNormalAxis(FastMath.PI/2,new Vector3f(0,1,0));
+
+        Quaternion upright=new Quaternion();
+        upright.fromAngleNormalAxis(FastMath.PI/2,new Vector3f(1,0,0));
+        OBBnode.setLocalRotation(new Quaternion(upright));
+        AABBnode.setLocalRotation(new Quaternion(upright));
+    }
+    Quaternion smallrotation;
+    protected void simpleUpdate(){
+        Quaternion q=new Quaternion();
+        q.slerp(smallrotation,tpf);
+        AABBnode.getLocalRotation().multLocal(
+                q);
+        OBBnode.getLocalRotation().multLocal(
+                q);
+
+
+        AABBnode.updateGeometricState(tpf,true);
+        OBBnode.updateGeometricState(tpf,true);
+
+    }
+    protected void simpleRender(){
+        display.getRenderer().draw(AABBnode);
+        display.getRenderer().draw(OBBnode);
+        if (showBounds){
+            display.getRenderer().drawBounds(AABBnode);
+            display.getRenderer().drawBounds(OBBnode);
         }
     }
 }
