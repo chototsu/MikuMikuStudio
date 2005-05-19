106df36b7b5bb70c1d842c0270eacee9e39df2f0
Mojomonkey <Mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2005-05-19 18:10:33 +0000

    Removed reliance on model bound. Using Target's instead.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@2216 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/scene/SharedMesh.java
+++ src/com/jme/scene/SharedMesh.java
@@ -31,74 +31,111 @@
  */
 package com.jme.scene;
 
+import com.jme.bounding.BoundingVolume;
 import com.jme.renderer.Renderer;
 import com.jme.scene.state.RenderState;
 
-
 /**
- * <code>SharedMesh</code> allows the sharing of data between multiple nodes. A
- * provided TriMesh is used as the model for this node. This allows the user to 
- * place multiple copies of the same object throughout the scene without having to
- * duplicate data. It should be known that any change to the provided target mesh
- * will affect the appearance of this mesh, including animations.
+ * <code>SharedMesh</code> allows the sharing of data between multiple nodes.
+ * A provided TriMesh is used as the model for this node. This allows the user
+ * to place multiple copies of the same object throughout the scene without
+ * having to duplicate data. It should be known that any change to the provided
+ * target mesh will affect the appearance of this mesh, including animations.
  * <br>
- * <b>Important:</b> It is highly recommended that the Target mesh is NOT placed into the
- * scenegraph, as it's translation, rotation and scale are replaced by the shared meshes
- * using it before they are rendered.
- * <br>
+ * <b>Important:</b> It is highly recommended that the Target mesh is NOT
+ * placed into the scenegraph, as it's translation, rotation and scale are
+ * replaced by the shared meshes using it before they are rendered. <br>
  * <b>Note:</b> Special thanks to Kevin Glass.
+ * 
  * @author Mark Powell
  * @version $id$
  */
 public class SharedMesh extends TriMesh {
-    private TriMesh target;
-    
-    /**
-     * Constructor creates a new <code>SharedMesh</code> object.
-     * @param name the name of this shared mesh.
-     * @param target the TriMesh to share the data.
-     */
-    public SharedMesh(String name, TriMesh target) {
-       super(name);
-       setTarget(target);
-    }
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * <code>setTarget</code> sets the shared data mesh. 
-     *
-     * @param target the TriMesh to share the data.
-     */
-    private void setTarget(TriMesh target) {
-        this.target = target;
-           
-       RenderState[] states = target.getRenderStateList();
-       for (int i=0;i<states.length;i++) {
-          if (states[i] != null) {
-             setRenderState(states[i]);
-          }
-       }
-       
-       this.setModelBound(target.getModelBound());
-           
-           
-    }
+	private TriMesh target;
 
-    /**
-     * draw renders the target mesh, at the translation, rotation and scale of
-     * this shared mesh.
-     * @see com.jme.scene.Spatial#draw(com.jme.renderer.Renderer)
-     */
-    public void draw(Renderer r) {
-       applyStates();
-       
-       if (!r.isProcessingQueue()) {
-          if (r.checkAndAdd(this))
-             return;
-       }
-       target.setLocalTranslation(worldTranslation);
-       target.setLocalRotation(worldRotation);
-       target.setLocalScale(worldScale);
-       
-       r.draw(target);
-    }
- }
+	/**
+	 * Constructor creates a new <code>SharedMesh</code> object.
+	 * 
+	 * @param name
+	 *            the name of this shared mesh.
+	 * @param target
+	 *            the TriMesh to share the data.
+	 */
+	public SharedMesh(String name, TriMesh target) {
+		super(name);
+		setTarget(target);
+	}
+
+	/**
+	 * <code>setTarget</code> sets the shared data mesh.
+	 * 
+	 * @param target
+	 *            the TriMesh to share the data.
+	 */
+	private void setTarget(TriMesh target) {
+		this.target = target;
+
+		RenderState[] states = target.getRenderStateList();
+		for (int i = 0; i < states.length; i++) {
+			if (states[i] != null) {
+				setRenderState(states[i]);
+			}
+		}
+	}
+
+	/**
+	 * <code>updateWorldBound</code> updates the bounding volume that contains
+	 * this geometry. The location of the geometry is based on the location of
+	 * all this node's parents.
+	 * 
+	 * @see com.jme.scene.Spatial#updateWorldBound()
+	 */
+	public void updateWorldBound() {
+		if (target.getModelBound() != null) {
+			worldBound = target.getModelBound().transform(worldRotation,
+					worldTranslation, worldScale, worldBound);
+		}
+	}
+
+	/**
+	 * <code>updateBound</code> recalculates the bounding object assigned to
+	 * the geometry. This resets it parameters to adjust for any changes to the
+	 * vertex information.
+	 * 
+	 */
+	public void updateModelBound() {
+		if (target.getModelBound() != null) {
+			target.getModelBound().computeFromPoints(vertex);
+			updateWorldBound();
+		}
+	}
+
+	/**
+	 * returns the model bound of the target object.
+	 */
+	public BoundingVolume getModelBound() {
+		return target.getModelBound();
+	}
+
+	/**
+	 * draw renders the target mesh, at the translation, rotation and scale of
+	 * this shared mesh.
+	 * 
+	 * @see com.jme.scene.Spatial#draw(com.jme.renderer.Renderer)
+	 */
+	public void draw(Renderer r) {
+		applyStates();
+
+		if (!r.isProcessingQueue()) {
+			if (r.checkAndAdd(this))
+				return;
+		}
+		target.setLocalTranslation(worldTranslation);
+		target.setLocalRotation(worldRotation);
+		target.setLocalScale(worldScale);
+
+		r.draw(target);
+	}
+}
