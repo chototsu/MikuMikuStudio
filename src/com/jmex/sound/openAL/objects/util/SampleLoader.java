8408804a6ff4c1b9c119b9b0b1d84c98edc61014
renanse <renanse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2005-06-21 01:40:40 +0000

    *** empty log message ***
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@2242 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jmex/sound/openAL/objects/util/SampleLoader.java
+++ src/com/jmex/sound/openAL/objects/util/SampleLoader.java
@@ -108,13 +108,13 @@ public class SampleLoader {
             ByteBuffer data = BufferUtils.createByteBuffer(byteOut.size());
             data.put(byteOut.toByteArray());
             data.rewind();
-//            if (ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN)
-//            {
-//                ShortBuffer tmp2 = data.duplicate().order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
-//                while(tmp2.hasRemaining())
-//                    data.putShort(tmp2.get());
-//                data.rewind();
-//            }
+            if (ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN)
+            {
+                ShortBuffer tmp2 = data.duplicate().order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
+                while(tmp2.hasRemaining())
+                    data.putShort(tmp2.get());
+                data.rewind();
+            }
             int channels = wavInput.channels();
             tmp = Buffer.generateBuffers(1);
             float time = (byteOut.size()) / (float)(wavInput.rate() * channels * 2);
@@ -156,13 +156,6 @@ public class SampleLoader {
             ByteBuffer data = BufferUtils.createByteBuffer(byteOut.size());
             data.put(byteOut.toByteArray());
             data.rewind();
-            if (ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN)
-            {
-                ShortBuffer tmp2 = data.duplicate().order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
-                while(tmp2.hasRemaining())
-                    data.putShort(tmp2.get());
-                data.rewind();
-            }
             int channels = oggInput.getInfo().channels;
             tmp = Buffer.generateBuffers(1);
             float time = (byteOut.size()) / (float)(oggInput.getInfo().rate * oggInput.getInfo().channels * 2);
