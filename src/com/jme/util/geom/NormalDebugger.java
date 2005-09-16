0d4b09624ec49ccd246e668986e716dc73a59b36
Mojomonkey <Mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2005-09-16 21:36:26 +0000

    Javadoc'd
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@2300 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/util/geom/NormalDebugger.java
+++ src/com/jme/util/geom/NormalDebugger.java
@@ -48,10 +48,25 @@ import com.jme.scene.state.LightState;
 
 /**
  * <code>NormalDebugger</code> is a simple utility allowing you to quickly see
- * the normals on a given section of the scenegraph.
+ * the normals on a given section of the scenegraph. Lines are added for every
+ * normal in the selected section of the scenegraph. These lines are added to
+ * the node that contains the geometry for which they represent the normals,
+ * thus allowing them to follow any transformation of any of the nodes between
+ * the geometry and the root.
+ *
+ * <p>
+ * To add lines for a section of the scenegraph (either a node or a geometry):
+ * <pre>
+ *   NormalDebugger.getInstance().register(section);
+ * </pre>
+ *
+ * To remove the lines for the section:
+ * <pre>
+ *   NormalDebugger.getInstance().unregister(section);
+ * </pre>
  * 
  * @author Emond Papegaaij
- * @version $Id: NormalDebugger.java,v 1.1 2005-09-15 21:29:58 renanse Exp $
+ * @version $Id: NormalDebugger.java,v 1.2 2005-09-16 21:36:26 Mojomonkey Exp $
  */
 public class NormalDebugger {
 	private static NormalDebugger INSTANCE;
@@ -59,9 +74,18 @@ public class NormalDebugger {
 	private Map lineNodes = new HashMap();
 	private Map children = new HashMap();
 
+	/**
+	 * Hide the default constructor as <code>NormalDebugger</code> is singleton.
+	 */
 	private NormalDebugger() {
 	}
 
+	/**
+	 * Returns the singleton instance of the <code>NormalDebugger</code>. It is
+	 * safe to get the instance before the initialization of the display system.
+	 * @return The singleton instance of the <code>NormalDebugger</code>, which
+	 * will be created on the first call.
+	 */
 	public static NormalDebugger getInstance() {
 		if (INSTANCE == null) {
 			INSTANCE = new NormalDebugger();
@@ -69,6 +93,11 @@ public class NormalDebugger {
 		return INSTANCE;
 	}
 
+	/**
+	 * Internal method that adds the normal lines to the given Geometry. The lines
+	 * will be added to a node, which will be added to the parent node of the
+	 * Geometry.
+	 */
 	private void addNormalsToParent(Geometry g) {
 		Node lineNode = new Node("Normals for "+g.getName());
 		g.getParent().attachChild(lineNode);
@@ -96,6 +125,10 @@ public class NormalDebugger {
 		lineNodes.put(g, lineNode);
 	}
 
+	/**
+	 * Internal method that recursivelly processes a node by adding the normal
+	 * lines to all children of the node.
+	 */
 	private void processNode(Node nodeToProcess) {
 		List nodeChildren = new ArrayList();
 		children.put(nodeToProcess, nodeChildren);
@@ -112,14 +145,34 @@ public class NormalDebugger {
 		}
 	}
 
+	/**
+	 * Registers the given <code>Geometry</code> for normal debugging. The normal
+	 * lines will be drawn immediately. Because the lines will be added to the
+	 * parent node of the <code>Geometry</code>, it is an error to call this
+	 * method with a <code>Geometry</code> that is not yet attached.
+	 * @param g The <code>Geometry</code> to add the normals to.
+	 */
 	public void register(Geometry g) {
 		addNormalsToParent(g);
 	}
 
+	/**
+	 * Registers the given <code>Node</code> for normal debugging. Registering a
+	 * <code>Node</code> will result in all children of the <code>Node</code>
+	 * being registered. The normal lines will be drawn immediately.
+	 * @param n The <code>Node</code> that contains the geometries to add the
+	 * normals to.
+	 */
 	public void register(Node n) {
 		processNode(n);
 	}
 
+	/**
+	 * Registers the given <code>Spatial</code> for normal debugging. This method
+	 * is provided for convenience and will simply call the register method for
+	 * either a <code>Geometry</code> or a <code>Node</code>.
+	 * @param s The <code>Spatial</code> to register.
+	 */
 	public void register(Spatial s) {
 		if (s instanceof Geometry) {
 			register((Geometry) s);
@@ -128,6 +181,12 @@ public class NormalDebugger {
 		}
 	}
 
+	/**
+	 * Unregisters the given <code>Geometry</code> for normal debugging. This will
+	 * remove all normal lines immediately. When the given <code>Geometry</code>
+	 * is currently not registered, nothing is done.
+	 * @param g The <code>Geometry</code> to unregister.
+	 */
 	public void unregister(Geometry g) {
 		Node lines = (Node)lineNodes.remove(g);
 		if (lines != null) {
@@ -135,6 +194,12 @@ public class NormalDebugger {
 		}
 	}
 
+	/**
+	 * Unregisters the given <code>Node</code> for normal debugging. This will
+	 * recursively unregister all children of the <code>Node</code>. When the
+	 * given <code>Node</code> is currently not registered, nothing is done.
+	 * @param n The <code>Node</code> to unregister.
+	 */
 	public void unregister(Node n) {
 		List nodeChildren = (List)children.remove(n);
 		if (nodeChildren != null) {
@@ -145,6 +210,12 @@ public class NormalDebugger {
 		}
 	}
 
+	/**
+	 * Unregisters the given <code>Spatial</code> for normal debugging. This
+	 * method is provided for convenience and will simply call the unregister
+	 * method for either a <code>Geometry</code> or a <code>Node</code>.
+	 * @param s The <code>Spatial</code> to unregister.
+	 */
 	public void unregister(Spatial s) {
 		if (s instanceof Geometry) {
 			unregister((Geometry) s);
@@ -153,6 +224,13 @@ public class NormalDebugger {
 		}
 	}
 
+	/**
+	 * Updates the normals for the given <code>Spatial</code>. Currently this will
+	 * result in all normals being removed and recreated. Calling this method
+	 * should only be needed when one of the local transformations of a
+	 * <code>Geometry</code> have changed and/or new objects were added/removed.
+	 * @param s The <code>Spatial</code> to update.
+	 */
 	public void update(Spatial s) {
 		unregister(s);
 		register(s);
