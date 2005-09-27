8829757de5dc2cdd81ff3185e9942d81c0be59b0
Mojomonkey <Mojomonkey@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2005-09-27 21:40:40 +0000

    Lesson 2 added.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@2392 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/jmetest/flagrushtut/Lesson2.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2003-2005 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package jmetest.flagrushtut;
+
+import com.jme.app.BaseGame;
+import com.jme.bounding.BoundingBox;
+import com.jme.image.Texture;
+import com.jme.input.InputSystem;
+import com.jme.input.KeyBindingManager;
+import com.jme.input.KeyInput;
+import com.jme.math.Vector3f;
+import com.jme.renderer.Camera;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.Node;
+import com.jme.scene.shape.Sphere;
+import com.jme.scene.state.TextureState;
+import com.jme.system.DisplaySystem;
+import com.jme.system.JmeException;
+import com.jme.util.TextureManager;
+import com.jme.util.Timer;
+
+/**
+ * <code>Tutorial 2</code> shows how to build our own Application 
+ * framework for Flag Rush.
+ * For Flag Rush Tutorial Series.
+ * @author Mark Powell
+ */
+public class Lesson2 extends BaseGame {
+	protected Timer timer;
+	//Our camera object for viewing the scene
+	private Camera cam;
+	//the root node of the scene graph
+	private Node scene;
+	//Reference to the keyboard
+	private KeyInput key;
+	//TextureState to show the monkey on the sphere.
+	private TextureState ts;
+	//display attributes for the window. We will keep these values
+	//to allow the user to change them
+	private int width, height, depth, freq;
+	private boolean fullscreen;
+
+	/**
+	 * Main entry point of the application
+	 */
+	public static void main(String[] args) {
+		Lesson2 app = new Lesson2();
+		//We will load our own "fantastic" Flag Rush logo. Yes, I'm an artist.
+		app.setDialogBehaviour(ALWAYS_SHOW_PROPS_DIALOG, Lesson2.class.getClassLoader()
+				.getResource("jmetest/data/images/FlagRush.png"));
+		app.start();
+	}
+
+	/**
+	 * During an update we only look for the escape button and update the timer
+	 * to get the framerate.
+	 * 
+	 * @see com.jme.app.SimpleGame#update()
+	 */
+	protected void update(float interpolation) {
+		//update the time to get the framerate
+		timer.update();
+		interpolation = timer.getTimePerFrame();
+		//if escape was pressed, we exit
+		if (KeyBindingManager.getKeyBindingManager().isValidCommand("exit")) {
+			System.exit(0);
+		}
+	}
+
+	/**
+	 * draws the scene graph
+	 * 
+	 * @see com.jme.app.SimpleGame#render()
+	 */
+	protected void render(float interpolation) {
+		//Clear the screen
+		display.getRenderer().clearBuffers();
+
+		display.getRenderer().draw(scene);
+
+	}
+
+	/**
+	 * initializes the display and camera.
+	 * 
+	 * @see com.jme.app.SimpleGame#initSystem()
+	 */
+	protected void initSystem() {
+		//store the properties information
+		width = properties.getWidth();
+		height = properties.getHeight();
+		depth = properties.getDepth();
+		freq = properties.getFreq();
+		fullscreen = properties.getFullscreen();
+		
+		try {
+			display = DisplaySystem.getDisplaySystem(properties.getRenderer());
+			display.createWindow(width, height, depth, freq, fullscreen);
+
+			cam = display.getRenderer().createCamera(width, height);
+		} catch (JmeException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		//set the background to black
+		display.getRenderer().setBackgroundColor(ColorRGBA.black);
+		
+		//initialize the camera
+		cam.setFrustumPerspective(45.0f, (float)width / (float)height, 1, 1000);
+		Vector3f loc = new Vector3f(0.0f, 0.0f, 25.0f);
+		Vector3f left = new Vector3f(-1.0f, 0.0f, 0.0f);
+		Vector3f up = new Vector3f(0.0f, 1.0f, 0.0f);
+		Vector3f dir = new Vector3f(0.0f, 0f, -1.0f);
+		// Move our camera to a correct place and orientation.
+		cam.setFrame(loc, left, up, dir);
+		/** Signal that we've changed our camera's location/frustum. */
+		cam.update();
+
+		InputSystem.createInputSystem(properties.getRenderer());
+		key = InputSystem.getKeyInput();
+		KeyBindingManager.getKeyBindingManager().setKeyInput(key);
+		display.getRenderer().setCamera(cam);
+
+		KeyBindingManager.getKeyBindingManager().set("exit",
+				KeyInput.KEY_ESCAPE);
+	}
+
+	/**
+	 * initializes the scene
+	 * 
+	 * @see com.jme.app.SimpleGame#initGame()
+	 */
+	protected void initGame() {
+		scene = new Node("Scene graph node");
+		cam.update();
+
+		//Create our Sphere
+		Sphere s = new Sphere("Sphere", 30, 30, 25);
+		s.setLocalTranslation(new Vector3f(0, 0, -40));
+		s.setModelBound(new BoundingBox());
+		s.updateModelBound();
+
+		ts = display.getRenderer().createTextureState();
+		ts.setEnabled(true);
+		ts.setTexture(TextureManager.loadTexture(Lesson2.class.getClassLoader()
+				.getResource("jmetest/data/images/Monkey.jpg"),
+				Texture.MM_LINEAR_LINEAR, Texture.FM_LINEAR));
+
+		s.setRenderState(ts);
+
+		scene.attachChild(s);
+
+		//update the scene graph for rendering
+		scene.updateGeometricState(0.0f, true);
+		scene.updateRenderState();
+	}
+
+	/**
+	 * will be called if the resolution changes
+	 * 
+	 * @see com.jme.app.SimpleGame#reinit()
+	 */
+	protected void reinit() {
+		display.recreateWindow(width, height, depth, freq, fullscreen);
+	}
+
+	/**
+	 * clean up the textures.
+	 * 
+	 * @see com.jme.app.SimpleGame#cleanup()
+	 */
+	protected void cleanup() {
+		ts.deleteAll();
+
+	}
+}
