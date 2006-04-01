a1cbee412059c2de94a7e9b560704ce963d271e7
irrisor <irrisor@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2006-04-01 10:40:56 +0000

    Issue 192: made a textcase for calcWelzl - performs fine - the error was solely in mergeOBB (see issue comment)
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@2891 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- junit/jmetest/bounds/BoundsTest.java
+++ junit/jmetest/bounds/BoundsTest.java
@@ -1,5 +1,7 @@
 package jmetest.bounds;
 
+import java.nio.FloatBuffer;
+
 import com.jme.bounding.BoundingBox;
 import com.jme.bounding.BoundingSphere;
 import com.jme.bounding.OrientedBoundingBox;
@@ -38,9 +40,54 @@ public class BoundsTest extends junit.framework.TestCase {
 
         BoundingSphere merged = (BoundingSphere) sphere.merge( obb );
         BoundingSphere merged2 = (BoundingSphere) merged.merge( obb );
+        BoundingSphere merged3 = (BoundingSphere) merged2.merge( obb );
         System.out.println( merged );
         System.out.println( merged2 );
+        System.out.println( merged3 );
         assertEquals( "center", merged.getCenter(), merged2.getCenter() );
         assertEquals( "radius", merged.getRadius(), merged2.getRadius(), FastMath.FLT_EPSILON );
+        assertEquals( "center", merged.getCenter(), merged3.getCenter() );
+        assertEquals( "radius", merged.getRadius(), merged3.getRadius(), FastMath.FLT_EPSILON );
+    }
+
+    public void testComputeSphereFromPoints() {
+        BoundingSphere sphere = new BoundingSphere();
+        float radius = 2;
+        checkPointsOnCircle( radius, sphere, 2 );
+        checkPointsOnCircle( radius, sphere, 3 );
+        checkPointsOnCircle( radius, sphere, 4 );
+        checkPointsOnCircle( radius, sphere, 5 );
+        checkPointsOnCircle( radius, sphere, 10 );
+
+        checkPointsOnSphere( radius, sphere, 2 );
+        checkPointsOnSphere( radius, sphere, 3 );
+        checkPointsOnSphere( radius, sphere, 4 );
+    }
+
+    private void checkPointsOnCircle( float radius, BoundingSphere sphere, int points ) {
+        FloatBuffer buffer;
+        buffer = FloatBuffer.allocate( points*2*3 );
+        for ( int i=0; i<points; i++ ) {
+            buffer.put( FastMath.cos( 2*i*FastMath.PI/points ) * ( i * radius / points ) )
+                    .put( FastMath.sin( 2*i*FastMath.PI/points ) * ( i * radius / points ) ).put( 0 );
+        }
+        for ( int i=0; i<points; i++ ) {
+            buffer.put( FastMath.cos( 2*i*FastMath.PI/points ) * radius ).put( FastMath.sin( 2*i*FastMath.PI/points ) * radius ).put( 0 );
+        }
+        sphere.computeFromPoints( buffer );
+        assertEquals( "radius from "+(points*2)+" points", radius, sphere.getRadius(), 0.001f );
+    }
+
+    private void checkPointsOnSphere( float radius, BoundingSphere sphere, int points ) {
+        FloatBuffer buffer;
+        buffer = FloatBuffer.allocate( points*2*3 );
+        for ( int i=0; i<points; i++ ) {
+            buffer.put( FastMath.cos( 2*i*FastMath.PI/points ) * ( radius * 0.9f ) ).put( FastMath.sin( 2*i*FastMath.PI/points ) * ( radius * 0.9f ) ).put( 0 );
+        }
+        for ( int i=0; i<points; i++ ) {
+            buffer.put( 0 ).put( FastMath.cos( 2*i*FastMath.PI/points ) * radius ).put( FastMath.sin( 2*i*FastMath.PI/points ) * radius );
+        }
+        sphere.computeFromPoints( buffer );
+        assertEquals( "radius from "+points+" points", radius, sphere.getRadius(), 0.001f );
     }
 }
