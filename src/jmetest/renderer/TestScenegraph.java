4b24fc14da6bcc4dca08e9541e0620128853806a
irrisor <irrisor@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2006-08-12 12:19:59 +0000

    MINOR: removed non-uniform scale (checked in by error)
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@3159 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/jmetest/renderer/TestScenegraph.java
+++ src/jmetest/renderer/TestScenegraph.java
@@ -61,7 +61,7 @@ import com.jme.util.geom.BufferUtils;
  * <code>TestScenegraph</code>
  *
  * @author Mark Powell
- * @version $Id: TestScenegraph.java,v 1.38 2006-06-23 22:31:56 nca Exp $
+ * @version $Id: TestScenegraph.java,v 1.39 2006-08-12 12:19:59 irrisor Exp $
  */
 public class TestScenegraph extends SimpleGame {
 
@@ -293,7 +293,6 @@ public class TestScenegraph extends SimpleGame {
         scene.attachChild(node1);
         rootNode.attachChild(line);
         rootNode.attachChild(scene);
-        rootNode.setLocalScale(new Vector3f(2,1,1));
         scene.attachChild(selectionBox);
 
         nc1 = new NodeHandler(node1, 5, 1 );
