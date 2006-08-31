a55cd6f95e47baad49a92870f59ea78f69a7dc20
rherlitz <rherlitz@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2006-08-31 22:11:10 +0000

    First iteration of a motionblur renderpass from an nvidia doc
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@3188 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/com/jmex/effects/glsl/MotionBlurRenderPass.java
@@ -0,0 +1,308 @@
+/*
+ * Copyright (c) 2003-2006 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jmex.effects.glsl;
+
+import com.jme.image.Texture;
+import com.jme.math.Matrix4f;
+import com.jme.renderer.*;
+import com.jme.renderer.pass.Pass;
+import com.jme.scene.Node;
+import com.jme.scene.Spatial;
+import com.jme.scene.state.*;
+import com.jme.system.DisplaySystem;
+import com.jme.util.LoggingSystem;
+import org.lwjgl.opengl.OpenGLException;
+import org.lwjgl.opengl.Util;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+
+/**
+ * GLSL motion blur pass.
+ *
+ * @author Rikard Herlitz (MrCoder) - initial implementation
+ */
+public class MotionBlurRenderPass extends Pass {
+	private static final long serialVersionUID = 1L;
+
+	private TextureRenderer tRenderer;
+	private Texture mainTexture;
+
+	private AlphaState alphaObj;
+	private CullState cullObj;
+	private TextureState tsObj;
+	private float blurStrength = -0.000035f;
+
+	private GLSLShaderObjectsState motionBlurShader;
+
+	private boolean freeze = false;
+	private boolean supported = true;
+	private boolean useCurrentScene = false;
+
+	private class DynamicObject {
+		public Spatial spatial;
+		public Matrix4f modelMatrix = new Matrix4f();
+		public Matrix4f modelViewMatrix = new Matrix4f();
+		public Matrix4f modelViewProjectionMatrix = new Matrix4f();
+	}
+
+	private ArrayList<DynamicObject> dynamicObjects = new ArrayList<DynamicObject>();
+
+	private Matrix4f tmpMatrix = new Matrix4f();
+	private Matrix4f projectionMatrix = new Matrix4f();
+
+	private float tpf = 0.0f;
+	private Camera cam;
+
+	public void addMotionBlurSpatial( Spatial spatial ) {
+		DynamicObject dynamicObject = new DynamicObject();
+		dynamicObject.spatial = spatial;
+		dynamicObjects.add( dynamicObject );
+	}
+
+	/**
+	 * Reset bloom parameters to default
+	 */
+	public void resetParameters() {
+	}
+
+	/**
+	 * Release pbuffers in TextureRenderer's. Preferably called from user cleanup method.
+	 */
+	public void cleanup() {
+		super.cleanUp();
+		if( tRenderer != null )
+			tRenderer.cleanup();
+	}
+
+	public boolean isSupported() {
+		return supported;
+	}
+
+	/**
+	 * Creates a new bloom renderpass
+	 *
+	 * @param cam		 Camera used for rendering the bloomsource
+	 * @param renderScale Scale of bloom texture
+	 */
+	public MotionBlurRenderPass( Camera cam, int renderScale ) {
+		this.cam = cam;
+		DisplaySystem display = DisplaySystem.getDisplaySystem();
+
+		resetParameters();
+
+		//Create texture renderers and rendertextures(alternating between two not to overwrite pbuffers)
+		tRenderer = display.createTextureRenderer(
+				display.getWidth() / renderScale, display.getHeight() / renderScale, false, true, false, false,
+				TextureRenderer.RENDER_TEXTURE_2D, 0 );
+		tRenderer.setBackgroundColor( new ColorRGBA( 0.0f, 0.0f, 0.0f, 1.0f ) );
+		tRenderer.setCamera( cam );
+		tRenderer.forceCopy( true );
+
+		mainTexture = new Texture();
+		mainTexture.setWrap( Texture.WM_ECLAMP_S_ECLAMP_T );
+		mainTexture.setFilter( Texture.FM_LINEAR );
+		tRenderer.setupTexture( mainTexture );
+
+		//Create extract intensity shader
+		motionBlurShader = display.getRenderer().createGLSLShaderObjectsState();
+		if( !motionBlurShader.isSupported() ) {
+			supported = false;
+		}
+		else {
+			reloadShader();
+		}
+
+		tsObj = display.getRenderer().createTextureState();
+		tsObj.setEnabled( true );
+		tsObj.setTexture( mainTexture, 0 );
+
+		cullObj = display.getRenderer().createCullState();
+		cullObj.setEnabled( true );
+		cullObj.setCullMode( CullState.CS_BACK );
+
+		alphaObj = display.getRenderer().createAlphaState();
+		alphaObj.setEnabled( true );
+		alphaObj.setBlendEnabled( true );
+		alphaObj.setSrcFunction( AlphaState.SB_SRC_ALPHA );
+		alphaObj.setDstFunction( AlphaState.DB_ONE_MINUS_SRC_ALPHA );
+	}
+
+	public void reloadShader() {
+		GLSLShaderObjectsState testShader = DisplaySystem.getDisplaySystem().getRenderer().createGLSLShaderObjectsState();
+		try {
+			testShader.load( MotionBlurRenderPass.class.getClassLoader().getResource( "com/jmex/effects/glsl/data/motionblur.vert" ),
+							 MotionBlurRenderPass.class.getClassLoader().getResource( "com/jmex/effects/glsl/data/motionblur.frag" ) );
+			testShader.apply();
+			Util.checkGLError();
+		} catch( OpenGLException e ) {
+			e.printStackTrace();
+			return;
+		}
+
+		motionBlurShader.load( MotionBlurRenderPass.class.getClassLoader().getResource( "com/jmex/effects/glsl/data/motionblur.vert" ),
+							   MotionBlurRenderPass.class.getClassLoader().getResource( "com/jmex/effects/glsl/data/motionblur.frag" ) );
+
+		motionBlurShader.clearUniforms();
+		motionBlurShader.setUniform( "screenTexture", 0 );
+		motionBlurShader.setUniform( "prevModelViewMatrix", new Matrix4f(), false );
+		motionBlurShader.setUniform( "prevModelViewProjectionMatrix", new Matrix4f(), false );
+		motionBlurShader.setUniform( "halfWinSize", DisplaySystem.getDisplaySystem().getWidth() * 0.5f, DisplaySystem.getDisplaySystem().getHeight() * 0.5f );
+		motionBlurShader.setUniform( "blurStrength", blurStrength );
+		motionBlurShader.apply();
+
+		LoggingSystem.getLogger().log( Level.INFO, "Shader reloaded..." );
+	}
+
+	public Texture getMainTexture() {
+		return mainTexture;
+	}
+
+	/**
+	 * Helper class to get all spatials rendered in one TextureRenderer.render() call.
+	 */
+	private class SpatialsRenderNode extends Node {
+		private static final long serialVersionUID = 7367501683137581101L;
+
+		public void draw( Renderer r ) {
+			Spatial child;
+			for( int i = 0, cSize = spatials.size(); i < cSize; i++ ) {
+				child = spatials.get( i );
+				if( child != null )
+					child.onDraw( r );
+			}
+		}
+
+		public void onDraw( Renderer r ) {
+			draw( r );
+		}
+	}
+
+	private final SpatialsRenderNode spatialsRenderNode = new SpatialsRenderNode();
+
+	@Override
+	protected void doUpdate( float tpf ) {
+		super.doUpdate( tpf );
+		this.tpf = tpf;
+	}
+
+	public void doRender( Renderer r ) {
+		if( !useCurrentScene && spatials.size() == 0 ) {
+			return;
+		}
+
+		// see if we should use the current scene to bloom, or only things added to the pass.
+		if( useCurrentScene ) {
+			// grab backbuffer to texture
+			tRenderer.copyBufferToTexture( mainTexture,
+										   DisplaySystem.getDisplaySystem().getWidth(),
+										   DisplaySystem.getDisplaySystem().getHeight(),
+										   1 );
+		}
+		else {
+			//Render scene to texture
+			tRenderer.updateCamera();
+			tRenderer.render( spatialsRenderNode, mainTexture );
+		}
+
+		Renderer.enforceState( motionBlurShader );
+		Renderer.enforceState( tsObj );
+		Renderer.enforceState( cullObj );
+
+		for( int i = 0; i < dynamicObjects.size(); i++ ) {
+			DynamicObject dynamicObject = dynamicObjects.get( i );
+
+			motionBlurShader.setUniform( "prevModelViewMatrix", dynamicObject.modelViewMatrix, false );
+			motionBlurShader.setUniform( "prevModelViewProjectionMatrix", dynamicObject.modelViewProjectionMatrix, false );
+			motionBlurShader.setUniform( "blurStrength", blurStrength / tpf );
+			motionBlurShader.apply();
+
+			r.draw( dynamicObject.spatial );
+			r.renderQueue();
+		}
+
+		Renderer.clearEnforcedState( RenderState.RS_GLSL_SHADER_OBJECTS );
+		Renderer.clearEnforcedState( RenderState.RS_TEXTURE );
+		Renderer.clearEnforcedState( RenderState.RS_CULL );
+
+		((AbstractCamera) cam).update();
+
+		projectionMatrix.set( ((AbstractCamera) cam).getProjectionMatrix() );
+		for( int i = 0; i < dynamicObjects.size(); i++ ) {
+			DynamicObject dynamicObject = dynamicObjects.get( i );
+			Matrix4f modelMatrix = dynamicObject.modelMatrix;
+			Matrix4f modelViewMatrix = dynamicObject.modelViewMatrix;
+			Matrix4f modelViewProjectionMatrix = dynamicObject.modelViewProjectionMatrix;
+			Spatial spatial = dynamicObject.spatial;
+
+			if( !freeze ) {
+				modelMatrix.loadIdentity();
+				spatial.getWorldRotation().toRotationMatrix( tmpMatrix );
+				modelMatrix.multLocal( tmpMatrix );
+				modelMatrix.m00 *= spatial.getWorldScale().x;
+				modelMatrix.m11 *= spatial.getWorldScale().y;
+				modelMatrix.m22 *= spatial.getWorldScale().z;
+				modelMatrix.setTranslation( spatial.getWorldTranslation() );
+				modelMatrix.transpose();
+			}
+
+			modelViewMatrix.set( modelMatrix );
+			modelViewMatrix.multLocal( ((AbstractCamera) cam).getModelViewMatrix() );
+			modelViewProjectionMatrix.set( modelViewMatrix ).multLocal( projectionMatrix );
+		}
+	}
+
+	public boolean useCurrentScene() {
+		return useCurrentScene;
+	}
+
+	public void setUseCurrentScene( boolean useCurrentScene ) {
+		this.useCurrentScene = useCurrentScene;
+	}
+
+	public boolean isFreeze() {
+		return freeze;
+	}
+
+	public void setFreeze( boolean freeze ) {
+		this.freeze = freeze;
+	}
+
+	public float getBlurStrength() {
+		return blurStrength;
+	}
+
+	public void setBlurStrength( float blurStrength ) {
+		this.blurStrength = blurStrength;
+	}
+}
---
+++ src/jmetest/effects/glsl/TestMotionBlur.java
@@ -0,0 +1,303 @@
+/*
+ * Copyright (c) 2003-2006 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package jmetest.effects.glsl;
+
+import com.jme.app.SimplePassGame;
+import com.jme.bounding.BoundingBox;
+import com.jme.image.Texture;
+import com.jme.input.KeyBindingManager;
+import com.jme.input.KeyInput;
+import com.jme.light.PointLight;
+import com.jme.math.FastMath;
+import com.jme.math.Vector3f;
+import com.jme.renderer.ColorRGBA;
+import com.jme.renderer.Renderer;
+import com.jme.renderer.pass.RenderPass;
+import com.jme.scene.Node;
+import com.jme.scene.SceneElement;
+import com.jme.scene.Spatial;
+import com.jme.scene.Text;
+import com.jme.scene.shape.Box;
+import com.jme.scene.shape.Quad;
+import com.jme.scene.shape.Sphere;
+import com.jme.scene.shape.Torus;
+import com.jme.scene.state.LightState;
+import com.jme.scene.state.TextureState;
+import com.jme.util.TextureManager;
+import com.jmex.effects.glsl.MotionBlurRenderPass;
+
+/**
+ * Motion blur effect pass test.
+ *
+ * @author Rikard Herlitz (MrCoder)
+ */
+public class TestMotionBlur extends SimplePassGame {
+	private MotionBlurRenderPass motionBlurRenderPass;
+	private int screenshotIndex = 0;
+	private Node debugQuadsNode;
+
+	private Spatial torus;
+	private Spatial sphere;
+
+	public static void main( String[] args ) {
+		TestMotionBlur app = new TestMotionBlur();
+		app.setDialogBehaviour( FIRSTRUN_OR_NOCONFIGFILE_SHOW_PROPS_DIALOG );
+		app.start();
+	}
+
+	protected void cleanup() {
+		super.cleanup();
+		if( motionBlurRenderPass != null )
+			motionBlurRenderPass.cleanup();
+	}
+
+	protected void simpleInitGame() {
+		display.setTitle( "MotionBlur Test" );
+
+		//Setup camera
+		cam.setFrustumPerspective( 45.0f, (float) display.getWidth() / (float) display.getHeight(), 1, 5000 );
+		cam.setLocation( new Vector3f( 200, 150, 200 ) );
+		cam.lookAt( new Vector3f( 0, 0, 0 ), Vector3f.UNIT_Y );
+		cam.update();
+
+		setupKeyBindings();
+
+		//Setup lights
+		PointLight light = new PointLight();
+		light.setDiffuse( new ColorRGBA( 1.0f, 1.0f, 1.0f, 1.0f ) );
+		light.setAmbient( new ColorRGBA( 0.5f, 0.5f, 0.5f, 1.0f ) );
+		light.setLocation( new Vector3f( 0, 30, 0 ) );
+		light.setEnabled( true );
+		lightState.attach( light );
+
+		//Add dummy objects to rootNode
+		rootNode.attachChild( createObjects() );
+
+		//Setup renderpasses
+		RenderPass rootPass = new RenderPass();
+		rootPass.add( rootNode );
+		pManager.add( rootPass );
+
+		motionBlurRenderPass = new MotionBlurRenderPass( cam, 1 );
+
+		if( !motionBlurRenderPass.isSupported() ) {
+			Text t = new Text( "Text", "GLSL Not supported on this computer." );
+			t.setRenderQueueMode( Renderer.QUEUE_ORTHO );
+			t.setLightCombineMode( LightState.OFF );
+			t.setLocalTranslation( new Vector3f( 0, 20, 0 ) );
+			fpsNode.attachChild( t );
+		}
+		else {
+			motionBlurRenderPass.add( rootNode );
+			motionBlurRenderPass.addMotionBlurSpatial( sphere );
+			motionBlurRenderPass.addMotionBlurSpatial( torus );
+			motionBlurRenderPass.setUseCurrentScene( true );
+			pManager.add( motionBlurRenderPass );
+		}
+
+		createDebugQuads();
+		fpsNode.attachChild( debugQuadsNode );
+
+		RenderPass fpsPass = new RenderPass();
+		fpsPass.add( fpsNode );
+		pManager.add( fpsPass );
+	}
+
+	protected void simpleUpdate() {
+		if( !motionBlurRenderPass.isFreeze() ) {
+			float time = (FastMath.sin( timer.getTimeInSeconds() * 0.5f ) + 1.0f) * 10.0f;
+			torus.getLocalRotation().fromAngles( time * 2f, time * 2.0f, time * 1.0f );
+			torus.getLocalTranslation().x = FastMath.sin( time * 1.0f ) * 100.0f;
+			torus.getLocalTranslation().z = FastMath.cos( time * 1.0f ) * 100.0f;
+
+			float scale = (FastMath.sin( timer.getTimeInSeconds() * 10.0f ) + 2.0f) * 0.5f;
+			sphere.getLocalScale().set( scale, scale, scale );
+			sphere.getLocalTranslation().z = FastMath.sin( time * 1.0f ) * 200.0f;
+		}
+
+		if( KeyBindingManager.getKeyBindingManager().isValidCommand( "1", false ) ) {
+			motionBlurRenderPass.setEnabled( !motionBlurRenderPass.isEnabled() );
+		}
+
+		if( KeyBindingManager.getKeyBindingManager().isValidCommand( "2", false ) ) {
+			motionBlurRenderPass.setBlurStrength( motionBlurRenderPass.getBlurStrength() * 0.5f );
+		}
+		if( KeyBindingManager.getKeyBindingManager().isValidCommand( "3", false ) ) {
+			motionBlurRenderPass.setBlurStrength( motionBlurRenderPass.getBlurStrength() * 2.0f );
+		}
+
+		if( KeyBindingManager.getKeyBindingManager().isValidCommand( "f", false ) ) {
+			motionBlurRenderPass.setFreeze( !motionBlurRenderPass.isFreeze() );
+		}
+
+		if( KeyBindingManager.getKeyBindingManager().isValidCommand( "g", false ) ) {
+			motionBlurRenderPass.reloadShader();
+		}
+
+		if( KeyBindingManager.getKeyBindingManager().isValidCommand( "0", false ) ) {
+			motionBlurRenderPass.resetParameters();
+			motionBlurRenderPass.setUseCurrentScene( true );
+		}
+
+		if( KeyBindingManager.getKeyBindingManager().isValidCommand( "shot", false ) ) {
+			display.getRenderer().takeScreenShot( "shot" + screenshotIndex++ );
+		}
+	}
+
+	private void setupKeyBindings() {
+		KeyBindingManager.getKeyBindingManager().set( "1", KeyInput.KEY_1 );
+		KeyBindingManager.getKeyBindingManager().set( "2", KeyInput.KEY_2 );
+		KeyBindingManager.getKeyBindingManager().set( "3", KeyInput.KEY_3 );
+		KeyBindingManager.getKeyBindingManager().set( "0", KeyInput.KEY_0 );
+		KeyBindingManager.getKeyBindingManager().set( "g", KeyInput.KEY_G );
+		KeyBindingManager.getKeyBindingManager().set( "f", KeyInput.KEY_F );
+		KeyBindingManager.getKeyBindingManager().set( "shot", KeyInput.KEY_F4 );
+
+		Text t = new Text( "Text", "1: enable/disable motionblur pass" );
+		t.setRenderQueueMode( Renderer.QUEUE_ORTHO );
+		t.setLightCombineMode( LightState.OFF );
+		t.setLocalTranslation( new Vector3f( 0, 20, 1 ) );
+		fpsNode.attachChild( t );
+
+		t = new Text( "Text", "2/3: decrease/increase blur strength" );
+		t.setRenderQueueMode( Renderer.QUEUE_ORTHO );
+		t.setLightCombineMode( LightState.OFF );
+		t.setLocalTranslation( new Vector3f( 0, 40, 1 ) );
+		fpsNode.attachChild( t );
+
+		t = new Text( "Text", "f: freeze/unfreeze movement" );
+		t.setRenderQueueMode( Renderer.QUEUE_ORTHO );
+		t.setLightCombineMode( LightState.OFF );
+		t.setLocalTranslation( new Vector3f( 0, 60, 1 ) );
+		fpsNode.attachChild( t );
+	}
+
+	private Node createObjects() {
+		Node objects = new Node( "objects" );
+
+		torus = new Torus( "Torus", 50, 50, 10, 25 );
+		torus.setLocalTranslation( new Vector3f( 50, -5, 20 ) );
+		TextureState ts = display.getRenderer().createTextureState();
+		Texture t0 = TextureManager.loadTexture(
+				TestMotionBlur.class.getClassLoader().getResource(
+						"jmetest/data/images/Monkey.jpg" ),
+				Texture.MM_LINEAR_LINEAR,
+				Texture.FM_LINEAR );
+		Texture t1 = TextureManager.loadTexture(
+				TestMotionBlur.class.getClassLoader().getResource(
+						"jmetest/data/texture/north.jpg" ),
+				Texture.MM_LINEAR_LINEAR,
+				Texture.FM_LINEAR );
+		t1.setEnvironmentalMapMode( Texture.EM_SPHERE );
+		ts.setTexture( t0, 0 );
+		ts.setTexture( t1, 1 );
+		ts.setEnabled( true );
+		torus.setRenderState( ts );
+		objects.attachChild( torus );
+
+		ts = display.getRenderer().createTextureState();
+		t0 = TextureManager.loadTexture(
+				TestMotionBlur.class.getClassLoader().getResource(
+						"jmetest/data/texture/wall.jpg" ),
+				Texture.MM_LINEAR_LINEAR,
+				Texture.FM_LINEAR );
+		t0.setWrap( Texture.WM_WRAP_S_WRAP_T );
+		ts.setTexture( t0 );
+
+		ts = display.getRenderer().createTextureState();
+		t0 = TextureManager.loadTexture(
+				TestMotionBlur.class.getClassLoader().getResource(
+						"jmetest/data/texture/cloud_land.jpg" ),
+				Texture.MM_LINEAR_LINEAR,
+				Texture.FM_LINEAR );
+		t0.setWrap( Texture.WM_WRAP_S_WRAP_T );
+		ts.setTexture( t0 );
+
+		sphere = new Sphere( "sphere", 16, 16, 10 );
+		sphere.setLocalTranslation( new Vector3f( 0, -10, 15 ) );
+		sphere.setRenderState( ts );
+		objects.attachChild( sphere );
+
+		Box box = new Box( "floor", new Vector3f( -1000, -10, -1000 ), new Vector3f( 1000, 10, 1000 ) );
+		box.setLocalTranslation( new Vector3f( 0, -100, 0 ) );
+		box.setRenderState( ts );
+		box.setModelBound( new BoundingBox() );
+		box.updateModelBound();
+		objects.attachChild( box );
+
+		ts = display.getRenderer().createTextureState();
+		t0 = TextureManager.loadTexture(
+				TestMotionBlur.class.getClassLoader().getResource(
+						"jmetest/data/texture/wall.jpg" ),
+				Texture.MM_LINEAR_LINEAR,
+				Texture.FM_LINEAR );
+		t0.setWrap( Texture.WM_WRAP_S_WRAP_T );
+		ts.setTexture( t0 );
+
+		box = new Box( "box1", new Vector3f( -10, -10, -10 ), new Vector3f( 10, 10, 10 ) );
+		box.setLocalTranslation( new Vector3f( 0, -20, 0 ) );
+		box.setRenderState( ts );
+		objects.attachChild( box );
+
+		box = new Box( "box2", new Vector3f( -5, -5, -5 ), new Vector3f( 5, 5, 5 ) );
+		box.setLocalTranslation( new Vector3f( 15, 30, 0 ) );
+		box.setRenderState( ts );
+		objects.attachChild( box );
+
+		box = new Box( "box3", new Vector3f( -5, -5, -5 ), new Vector3f( 5, 5, 5 ) );
+		box.setLocalTranslation( new Vector3f( 0, -10, 45 ) );
+		box.setRenderState( ts );
+		objects.attachChild( box );
+
+		return objects;
+	}
+
+	private void createDebugQuads() {
+		debugQuadsNode = new Node( "quadNode" );
+		debugQuadsNode.setCullMode( SceneElement.CULL_NEVER );
+
+		float quadWidth = display.getWidth() / 8;
+		float quadHeight = display.getWidth() / 8;
+		Quad debugQuad = new Quad( "reflectionQuad", quadWidth, quadHeight );
+		debugQuad.setRenderQueueMode( Renderer.QUEUE_ORTHO );
+		debugQuad.setCullMode( SceneElement.CULL_NEVER );
+		debugQuad.setLightCombineMode( LightState.OFF );
+		TextureState ts = display.getRenderer().createTextureState();
+		ts.setTexture( motionBlurRenderPass.getMainTexture() );
+		debugQuad.setRenderState( ts );
+		debugQuad.updateRenderState();
+		debugQuad.getLocalTranslation().set( quadWidth * 0.6f, quadHeight * 1.0f, 1.0f );
+		debugQuadsNode.attachChild( debugQuad );
+	}
+
+}
