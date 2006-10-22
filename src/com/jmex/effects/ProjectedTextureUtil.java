62857be07f868259cd9b2d03cf84bbc7b6ab6cd3
rherlitz <rherlitz@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2006-10-22 09:47:45 +0000

    moved some texture projection stuff into a new class ProjectedTextureUtil updated testprojectedtexture with using projectedtextureutil and added another projector
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@3269 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/com/jmex/effects/ProjectedTextureUtil.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2003-2006 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jmex.effects;
+
+import com.jme.math.Matrix4f;
+import com.jme.math.Vector3f;
+import com.jme.math.FastMath;
+import com.jme.image.Texture;
+
+import java.nio.FloatBuffer;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.glu.GLU;
+
+/**
+ * <code>ProjectedTextureUtil</code>
+ *
+ * @author Rikard Herlitz (MrCoder)
+ */
+public class ProjectedTextureUtil {
+	private static Matrix4f lightProjectionMatrix = new Matrix4f();
+	private static Matrix4f lightViewMatrix = new Matrix4f();
+	private static Matrix4f biasMatrix = new Matrix4f(
+			0.5f, 0.0f, 0.0f, 0.0f,
+			0.0f, 0.5f, 0.0f, 0.0f,
+			0.0f, 0.0f, 0.5f, 0.0f,
+			0.5f, 0.5f, 0.5f, 1.0f
+	); //bias from [-1, 1] to [0, 1]
+
+	/**
+	 * Sets the provided texture up for projection use
+	 * @param texture Texture to use for projection
+	 * @param wrapMode Wrapping mode, any of Texture.WM_XXX
+	 * @param combineMode Combine mode, any of Texture.ACF_XXX
+	 */
+	public static void setupProjectedTexture( Texture texture, int wrapMode, int combineMode ) {
+		texture.setMatrix( new Matrix4f() );
+		texture.setEnvironmentalMapMode( Texture.EM_EYE_LINEAR );
+		texture.setWrap( wrapMode);
+
+		texture.setApply( Texture.AM_COMBINE );
+		texture.setCombineFuncRGB( combineMode );
+		texture.setCombineSrc0RGB( Texture.ACS_TEXTURE );
+		texture.setCombineOp0RGB( Texture.ACO_SRC_COLOR );
+		texture.setCombineSrc1RGB( Texture.ACS_PREVIOUS );
+		texture.setCombineOp1RGB( Texture.ACO_SRC_COLOR );
+		texture.setCombineScaleRGB( 1.0f );
+	}
+
+	/**
+	 * Updated texture matrix on the provided texture
+	 * @param texture Texture to update texturematrix on
+	 * @param fov Projector field of view, in angles
+	 * @param aspect Projector frustum aspect ratio
+	 * @param near Projector frustum near plane
+	 * @param far Projector frustum far plane
+	 * @param pos Projector position
+	 * @param aim Projector look at position
+	 */
+	public static void updateProjectedTexture( Texture texture, float fov, float aspect, float near, float far, Vector3f pos, Vector3f aim ) {
+		matrixPerspective( fov, aspect, near, far, lightProjectionMatrix );
+		matrixLookAt( pos, aim, lightViewMatrix );
+		texture.getMatrix().set( lightViewMatrix.multLocal( lightProjectionMatrix ).multLocal( biasMatrix ) );
+	}
+
+	//UTILS
+	private static final FloatBuffer tmp_FloatBuffer = org.lwjgl.BufferUtils.createFloatBuffer( 16 );
+	private static Vector3f localDir = new Vector3f();
+	private static Vector3f localLeft = new Vector3f();
+	private static Vector3f localUp = new Vector3f();
+	private static Vector3f tmpVec = new Vector3f();
+
+	public static void matrixLookAt( Vector3f location, Vector3f at, Matrix4f result ) {
+		localDir.set( at ).subtractLocal( location ).normalizeLocal();
+		localDir.cross( Vector3f.UNIT_Y, localLeft );
+		localLeft.cross( localDir, localUp );
+
+		GL11.glPushMatrix();
+
+		// set view matrix
+		GL11.glMatrixMode( GL11.GL_MODELVIEW );
+		GL11.glLoadIdentity();
+		GLU.gluLookAt(
+				location.x,
+				location.y,
+				location.z,
+				at.x,
+				at.y,
+				at.z,
+				localUp.x,
+				localUp.y,
+				localUp.z );
+
+		if( result != null ) {
+			tmp_FloatBuffer.rewind();
+			GL11.glGetFloat( GL11.GL_MODELVIEW_MATRIX, tmp_FloatBuffer );
+			tmp_FloatBuffer.rewind();
+			result.readFloatBuffer( tmp_FloatBuffer );
+		}
+
+		GL11.glPopMatrix();
+	}
+
+	public static void matrixPerspective( float fovY, float aspect, float near, float far, Matrix4f result ) {
+		GL11.glPushMatrix();
+
+		// set view matrix
+		GL11.glMatrixMode( GL11.GL_MODELVIEW );
+		GL11.glLoadIdentity();
+		GLU.gluPerspective( fovY, aspect, near, far );
+
+		if( result != null ) {
+			tmp_FloatBuffer.rewind();
+			GL11.glGetFloat( GL11.GL_MODELVIEW_MATRIX, tmp_FloatBuffer );
+			tmp_FloatBuffer.rewind();
+			result.readFloatBuffer( tmp_FloatBuffer );
+		}
+
+		GL11.glPopMatrix();
+	}
+
+	public static void matrixProjection( float fovY, float aspect, float near, float far, Matrix4f result ) {
+		float h = FastMath.tan( fovY * FastMath.DEG_TO_RAD ) * near * .5f;
+		float w = h * aspect;
+		float frustumLeft = -w;
+		float frustumRight = w;
+		float frustumBottom = -h;
+		float frustumTop = h;
+		float frustumNear = near;
+		float frustumFar = far;
+
+		GL11.glPushMatrix();
+
+		GL11.glMatrixMode( GL11.GL_PROJECTION );
+		GL11.glLoadIdentity();
+		GL11.glFrustum(
+				frustumLeft,
+				frustumRight,
+				frustumBottom,
+				frustumTop,
+				frustumNear,
+				frustumFar );
+
+		if( result != null ) {
+			tmp_FloatBuffer.rewind();
+			GL11.glGetFloat( GL11.GL_PROJECTION_MATRIX, tmp_FloatBuffer );
+			tmp_FloatBuffer.rewind();
+			result.readFloatBuffer( tmp_FloatBuffer );
+		}
+
+		GL11.glPopMatrix();
+	}
+}
--- src/jmetest/effects/TestProjectedTexture.java
+++ src/jmetest/effects/TestProjectedTexture.java
@@ -36,7 +36,6 @@ import com.jme.app.SimpleGame;
 import com.jme.bounding.BoundingBox;
 import com.jme.image.Texture;
 import com.jme.math.FastMath;
-import com.jme.math.Matrix4f;
 import com.jme.math.Vector3f;
 import com.jme.scene.Node;
 import com.jme.scene.shape.Box;
@@ -45,19 +44,17 @@ import com.jme.scene.state.TextureState;
 import com.jme.util.TextureKey;
 import com.jme.util.TextureManager;
 import com.jme.util.export.binary.BinaryImporter;
+import com.jmex.effects.ProjectedTextureUtil;
 import com.jmex.model.XMLparser.Converters.MilkToJme;
 import com.jmex.terrain.TerrainPage;
 import com.jmex.terrain.util.ImageBasedHeightMap;
 import com.jmex.terrain.util.ProceduralSplatTextureGenerator;
 import jmetest.renderer.loader.TestMilkJmeWrite;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.glu.GLU;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.FloatBuffer;
 
 /**
  * <code>TestProjectedTexture</code>
@@ -65,43 +62,48 @@ import java.nio.FloatBuffer;
  * @author Rikard Herlitz (MrCoder)
  */
 public class TestProjectedTexture extends SimpleGame {
-	TerrainPage terrain;
-	Texture projectedTexture;
+	private TerrainPage terrain;
 
-	Node projectorModel;
-	Vector3f projectorAim = new Vector3f();
+	private Texture projectedTexture1;
+	private Node projectorModel1;
+	private Vector3f projectorAim1 = new Vector3f();
 
-	Matrix4f textureMatrix = new Matrix4f();
-	Matrix4f lightProjectionMatrix = new Matrix4f();
-	Matrix4f lightViewMatrix = new Matrix4f();
-	Matrix4f biasMatrix = new Matrix4f(
-			0.5f, 0.0f, 0.0f, 0.0f,
-			0.0f, 0.5f, 0.0f, 0.0f,
-			0.0f, 0.0f, 0.5f, 0.0f,
-			0.5f, 0.5f, 0.5f, 1.0f
-	); //bias from [-1, 1] to [0, 1]
+	private Texture projectedTexture2;
+	private Box projectorModel2;
+	private Vector3f projectorAim2 = new Vector3f();
 
 	public static void main( String[] args ) {
 		TestProjectedTexture app = new TestProjectedTexture();
-		app.setDialogBehaviour( FIRSTRUN_OR_NOCONFIGFILE_SHOW_PROPS_DIALOG );
+		app.setDialogBehaviour( ALWAYS_SHOW_PROPS_DIALOG );
 		app.start();
 	}
 
 	protected void simpleUpdate() {
 		//make a funny projector animation
-		projectorModel.getLocalTranslation().set( FastMath.sin( timer.getTimeInSeconds() ) * (FastMath.sin( timer.getTimeInSeconds() * 0.7f ) * 15.0f + 20.0f),
+		projectorModel1.getLocalTranslation().set( FastMath.sin( timer.getTimeInSeconds() ) * (FastMath.sin( timer.getTimeInSeconds() * 0.7f ) * 15.0f + 20.0f),
 												  FastMath.sin( timer.getTimeInSeconds() * 0.5f ) * 10.0f + 20.0f,
 												  FastMath.cos( timer.getTimeInSeconds() * 1.2f ) * (FastMath.sin( timer.getTimeInSeconds() ) * 15.0f + 20.0f) );
-		projectorAim.set( FastMath.sin( timer.getTimeInSeconds() * 0.8f - FastMath.PI ) * (FastMath.sin( timer.getTimeInSeconds() * 0.5f - FastMath.PI ) * 10.0f + 10.0f),
+		projectorAim1.set( FastMath.sin( timer.getTimeInSeconds() * 0.8f - FastMath.PI ) * (FastMath.sin( timer.getTimeInSeconds() * 0.5f - FastMath.PI ) * 10.0f + 10.0f),
 						  0.0f,
 						  FastMath.cos( timer.getTimeInSeconds() * 0.6f - FastMath.PI ) * (FastMath.sin( timer.getTimeInSeconds() * 0.3f - FastMath.PI ) * 10.0f + 10.0f) );
-		projectorModel.lookAt( projectorAim, Vector3f.UNIT_Y );
-		projectorModel.updateGeometricState( 0.0f, true );
+		projectorModel1.lookAt( projectorAim1, Vector3f.UNIT_Y );
+		projectorModel1.updateGeometricState( 0.0f, true );
 
-		//create texture matrix
-		matrixPerspective( 30.0f, 1.0f, 1.0f, 1000.0f, lightProjectionMatrix );
-		matrixLookAt( projectorModel.getLocalTranslation(), projectorAim, lightViewMatrix );
-		textureMatrix.set( lightViewMatrix.mult( lightProjectionMatrix ).multLocal( biasMatrix ) );
+		//update texture matrix
+		ProjectedTextureUtil.updateProjectedTexture( projectedTexture1, 30.0f, 1.5f, 1.0f, 1000.0f, projectorModel1.getLocalTranslation(), projectorAim1 );
+
+		//make a second funny projector animation
+		projectorModel2.getLocalTranslation().set( FastMath.sin( timer.getTimeInSeconds() - FastMath.PI ) * (FastMath.sin( timer.getTimeInSeconds() * 1.1f ) * 15.0f + 20.0f),
+												  FastMath.sin( timer.getTimeInSeconds() * 0.7f - FastMath.PI ) * 10.0f + 30.0f,
+												  FastMath.cos( timer.getTimeInSeconds() * 0.4f - FastMath.PI ) * (FastMath.sin( timer.getTimeInSeconds() ) * 15.0f + 20.0f) );
+		projectorAim2.set( FastMath.sin( timer.getTimeInSeconds() * 0.4f ) * (FastMath.sin( timer.getTimeInSeconds() * 0.7f ) * 10.0f + 10.0f),
+						  0.0f,
+						  FastMath.cos( timer.getTimeInSeconds() * 0.3f ) * (FastMath.sin( timer.getTimeInSeconds() * 0.8f ) * 10.0f + 10.0f) );
+		projectorModel2.lookAt( projectorAim2, Vector3f.UNIT_Y );
+		projectorModel2.updateGeometricState( 0.0f, true );
+
+		//update texture matrix
+		ProjectedTextureUtil.updateProjectedTexture( projectedTexture2, 20.0f, 1.0f, 1.0f, 1000.0f, projectorModel2.getLocalTranslation(), projectorAim2 );
 	}
 
 	protected void simpleInitGame() {
@@ -130,14 +132,28 @@ public class TestProjectedTexture extends SimpleGame {
 			}
 			URL TEXdir2 = TestMilkJmeWrite.class.getClassLoader().getResource(
 					"jmetest/data/model/msascii/" );
-			projectorModel = null;
+			projectorModel1 = null;
 			try {
 				TextureKey.setOverridingLocation( TEXdir2 );
-				projectorModel = (Node) BinaryImporter.getInstance().load( new ByteArrayInputStream( BO2.toByteArray() ) );
+				projectorModel1 = (Node) BinaryImporter.getInstance().load( new ByteArrayInputStream( BO2.toByteArray() ) );
 			} catch( IOException e ) {
 				System.out.println( "darn exceptions:" + e.getMessage() );
 			}
-			rootNode.attachChild( projectorModel );
+			rootNode.attachChild( projectorModel1 );
+
+			projectorModel2 = new Box( "Projector2", new Vector3f(), 1, 1, 2 );
+			projectorModel2.setModelBound( new BoundingBox() );
+			projectorModel2.updateModelBound();
+			TextureState ts = display.getRenderer().createTextureState();
+			Texture t0 = TextureManager.loadTexture(
+					TestProjectedTexture.class.getClassLoader().getResource(
+							"jmetest/data/texture/Detail.jpg" ),
+					Texture.MM_LINEAR_LINEAR,
+					Texture.FM_LINEAR );
+			t0.setWrap( Texture.WM_WRAP_S_WRAP_T );
+			ts.setTexture( t0, 0 );
+			projectorModel2.setRenderState( ts );
+			rootNode.attachChild( projectorModel2 );
 
 			//create terrain
 			URL grayScale = TestProjectedTexture.class.getClassLoader().getResource( "jmetest/data/texture/terrain.png" );
@@ -164,21 +180,11 @@ public class TestProjectedTexture extends SimpleGame {
 					"jmetest/data/texture/water.png" ) ) );
 			pst.createTexture( 512 );
 
-			TextureState ts = display.getRenderer().createTextureState();
+			ts = display.getRenderer().createTextureState();
 			ts.setEnabled( true );
 			Texture t1 = TextureManager.loadTexture( pst.getImageIcon().getImage(), Texture.MM_LINEAR_LINEAR, Texture.FM_LINEAR, true );
 			ts.setTexture( t1, 0 );
 
-			projectedTexture = TextureManager.loadTexture( TestProjectedTexture.class.getClassLoader().getResource(
-					"jmetest/data/images/Monkey.png" ), Texture.MM_LINEAR_LINEAR, Texture.FM_LINEAR );
-			ts.setTexture( projectedTexture, 1 );
-
-			//this is were we set the texture up for projection
-			projectedTexture.setMatrix( textureMatrix );
-			projectedTexture.setEnvironmentalMapMode( Texture.EM_EYE_LINEAR );
-			projectedTexture.setWrap( Texture.WM_BCLAMP_S_BCLAMP_T );
-
-			//make the projected texture blend with the terrain texture
 			t1.setApply( Texture.AM_COMBINE );
 			t1.setCombineFuncRGB( Texture.ACF_MODULATE );
 			t1.setCombineSrc0RGB( Texture.ACS_TEXTURE );
@@ -187,13 +193,21 @@ public class TestProjectedTexture extends SimpleGame {
 			t1.setCombineOp1RGB( Texture.ACO_SRC_COLOR );
 			t1.setCombineScaleRGB( 1.0f );
 
-			projectedTexture.setApply( Texture.AM_COMBINE );
-			projectedTexture.setCombineFuncRGB( Texture.ACF_ADD );
-			projectedTexture.setCombineSrc0RGB( Texture.ACS_TEXTURE );
-			projectedTexture.setCombineOp0RGB( Texture.ACO_SRC_COLOR );
-			projectedTexture.setCombineSrc1RGB( Texture.ACS_PREVIOUS );
-			projectedTexture.setCombineOp1RGB( Texture.ACO_SRC_COLOR );
-			projectedTexture.setCombineScaleRGB( 1.0f );
+			//create a texture to use for projection
+			projectedTexture1 = TextureManager.loadTexture( TestProjectedTexture.class.getClassLoader().getResource(
+					"jmetest/data/images/Monkey.png" ), Texture.MM_LINEAR_LINEAR, Texture.FM_LINEAR );
+			ts.setTexture( projectedTexture1, 1 );
+
+			//this is were we set the texture up for projection
+			ProjectedTextureUtil.setupProjectedTexture( projectedTexture1, Texture.WM_BCLAMP_S_BCLAMP_T, Texture.ACF_ADD );
+
+			//create another texture to use for projection
+			projectedTexture2 = TextureManager.loadTexture( TestProjectedTexture.class.getClassLoader().getResource(
+					"jmetest/data/texture/halo.jpg" ), Texture.MM_LINEAR_LINEAR, Texture.FM_LINEAR );
+			ts.setTexture( projectedTexture2, 2 );
+
+			//this is were we set the texture up for projection
+			ProjectedTextureUtil.setupProjectedTexture( projectedTexture2, Texture.WM_BCLAMP_S_BCLAMP_T, Texture.ACF_ADD );
 
 			terrain.setRenderState( ts );
 
@@ -201,109 +215,24 @@ public class TestProjectedTexture extends SimpleGame {
 			dummyBox.setModelBound( new BoundingBox() );
 			dummyBox.updateModelBound();
 			ts = display.getRenderer().createTextureState();
-			Texture t0 = TextureManager.loadTexture(
+			t0 = TextureManager.loadTexture(
 					TestProjectedTexture.class.getClassLoader().getResource(
 							"jmetest/data/texture/wall.jpg" ),
 					Texture.MM_LINEAR_LINEAR,
 					Texture.FM_LINEAR );
 			t0.setWrap( Texture.WM_WRAP_S_WRAP_T );
 			ts.setTexture( t0, 0 );
-			ts.setTexture( projectedTexture, 1 );
+			ts.setTexture( projectedTexture1, 1 );
+			ts.setTexture( projectedTexture2, 2 );
 			dummyBox.setRenderState( ts );
 			rootNode.attachChild( dummyBox );
+
+			terrain.lock();
 
 			rootNode.setRenderQueueMode( com.jme.renderer.Renderer.QUEUE_OPAQUE );
 			fpsNode.setRenderQueueMode( com.jme.renderer.Renderer.QUEUE_OPAQUE );
 		} catch( Exception e ) {
 			e.printStackTrace();
 		}
-	}
-
-	//UTILS
-	private static final FloatBuffer tmp_FloatBuffer = org.lwjgl.BufferUtils.createFloatBuffer( 16 );
-	private Vector3f localDir = new Vector3f();
-	private Vector3f localLeft = new Vector3f();
-	private Vector3f localUp = new Vector3f();
-	private Vector3f tmpVec = new Vector3f();
-
-	private void matrixLookAt( Vector3f location, Vector3f at, Matrix4f result ) {
-		localDir.set( at ).subtractLocal( location ).normalizeLocal();
-		localDir.cross( Vector3f.UNIT_Y, localLeft );
-		localLeft.cross( localDir, localUp );
-
-		GL11.glPushMatrix();
-
-		// set view matrix
-		GL11.glMatrixMode( GL11.GL_MODELVIEW );
-		GL11.glLoadIdentity();
-		GLU.gluLookAt(
-				location.x,
-				location.y,
-				location.z,
-				at.x,
-				at.y,
-				at.z,
-				localUp.x,
-				localUp.y,
-				localUp.z );
-
-		if( result != null ) {
-			tmp_FloatBuffer.rewind();
-			GL11.glGetFloat( GL11.GL_MODELVIEW_MATRIX, tmp_FloatBuffer );
-			tmp_FloatBuffer.rewind();
-			result.readFloatBuffer( tmp_FloatBuffer );
-		}
-
-		GL11.glPopMatrix();
-	}
-
-	private void matrixPerspective( float fovY, float aspect, float near, float far, Matrix4f result ) {
-		GL11.glPushMatrix();
-
-		// set view matrix
-		GL11.glMatrixMode( GL11.GL_MODELVIEW );
-		GL11.glLoadIdentity();
-		GLU.gluPerspective( fovY, aspect, near, far );
-
-		if( result != null ) {
-			tmp_FloatBuffer.rewind();
-			GL11.glGetFloat( GL11.GL_MODELVIEW_MATRIX, tmp_FloatBuffer );
-			tmp_FloatBuffer.rewind();
-			result.readFloatBuffer( tmp_FloatBuffer );
-		}
-
-		GL11.glPopMatrix();
-	}
-
-	private void matrixProjection( float fovY, float aspect, float near, float far, Matrix4f result ) {
-		float h = FastMath.tan( fovY * FastMath.DEG_TO_RAD ) * near * .5f;
-		float w = h * aspect;
-		float frustumLeft = -w;
-		float frustumRight = w;
-		float frustumBottom = -h;
-		float frustumTop = h;
-		float frustumNear = near;
-		float frustumFar = far;
-
-		GL11.glPushMatrix();
-
-		GL11.glMatrixMode( GL11.GL_PROJECTION );
-		GL11.glLoadIdentity();
-		GL11.glFrustum(
-				frustumLeft,
-				frustumRight,
-				frustumBottom,
-				frustumTop,
-				frustumNear,
-				frustumFar );
-
-		if( result != null ) {
-			tmp_FloatBuffer.rewind();
-			GL11.glGetFloat( GL11.GL_PROJECTION_MATRIX, tmp_FloatBuffer );
-			tmp_FloatBuffer.rewind();
-			result.readFloatBuffer( tmp_FloatBuffer );
-		}
-
-		GL11.glPopMatrix();
 	}
 }
