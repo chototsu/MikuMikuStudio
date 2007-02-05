503c02b0f45af4c339141f7ca6adc1af5b3156ac
nca <nca@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2007-02-05 16:49:42 +0000

    ISSUE MINOR:
    License header added
    new collision system supported.
    
    
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@3406 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jmex/effects/particles/ParticleMesh.java
+++ src/com/jmex/effects/particles/ParticleMesh.java
@@ -1,8 +1,41 @@
+/*
+ * Copyright (c) 2003-2007 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.jmex.effects.particles;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+import com.jme.bounding.CollisionTree;
+import com.jme.bounding.CollisionTreeManager;
 import com.jme.intersection.CollisionResults;
 import com.jme.math.Vector2f;
 import com.jme.math.Vector3f;
@@ -28,7 +61,7 @@ import com.jme.util.geom.BufferUtils;
  * geometric data.
  * 
  * @author Joshua Slack
- * @version $Id: ParticleMesh.java,v 1.10 2006-11-16 19:44:12 nca Exp $
+ * @version $Id: ParticleMesh.java,v 1.11 2007-02-05 16:49:42 nca Exp $
  */
 public class ParticleMesh extends ParticleGeometry {
 
@@ -305,16 +338,18 @@ public class ParticleMesh extends ParticleGeometry {
      * @return True if they intersect.
      */
     public boolean hasTriangleCollision(TriMesh toCheck, int thisBatch, int checkBatch) {
-        if (getBatch(thisBatch).getCollisionTree() == null
-                || toCheck.getBatch(checkBatch).getCollisionTree() == null
+    	CollisionTree thisCT = CollisionTreeManager.getInstance().getCollisionTree(getBatch(thisBatch));
+    	CollisionTree toCheckCT = CollisionTreeManager.getInstance().getCollisionTree(toCheck.getBatch(checkBatch));
+    	
+        if (thisCT == null
+                || toCheckCT == null
                 || !isCollidable || !toCheck.isCollidable())
             return false;
         
-        getBatch(thisBatch).getCollisionTree().bounds.transform(
+        thisCT.getBounds().transform(
                 worldRotation, worldTranslation, worldScale,
-                getBatch(thisBatch).getCollisionTree().worldBounds);
-        return getBatch(thisBatch).getCollisionTree().intersect(
-                toCheck.getBatch(checkBatch).getCollisionTree());        
+                thisCT.getWorldBounds());
+        return thisCT.intersect(toCheckCT);        
     }
 
     public void write(JMEExporter e) throws IOException {
