7fe90fdf96422014adc1f207ce953fefb87cb2b3
nca <nca@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2007-08-02 23:24:07 +0000

    Renamed to proper naming for jME.  logging cleanup
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@3600 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jmex/model/collada/ColladaToJme.java
+++ src/com/jmex/model/collada/ColladaToJme.java
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 2003-2007 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package com.jmex.model.collada;
 
 import java.io.File;
@@ -6,20 +38,24 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.jme.app.SimpleHeadlessApp;
 import com.jme.scene.Spatial;
 import com.jme.util.export.binary.BinaryExporter;
 
-public class ColladaToFate extends SimpleHeadlessApp {
+public class ColladaToJme extends SimpleHeadlessApp {
+    private static final Logger logger = Logger.getLogger(ColladaToJme.class
+            .getName());
+    
    Spatial collada;
    static String in;
    static String texDir;
    static String outDir;
     public static void main(String[] args) {
         if (args.length < 3 || args.length > 3) {
-            System.out
-                    .println("USAGE: ColladaToFate <COLLADA File> <Texture Directory> <Fate File>");
+            logger.info("USAGE: ColladaToJme <COLLADA File> <Texture Directory> <Jme File>");
             System.exit(1);
         }
         in = args[0];
@@ -33,8 +69,8 @@ public class ColladaToFate extends SimpleHeadlessApp {
             out.mkdir();
         }
         
-        ColladaToFate ctf = new ColladaToFate();
-        ctf.start();
+        ColladaToJme ctj = new ColladaToJme();
+        ctj.start();
 
     }
 
@@ -45,7 +81,7 @@ public class ColladaToFate extends SimpleHeadlessApp {
         this.finished = true;
         long end = System.nanoTime();
         
-        System.out.println("Conversion took: " + ((end-start)/1000000000) + " seconds.");
+        logger.info("Conversion took: " + ((end-start)/1000000000) + " seconds.");
     }
     
     protected void writeFile(String inputFile, String texdir) {
@@ -54,10 +90,10 @@ public class ColladaToFate extends SimpleHeadlessApp {
             if(!inputFile.endsWith("/")) {
                 inputFile += "/";
             }
-            System.out.println(inputFile + " is a Directory, getting subfiles: ");
+            logger.info(inputFile + " is a Directory, getting subfiles: ");
             String[] files = inFile.list();
             for(int i = 0; i < files.length; i++) {
-                System.out.println("Sending: " + (inputFile+files[i]));
+                logger.info("Sending: " + (inputFile+files[i]));
                 writeFile(inputFile+files[i], texdir);
             }
             
@@ -68,9 +104,9 @@ public class ColladaToFate extends SimpleHeadlessApp {
             collada = null;
             System.gc();
             
-            System.out.println("Found Collada file, converting: " + inputFile);
-            String out = outDir + inFile.getName().substring( 0, inFile.getName().toUpperCase().indexOf(".DAE") ) + ".fate";
-            System.out.println("Storing as: " + out);
+            logger.info("Found Collada file, converting: " + inputFile);
+            String out = outDir + inFile.getName().substring( 0, inFile.getName().toUpperCase().indexOf(".DAE") ) + ".jme";
+            logger.info("Storing as: " + out);
             URL url = null;
             String modelName = inFile.getName().substring(0,
                     inFile.getName().indexOf("."));
@@ -78,17 +114,16 @@ public class ColladaToFate extends SimpleHeadlessApp {
                 url = new File(texDir).toURI().toURL();
             } catch (MalformedURLException e2) {
                 // TODO Auto-generated catch block
-                e2.printStackTrace();
+                logger.log(Level.WARNING, "Error creating File", e2);
             }
             FileInputStream input = null;
             try {
                 input = new FileInputStream(inFile);
             } catch (FileNotFoundException e1) {
-                e1.printStackTrace();
+                logger.log(Level.WARNING, "Error creating FileInputStream", e1);
             }
             if (input == null) {
-                System.out
-                        .println("Unable to find file");
+                logger.info("Unable to find file");
                 System.exit(0);
             }
             
@@ -98,7 +133,7 @@ public class ColladaToFate extends SimpleHeadlessApp {
                 collada = ColladaImporter.getModel();
                 ColladaImporter.cleanUp();
             } catch (Exception e) {
-                e.printStackTrace( );
+                logger.log(Level.SEVERE, "Error loading Collada file", e);
             }
             
             collada.updateGeometricState(0, true);
@@ -111,7 +146,7 @@ public class ColladaToFate extends SimpleHeadlessApp {
                 }
                 BinaryExporter.getInstance().save(collada, f);
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.log(Level.SEVERE, "Error saving Collada file", e);
             }
         }
     }
