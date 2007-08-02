907861164e1889824f4c59078e8e3e104406969d
nca <nca@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2007-08-02 23:08:03 +0000

    license year update
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@3596 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jmex/model/Face.java
+++ src/com/jmex/model/Face.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003-2006 jMonkeyEngine
+ * Copyright (c) 2003-2007 jMonkeyEngine
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -35,7 +35,7 @@ package com.jmex.model;
 /**
  * <code>Face</code>
  * @author Mark Powell
- * @version $Id: Face.java,v 1.3 2006-01-13 19:39:42 renanse Exp $
+ * @version $Id: Face.java,v 1.4 2007-08-02 23:08:03 nca Exp $
  */
 public class Face {
 	public int[] vertIndex = new int[3];
