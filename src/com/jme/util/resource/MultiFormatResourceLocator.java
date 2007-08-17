65e0d37c45edea6796f025ba6db59191ae10969e
nca <nca@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2007-08-17 20:46:06 +0000

    New resource locator utility classes.  Enables multiple location lookup of various resources throughout the code.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@3660 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/com/jme/util/resource/MultiFormatResourceLocator.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2003-2007 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jme.util.resource;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class MultiFormatResourceLocator extends SimpleResourceLocator {
+
+    private String[] extensions;
+    private boolean trySpecifiedFormatFirst = false;
+
+    public MultiFormatResourceLocator(URI baseDir, String ... extensions) {
+        super(baseDir);
+        
+        if (extensions == null) {
+            throw new NullPointerException("extensions can not be null.");
+        }
+        this.extensions = extensions;
+    }
+
+    public MultiFormatResourceLocator(URL baseDir, String ... extensions) throws URISyntaxException {
+        this(baseDir.toURI(), extensions);
+    }
+    
+    @Override
+    public URL locateResource(String resourceName) {
+        if (trySpecifiedFormatFirst) {
+            URL u = super.locateResource(resourceName);
+            if (u != null) {
+                return u;
+            }
+        }
+        
+        String baseFileName = getBaseFileName(resourceName);
+        for (int i = 0; i < extensions.length; i++) {
+            URL u = super.locateResource(baseFileName+extensions[i]);
+            if (u != null) {
+                return u;
+            }
+        }
+        
+        if (!trySpecifiedFormatFirst) {
+            // If all else fails, just try the original name.
+            return super.locateResource(resourceName);
+        } else {
+            return null;
+        }
+    }
+
+    private String getBaseFileName(String resourceName) {
+        File f = new File(resourceName);
+        String name = f.getName();
+        int dot = name.lastIndexOf('.');
+        if (dot < 0) {
+            return resourceName;
+        } else {
+            return resourceName.substring(0, dot);
+        }
+    }
+
+    public boolean isTrySpecifiedFormatFirst() {
+        return trySpecifiedFormatFirst;
+    }
+
+    public void setTrySpecifiedFormatFirst(boolean trySpecifiedFormatFirst) {
+        this.trySpecifiedFormatFirst = trySpecifiedFormatFirst;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof MultiFormatResourceLocator) {
+            return baseDir.equals(((MultiFormatResourceLocator)obj).baseDir) && extensions.equals(((MultiFormatResourceLocator)obj).extensions);
+        }
+        return false;
+    }
+}
---
+++ src/com/jme/util/resource/SimpleResourceLocator.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2003-2007 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.jme.util.resource;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class SimpleResourceLocator implements ResourceLocator {
+
+    protected URI baseDir;
+
+    public SimpleResourceLocator(URI baseDir) {
+        if (baseDir == null) {
+            throw new NullPointerException("baseDir can not be null.");
+        }
+        this.baseDir = baseDir;
+    }
+
+    public SimpleResourceLocator(URL baseDir) throws URISyntaxException {
+        if (baseDir == null) {
+            throw new NullPointerException("baseDir can not be null.");
+        }
+        this.baseDir = baseDir.toURI();
+    }
+    
+    public URL locateResource(String resourceName) {
+        // Try to locate using resourceName as is.
+        try {
+            // Fix spaces to make a valid URI
+            resourceName = resourceName.replaceAll(" ", "\\%20");
+            URI fullURI = baseDir.resolve(resourceName).normalize();
+            URL rVal = fullURI.toURL(); 
+            // XXX: open a connection to see if this is a valid resource.
+            // XXX: Perhaps this is wasteful?  Also, what info will determine validity?
+            if (rVal.openConnection().getContentLength() > 0) {
+                return rVal;
+            }
+        } catch (IOException e) {
+            ; // URL wasn't valid in some way, so try up a path.
+        } catch (IllegalArgumentException e) {
+            ; // URL wasn't valid in some way, so try up a path.
+        }
+    
+        String oldname = resourceName; 
+        resourceName = trimResourceName(resourceName);
+        if (resourceName == null || resourceName.equals(oldname)) {
+            return null;
+        } else {
+            return locateResource(resourceName);
+        }
+    }
+
+    protected String trimResourceName(String resourceName) {
+        File f = new File(resourceName);
+        if (f.getParentFile() != null) {
+            File grandpa = f.getParentFile().getParentFile();
+            if (grandpa != null) {
+                return grandpa.toString().concat("/").concat(f.getName());
+            } else {
+                return f.getName();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof SimpleResourceLocator) {
+            return baseDir.equals(((SimpleResourceLocator)obj).baseDir);
+        }
+        return false;
+    }
+}
