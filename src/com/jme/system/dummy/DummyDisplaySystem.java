739101eacdc85b0d308e55fd9dcaec688c4905df
renanse <renanse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2008-07-29 22:11:19 +0000

    Errant check in, causing tests not to function.
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@3954 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/system/dummy/DummyDisplaySystem.java
+++ src/com/jme/system/dummy/DummyDisplaySystem.java
@@ -59,7 +59,6 @@ public class DummyDisplaySystem extends DisplaySystem {
     private DummyRenderer renderer;
 
     public DummyDisplaySystem() {
-        setSystemProvider(new DummySystemProvider(this));
         created = true;
         renderer = new DummyRenderer();
     }
