4f400bc0209dc4343fc5267d57dfc0fab35bfccc
blaine.dev <blaine.dev@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2009-05-01 23:10:50 +0000

    Ogre DotScene example works, but I want to make it animate before announcing
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@4319 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

---
+++ src/jmetest/ogrexml/TestDotScene.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2003-2009 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package jmetest.ogrexml;
+
+import com.jmex.model.ModelFormatException;
+import com.jme.app.SimpleGame;
+import com.jme.math.Vector3f;
+import com.jme.system.DisplaySystem;
+import com.jme.util.resource.ResourceLocatorTool;
+import com.jme.util.resource.ClasspathResourceLocator;
+import com.jmex.model.ogrexml.SceneLoader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Shows how to load an Ogre dotScene file.
+ *
+ * The supplied model has 20 skeletal animations, and we cycle through them
+ * for your visual entertainment.
+ *
+ * @author Blaine Simpson (blaine dot simpson at admc dot com)
+ * @since jME 2.0
+ * @version $Revision$, $Date$
+ */
+public class TestDotScene extends SimpleGame {
+    private static final Logger logger = Logger.getLogger(
+            TestDotScene.class.getName());
+
+    public static void main(String[] args){
+        TestDotScene app = new TestDotScene();
+        app.setConfigShowMode(ConfigShowMode.AlwaysShow);
+        app.start();
+    }
+
+    @Override
+    protected void simpleInitGame() {
+        ResourceLocatorTool.addResourceLocator(
+                ResourceLocatorTool.TYPE_TEXTURE,
+                new ClasspathResourceLocator());
+        /* If you keep all of the scene resources in a single directory,
+         * the SceneLoader class will find everything else automatically.
+         * That's how we have the resources located for this example. */
+
+        DisplaySystem.getDisplaySystem().setTitle(
+                getClass().getName().replaceFirst(".*\\.", ""));
+
+        cam.setLocation(new Vector3f(0f, 1f, 3f));
+          // Move the camera in closer
+
+        SceneLoader ogreSceneLoader = null;
+        String ninjaSceneString = "/jmetest/data/model/ogrexml/ninja-scene.xml";
+        try {
+            URL sceneUrl = ResourceLocatorTool.locateResource(
+                    ResourceLocatorTool.TYPE_MODEL, ninjaSceneString);
+
+            if (sceneUrl == null)
+                throw new IllegalStateException(
+                        "Required runtime resource missing: "
+                        + ninjaSceneString);
+            ogreSceneLoader = new SceneLoader();
+            ogreSceneLoader.load(sceneUrl.toURI());
+            ogreSceneLoader.setModelsOnly(true);
+             // modelsOnly means to ignore lights, cams, env. in scene file.
+            rootNode.attachChild(ogreSceneLoader.getScene());
+            logger.info("Successfully loaded and attached scene to rootNode");
+        } catch (ModelFormatException mfe) {
+            logger.log(Level.SEVERE, "Model file is corrupted", mfe);
+            // Not recoverable.
+            throw new RuntimeException(mfe);
+        } catch (IOException ioe) {
+            logger.log(Level.SEVERE, "Unrecoverable I/O failure", ioe);
+            // Not recoverable.
+            throw new RuntimeException(ioe);
+        } catch (URISyntaxException use) {
+            // Should not get this, since we are generating URI from a URL.
+            throw new RuntimeException(use);
+        } finally {
+            ogreSceneLoader = null;  // encourage GC
+              // Pretty useless here, but useful in a real app.
+        }
+    }
+}
