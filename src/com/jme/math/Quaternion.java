1b479f0c417781d0c1ba1d2e0a0fececad4c2e17
julien.gouesse <julien.gouesse@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2009-07-02 08:08:30 +0000

    Fix for the bug "Quaternion not serializable with XMLEncoder unlike Vector3f"
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@4457 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- src/com/jme/math/Quaternion.java
+++ src/com/jme/math/Quaternion.java
@@ -1274,5 +1274,37 @@ public class Quaternion implements Externalizable, Savable, Cloneable {
             throw new AssertionError(); // can not happen
         }
     }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public float getW() {
+        return w;
+    }
+
+    public void setW(float w) {
+        this.w = w;
+    }
 }
 
