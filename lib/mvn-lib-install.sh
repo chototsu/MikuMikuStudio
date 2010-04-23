2f58a5319289246d3eac0ded2831332e9c125475
christoph.luder <christoph.luder@75d07b2b-3a1a-0410-a2c5-0572b91ccdca>
2010-04-23 21:32:32 +0000

    added swt 64bit lib, fixed pom.xml to not execute Disk/ TubeAngleTests as JUnit Tests
    git-svn-id: http://jmonkeyengine.googlecode.com/svn/trunk@5143 75d07b2b-3a1a-0410-a2c5-0572b91ccdca

--- lib/mvn-lib-install.sh
+++ lib/mvn-lib-install.sh
@@ -8,3 +8,7 @@ mvn install:install-file -Dfile=lib/lwjgl/jinput.jar -DgroupId=net.java.dev.jinp
 mvn install:install-file -Dfile=lib/jorbis/jorbis-0.0.17.jar -DgroupId=jorbis -DartifactId=jorbis -Dversion=0.0.17 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/jogl/jogl.jar -DgroupId=net.java.dev.jogl -DartifactId=jogl -Dversion=1.1.1 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/jogl/gluegen-rt.jar -DgroupId=net.java.dev.gluegen -DartifactId=gluegen-rt -Dversion=1.0b06 -Dpackaging=jar
+
+#this is needed for windows 64
+#TOTO check other OS
+mvn install:install-file -Dfile=lib/swt/windows/swt_64.jar -DgroupId=org.eclipse.swt.win32.win32 -DartifactId=swt -Dversion=3.4 -Dpackaging=jar
